f7c2cad971253cea9cefa24b60bf0ea315f8ff07
Ilkka Seppala <iluwatar@gmail.com>
2014-09-07 15:20:27 +0300

    Added explanation about class adapter vs. object adapter in the adapter example.

--- adapter/src/main/java/com/iluwatar/App.java
+++ adapter/src/main/java/com/iluwatar/App.java
@@ -2,8 +2,14 @@ package com.iluwatar;
 
 /**
  * 
- * Adapter (GnomeEngineer) converts the interface of the
- * target class (GoblinGlider) into suitable one expected
+ * There are two variations of the Adapter pattern: The
+ * class adapter implements the adaptee's interface whereas
+ * the object adapter uses composition to contain the adaptee
+ * in the adapter object. This example uses the object adapter
+ * approach.
+ * 
+ * The Adapter (GnomeEngineer) converts the interface of the
+ * target class (GoblinGlider) into a suitable one expected
  * by the client (GnomeEngineeringManager).
  *
  */
