bde5b343d0dab8c1b77caa3a6e9183af33ef10f7
Matthew <lordmat@gmail.com>
2014-10-07 16:23:37 +0100

    Just formatting App classes to be like the other class files on the project

--- observer/src/main/java/com/iluwatar/App.java
+++ observer/src/main/java/com/iluwatar/App.java
@@ -1,25 +1,23 @@
 package com.iluwatar;
 
 /**
- * 
- * Observer pattern defines one-to-many relationship
- * between objects. The target object sends change
- * notifications to its registered observers.
+ *
+ * Observer pattern defines one-to-many relationship between objects. The target
+ * object sends change notifications to its registered observers.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
 
-    	Weather weather = new Weather();
-    	weather.addObserver(new Orcs());
-    	weather.addObserver(new Hobbits());
-    	
-    	weather.timePasses();
-    	weather.timePasses();
-    	weather.timePasses();
-    	weather.timePasses();
-    	
+    public static void main(String[] args) {
+
+        Weather weather = new Weather();
+        weather.addObserver(new Orcs());
+        weather.addObserver(new Hobbits());
+
+        weather.timePasses();
+        weather.timePasses();
+        weather.timePasses();
+        weather.timePasses();
+
     }
 }
