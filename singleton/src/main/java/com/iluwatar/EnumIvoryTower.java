16ac98305305673c0b627959a8da99b305a1b538
alwayswithme <phx13ye@gmail.com>
2014-11-25 16:24:32 +0800

    change comment

--- singleton/src/main/java/com/iluwatar/EnumIvoryTower.java
+++ singleton/src/main/java/com/iluwatar/EnumIvoryTower.java
@@ -3,6 +3,7 @@ package com.iluwatar;
 /**
  *
  * Enum Singleton class.
+ * http://stackoverflow.com/questions/70689/what-is-an-efficient-way-to-implement-a-singleton-pattern-in-java
  *
  */
 public enum EnumIvoryTower {
