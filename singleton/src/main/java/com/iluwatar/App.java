470f3d24608111613f2a1d41b99ca30345055cc9
Ilkka Seppala <iluwatar@gmail.com>
2014-11-26 22:47:44 +0200

    Added comments to singleton app.

--- singleton/src/main/java/com/iluwatar/App.java
+++ singleton/src/main/java/com/iluwatar/App.java
@@ -12,11 +12,13 @@ public class App {
 
 	public static void main(String[] args) {
 
+		// eagerly initialized singleton
 		IvoryTower ivoryTower1 = IvoryTower.getInstance();
 		IvoryTower ivoryTower2 = IvoryTower.getInstance();
 		System.out.println("ivoryTower1=" + ivoryTower1);
 		System.out.println("ivoryTower2=" + ivoryTower2);
 
+		// lazily initialized singleton
 		ThreadSafeLazyLoadedIvoryTower threadSafeIvoryTower1 = ThreadSafeLazyLoadedIvoryTower
 				.getInstance();
 		ThreadSafeLazyLoadedIvoryTower threadSafeIvoryTower2 = ThreadSafeLazyLoadedIvoryTower
@@ -24,6 +26,7 @@ public class App {
 		System.out.println("threadSafeIvoryTower1=" + threadSafeIvoryTower1);
 		System.out.println("threadSafeIvoryTower2=" + threadSafeIvoryTower2);
 
+		// enum singleton
 		EnumIvoryTower enumIvoryTower1 = EnumIvoryTower.INSTANCE;
 		EnumIvoryTower enumIvoryTower2 = EnumIvoryTower.INSTANCE;
 		System.out.println("enumIvoryTower1=" + enumIvoryTower1);
