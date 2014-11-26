baf177a81edadf077f6534b599513c45057309c7
Ilkka Seppala <iluwatar@gmail.com>
2014-11-26 22:41:44 +0200

    Added book reference to singleton enum implementation.

--- singleton/src/main/java/com/iluwatar/EnumIvoryTower.java
+++ singleton/src/main/java/com/iluwatar/EnumIvoryTower.java
@@ -3,9 +3,11 @@ package com.iluwatar;
 /**
  *
  * Enum Singleton class.
+ * Effective Java 2nd Edition (Joshua Bloch) p. 18
  *
  */
 public enum EnumIvoryTower {
+	
     INSTANCE;
 
     @Override
