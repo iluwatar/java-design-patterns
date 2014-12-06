6301e0bc111e280374f0589495e1313fbd77736e
Ilkka Seppala <iluwatar@gmail.com>
2014-12-06 20:34:41 +0200

    Added unit tests for each of the patterns.

---
+++ abstract-factory/src/test/java/com/iluwatar/AppTest.java
@@ -0,0 +1,13 @@
+package com.iluwatar;
+import org.junit.Test;
+
+import com.iluwatar.App;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		String[] args = {};
+		App.main(args);
+	}
+}
---
+++ iterator/src/test/java/com/iluwatar/AppTest.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		String[] args = {};
+		App.main(args);
+	}
+}
