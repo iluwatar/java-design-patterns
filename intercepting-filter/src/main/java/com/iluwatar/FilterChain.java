484e1ddfcac3ba22a3b6e7ba1bfd6eb917f388c9
Ilkka Seppala <iluwatar@gmail.com>
2015-04-10 17:27:20 +0300

    #43 Removed some commented out code.

--- intercepting-filter/src/main/java/com/iluwatar/FilterChain.java
+++ intercepting-filter/src/main/java/com/iluwatar/FilterChain.java
@@ -25,39 +25,10 @@ public class FilterChain {
 	}
 
 	public String execute(Order order) {
-
 		if (chain != null) {
 			return chain.execute(order);
 		} else {
 			return "RUNNING...";
 		}
-		
-//		String tempout[] = new String[filters.size()];
-//
-//		String tempin[] = request.split("&");
-//		int i = 0;
-//		try {
-//			for (Filter filter : filters) {
-//				tempout[i] = null;
-//				tempout[i++] = filter.execute(tempin);
-//			}
-//		} catch (Exception e) {
-//			return "NOT ENOUGHT INPUT";
-//		}
-//
-//		if (tempout[4] == null) {
-//			return "INVALID ORDER!";
-//		} else if (tempout[3] == null) {
-//			return "INVALID DEPOSIT NUMBER!";
-//		} else if (tempout[2] == null) {
-//			return "INVALID ADRDESS!";
-//		} else if (tempout[1] == null) {
-//			return "INVALID Contact Number!";
-//		} else if (tempout[0] == null) {
-//			return "INVALID Name!";
-//		} else {
-//			target.execute(tempout);
-//			return "RUNNING...";
-//		}
 	}
 }
