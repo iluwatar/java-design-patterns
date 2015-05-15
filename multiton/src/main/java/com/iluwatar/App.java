f2457699b0290762effb61fddc7e566412975a93
Ilkka Seppala <iluwatar@gmail.com>
2015-05-16 00:17:28 +0300

    Finished Multiton example.

--- multiton/src/main/java/com/iluwatar/App.java
+++ multiton/src/main/java/com/iluwatar/App.java
@@ -3,5 +3,14 @@ package com.iluwatar;
 public class App {
 	
     public static void main( String[] args ) {
+    	System.out.println("KHAMUL=" + Nazgul.getInstance(NazgulName.KHAMUL));
+    	System.out.println("MURAZOR=" + Nazgul.getInstance(NazgulName.MURAZOR));
+    	System.out.println("DWAR=" + Nazgul.getInstance(NazgulName.DWAR));
+    	System.out.println("JI_INDUR=" + Nazgul.getInstance(NazgulName.JI_INDUR));
+    	System.out.println("AKHORAHIL=" + Nazgul.getInstance(NazgulName.AKHORAHIL));
+    	System.out.println("HOARMURATH=" + Nazgul.getInstance(NazgulName.HOARMURATH));
+    	System.out.println("ADUNAPHEL=" + Nazgul.getInstance(NazgulName.ADUNAPHEL));
+    	System.out.println("REN=" + Nazgul.getInstance(NazgulName.REN));
+    	System.out.println("UVATHA=" + Nazgul.getInstance(NazgulName.UVATHA));    	
     }
 }
---
+++ multiton/src/main/java/com/iluwatar/NazgulName.java
@@ -0,0 +1,7 @@
+package com.iluwatar;
+
+public enum NazgulName {
+
+	KHAMUL, MURAZOR, DWAR, JI_INDUR, AKHORAHIL, HOARMURATH, ADUNAPHEL, REN, UVATHA;
+	
+}
