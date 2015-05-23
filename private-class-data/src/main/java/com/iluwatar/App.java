851fe4b3e0b0cbda26284c16860cf775191af53e
Ilkka Seppälä <iluwatar@gmail.com>
2015-05-23 15:45:30 +0300

    Work on the example.

--- private-class-data/src/main/java/com/iluwatar/App.java
+++ private-class-data/src/main/java/com/iluwatar/App.java
@@ -3,5 +3,9 @@ package com.iluwatar;
 public class App {
 	
     public static void main( String[] args ) {
+    	Stew stew = new Stew(1, 2, 3, 4);
+    	stew.mix();
+    	stew.taste();
+    	stew.mix();
     }
 }
