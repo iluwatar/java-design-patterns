c6cf96b641e7564e3fc7d787cb9f1ce59c312efc
Ilkka Seppala <iluwatar@gmail.com>
2015-07-22 00:31:50 +0300

    #55 Changed Hibernate to create-drop mode

--- repository/src/main/java/com/iluwatar/App.java
+++ repository/src/main/java/com/iluwatar/App.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+	
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
