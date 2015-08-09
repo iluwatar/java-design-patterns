d1bc28bb22e2e650ce56c4cd93fb6c001c570ee6
Ilkka Seppala <iluwatar@gmail.com>
2015-08-09 14:22:23 +0300

    #84 Utilize Repository layer from another example

--- layers/src/main/java/com/iluwatar/layers/App.java
+++ layers/src/main/java/com/iluwatar/layers/App.java
@@ -1,8 +1,48 @@
 package com.iluwatar.layers;
 
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		PersonDao dao = context.getBean(PersonDao.class);
+
+		Person peter = new Person("Peter", "Sagan");
+		Person nasta = new Person("Nasta", "Kuzminova");
+
+		// Add new Person records
+		dao.save(peter);
+		dao.save(nasta);
+
+		// Count Person records
+		System.out.println("Count Person records: " + dao.count());
+
+		// Print all records
+		List<Person> persons = (List<Person>) dao.findAll();
+		for (Person person : persons) {
+			System.out.println(person);
+		}
+
+		// Find Person by surname
+		System.out.println("Find by surname 'Sagan': "	+ dao.findBySurname("Sagan"));
+
+		// Update Person
+		nasta.setName("Barbora");
+		nasta.setSurname("Spotakova");
+		dao.save(nasta);
+
+		System.out.println("Find by id 2: " + dao.findOne(2L));
+
+		// Remove record from Person
+		dao.delete(2L);
+
+		// And finally count records
+		System.out.println("Count Person records: " + dao.count());
+
+		context.close();
 	}
 }
