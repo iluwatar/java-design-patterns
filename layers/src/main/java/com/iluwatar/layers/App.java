1bcd822c58bbca40507011d818b3b8d8f227f9e0
Ilkka Seppala <iluwatar@gmail.com>
2015-08-09 14:00:27 +0300

    #84 Skeleton for Layers architectural pattern

---
+++ layers/src/main/java/com/iluwatar/layers/App.java
@@ -0,0 +1,8 @@
+package com.iluwatar.layers;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ layers/src/test/java/com/iluwatar/layers/AppTest.java
@@ -0,0 +1,12 @@
+package com.iluwatar.layers;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		String[] args = {};
+		App.main(args);
+	}
+}
