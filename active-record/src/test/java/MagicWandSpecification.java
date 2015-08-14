7bc202c32da410974229556973e3afd9630d2654
Stephen Lazarionok <stephen.lazarionok@gmail.com>
2015-08-13 21:57:24 -0400

    Fix padding in pom.xml and change variable name

--- active-record/src/test/java/MagicWandSpecification.java
+++ active-record/src/test/java/MagicWandSpecification.java
@@ -13,48 +13,48 @@ public class MagicWandSpecification {
     @Test
     public void shouldCastFireball_IfMaigPower10AndMore() throws SpellCastException {
 
-        final MagicWand stick = new MagicWand();
-        stick.setWood(WandWoodType.WINE);
-        stick.setCore(WandCoreType.DRAGON_HEARTSTRING);
-        stick.setLengthInches(10.0d);
+        final MagicWand wand = new MagicWand();
+        wand.setWood(WandWoodType.WINE);
+        wand.setCore(WandCoreType.DRAGON_HEARTSTRING);
+        wand.setLengthInches(10.0d);
 
-        Assert.assertTrue(stick.calculateMagicPower() == 10.0d);
-        stick.castFireball();
+        Assert.assertTrue(wand.calculateMagicPower() == 10.0d);
+        wand.castFireball();
     }
 
     @Test(expected = SpellCastException.class)
     public void shouldNotCastFireball_IfMaigPowerLessThan10() throws SpellCastException {
 
-        final MagicWand stick = new MagicWand();
-        stick.setWood(WandWoodType.WINE);
-        stick.setCore(WandCoreType.DRAGON_HEARTSTRING);
-        stick.setLengthInches(9.0d);
+        final MagicWand wand = new MagicWand();
+        wand.setWood(WandWoodType.WINE);
+        wand.setCore(WandCoreType.DRAGON_HEARTSTRING);
+        wand.setLengthInches(9.0d);
 
-        Assert.assertTrue(stick.calculateMagicPower() == 9.0d);
-        stick.castFireball();
+        Assert.assertTrue(wand.calculateMagicPower() == 9.0d);
+        wand.castFireball();
     }
 
     @Test
     public void shouldCastLigthing_IfMaigPower20AndMore() throws SpellCastException {
 
-        final MagicWand stick = new MagicWand();
-        stick.setWood(WandWoodType.WINE);
-        stick.setCore(WandCoreType.DRAGON_HEARTSTRING);
-        stick.setLengthInches(20.5d);
+        final MagicWand wand = new MagicWand();
+        wand.setWood(WandWoodType.WINE);
+        wand.setCore(WandCoreType.DRAGON_HEARTSTRING);
+        wand.setLengthInches(20.5d);
 
-        Assert.assertTrue(stick.calculateMagicPower() == 20.5d);
-        stick.castLighting();
+        Assert.assertTrue(wand.calculateMagicPower() == 20.5d);
+        wand.castLighting();
     }
 
     @Test(expected = SpellCastException.class)
     public void shouldNotCastLigthing_IfMaigPowerLessThan20() throws SpellCastException {
 
-        final MagicWand stick = new MagicWand();
-        stick.setWood(WandWoodType.WINE);
-        stick.setCore(WandCoreType.DRAGON_HEARTSTRING);
-        stick.setLengthInches(18.0d);
+        final MagicWand wand = new MagicWand();
+        wand.setWood(WandWoodType.WINE);
+        wand.setCore(WandCoreType.DRAGON_HEARTSTRING);
+        wand.setLengthInches(18.0d);
 
-        Assert.assertTrue(stick.calculateMagicPower() == 18.0d);
-        stick.castLighting();
+        Assert.assertTrue(wand.calculateMagicPower() == 18.0d);
+        wand.castLighting();
     }
 }
