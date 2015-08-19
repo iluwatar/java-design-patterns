5295ad6c2f1625268f30d5bb9939d1e7af657198
Ilkka Seppala <iluwatar@gmail.com>
2015-08-19 22:00:36 +0300

    #107 Added missing test for Layers example

---
+++ layers/src/test/java/com/iluwatar/layers/AppTest.java
@@ -0,0 +1,19 @@
+package com.iluwatar.layers;
+
+import org.junit.Test;
+
+import com.iluwatar.layers.App;
+
+/**
+ * 
+ * Application test
+ *
+ */
+public class AppTest {
+
+	@Test
+	public void test() {
+		String[] args = {};
+		App.main(args);
+	}
+}
