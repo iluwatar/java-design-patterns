520f81d62ec59cc308bc2cf7344bca52ae9e51c7
Ilkka Seppala <iluwatar@gmail.com>
2015-08-21 22:28:51 +0300

    #107 Improve JavaDoc for Observer example

--- observer/src/main/java/com/iluwatar/observer/generic/Observer.java
+++ observer/src/main/java/com/iluwatar/observer/generic/Observer.java
@@ -1,5 +1,13 @@
 package com.iluwatar.observer.generic;
 
+/**
+ * 
+ * Observer
+ *
+ * @param <S>
+ * @param <O>
+ * @param <A>
+ */
 public interface Observer<S extends Observable<S, O, A>, O extends Observer<S, O, A>, A> {
 
     void update(S subject, A argument);
