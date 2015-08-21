743b6e69c591004594965df463916c620bfedf2b
Ilkka Seppala <iluwatar@gmail.com>
2015-08-21 22:59:59 +0300

    #107 Service Locator JavaDoc

--- service-locator/src/main/java/com/iluwatar/servicelocator/App.java
+++ service-locator/src/main/java/com/iluwatar/servicelocator/App.java
@@ -1,12 +1,18 @@
 package com.iluwatar.servicelocator;
 
 /**
- * Service locator pattern, used to lookup jndi services
+ * Service locator pattern, used to lookup JNDI-services
  * and cache them for subsequent requests.
  *
  * @author saifasif
+ * 
  */
 public class App {
+	
+	/**
+	 * Program entry point
+	 * @param args command line args
+	 */
     public static void main(String[] args) {
         Service service = ServiceLocator.getService("jndi/serviceA");
         service.execute();
