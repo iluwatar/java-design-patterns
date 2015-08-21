57a271d4feaed90d221625dd315e0e58efb4d268
Ilkka Seppala <iluwatar@gmail.com>
2015-08-21 22:49:53 +0300

    #107 Improve Repository example's JavaDoc

--- repository/src/main/java/com/iluwatar/repository/App.java
+++ repository/src/main/java/com/iluwatar/repository/App.java
@@ -13,14 +13,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * query construction code is concentrated. This becomes more important when there are a large 
  * number of domain classes or heavy querying. In these cases particularly, adding this layer helps
  * minimize duplicate query logic.
- * 
- * In this example we utilize Spring Data to automatically generate a repository for us from the Person
- * domain object. Using the PersonDao we perform CRUD operations on the entity. Underneath we have
+ * <p>
+ * In this example we utilize Spring Data to automatically generate a repository for us from the {@link Person}
+ * domain object. Using the {@link PersonRepository} we perform CRUD operations on the entity. Underneath we have
  * configured in-memory H2 database for which schema is created and dropped on each run.
  *
  */
 public class App {
 	
+	/**
+	 * Program entry point
+	 * @param args command line args
+	 */
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
