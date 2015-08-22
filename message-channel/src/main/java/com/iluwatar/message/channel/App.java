acd7070831997a37e7cda3e19040a22ffa00b751
Ilkka Seppala <iluwatar@gmail.com>
2015-08-22 16:37:19 +0300

    #143 Work on the example code

--- message-channel/src/main/java/com/iluwatar/message/channel/App.java
+++ message-channel/src/main/java/com/iluwatar/message/channel/App.java
@@ -1,14 +1,27 @@
 package com.iluwatar.message.channel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class App {
 	
 	public static void main(String[] args) throws Exception {
 		CamelContext context = new DefaultCamelContext();
+		
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				from("direct:greetings").to("stream:out");
+			}
+		});
+		
 		context.start();
-		Thread.sleep(10000);
+		ProducerTemplate template = context.createProducerTemplate();
+		template.sendBody("direct:greetings", "jou man");
+		Thread.sleep(1000);
 		context.stop();
 	}
 }
