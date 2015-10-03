cdd586ec7cc3e7d83bcec61235ce7e010d3115ff
Ilkka Seppala <iluwatar@gmail.com>
2015-10-03 12:40:24 +0300

    Improve Bridge Javadoc

--- bridge/src/main/java/com/iluwatar/bridge/App.java
+++ bridge/src/main/java/com/iluwatar/bridge/App.java
@@ -2,6 +2,9 @@ package com.iluwatar.bridge;
 
 /**
  * 
+ * The Bridge pattern can also be thought of as two layers of abstraction. With Bridge, 
+ * you can decouple an abstraction from its implementation so that the two can vary independently.
+ * <p>
  * In Bridge pattern both abstraction ({@link MagicWeapon}) and implementation
  * ({@link MagicWeaponImpl}) have their own class hierarchies. The interface of the
  * implementations can be changed without affecting the clients.
