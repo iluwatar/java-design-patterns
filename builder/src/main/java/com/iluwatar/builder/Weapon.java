3af06a3a3afe85dd358ec99e958ad88a0400ff40
Ankur Kaushal <ankurkaushal@outlook.com>
2015-11-01 18:03:29 -0500

    Reformat builder pattern - issue #224

--- builder/src/main/java/com/iluwatar/builder/Weapon.java
+++ builder/src/main/java/com/iluwatar/builder/Weapon.java
@@ -7,11 +7,10 @@ package com.iluwatar.builder;
  */
 public enum Weapon {
 
-	DAGGER, SWORD, AXE, WARHAMMER, BOW;
+  DAGGER, SWORD, AXE, WARHAMMER, BOW;
 
-	@Override
-	public String toString() {
-		return name().toLowerCase();
-	}
-
+  @Override
+  public String toString() {
+    return name().toLowerCase();
+  }
 }
