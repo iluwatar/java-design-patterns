87baa98d2b6cdf937300ecef4f5466b8af68473e
Ankur Kaushal <ankurkaushal@outlook.com>
2015-11-01 02:05:54 -0500

    Reformat Code According to Google Style Guide #224

--- observer/src/main/java/com/iluwatar/observer/WeatherObserver.java
+++ observer/src/main/java/com/iluwatar/observer/WeatherObserver.java
@@ -7,6 +7,6 @@ package com.iluwatar.observer;
  */
 public interface WeatherObserver {
 
-	void update(WeatherType currentWeather);
+  void update(WeatherType currentWeather);
 
 }
--- observer/src/main/java/com/iluwatar/observer/generic/GWeather.java
+++ observer/src/main/java/com/iluwatar/observer/generic/GWeather.java
@@ -9,16 +9,16 @@ import com.iluwatar.observer.WeatherType;
  */
 public class GWeather extends Observable<GWeather, Race, WeatherType> {
 
-    private WeatherType currentWeather;
+  private WeatherType currentWeather;
 
-    public GWeather() {
-        currentWeather = WeatherType.SUNNY;
-    }
+  public GWeather() {
+    currentWeather = WeatherType.SUNNY;
+  }
 
-    public void timePasses() {
-        WeatherType[] enumValues = WeatherType.values();
-        currentWeather = enumValues[(currentWeather.ordinal() + 1) % enumValues.length];
-        System.out.println("The weather changed to " + currentWeather + ".");
-        notifyObservers(currentWeather);
-    }
+  public void timePasses() {
+    WeatherType[] enumValues = WeatherType.values();
+    currentWeather = enumValues[(currentWeather.ordinal() + 1) % enumValues.length];
+    System.out.println("The weather changed to " + currentWeather + ".");
+    notifyObservers(currentWeather);
+  }
 }
