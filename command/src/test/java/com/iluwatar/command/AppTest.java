c115d5e55c92f4dfc941ca2dd9753254736770a1
Richard Jones <richard.jones@vistajet.com>
2015-11-08 13:27:53 +0000

    Add unit test to show that command patterns is working as expected

--- command/src/test/java/com/iluwatar/command/AppTest.java
+++ command/src/test/java/com/iluwatar/command/AppTest.java
@@ -2,18 +2,108 @@ package com.iluwatar.command;
 
 import org.junit.Test;
 
-import com.iluwatar.command.App;
+import static org.junit.Assert.assertEquals;
 
-/**
- * 
- * Application test
- *
- */
 public class AppTest {
 
 	@Test
-	public void test() {
-		String[] args = {};
-		App.main(args);
+	public void test_DoingUndoingrRedoingSpells() {
+        Wizard wizard = new Wizard();
+        Goblin goblin = new Goblin();
+
+        //initial state
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //do spells
+        wizard.castSpell(new ShrinkSpell(), goblin);
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        wizard.castSpell(new InvisibilitySpell(), goblin);
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.INVISIBLE, goblin.getVisibility());
+
+        //now start undoing
+        wizard.undoLastSpell();
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        wizard.undoLastSpell();
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //now start redoing
+        wizard.redoLastSpell();
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        wizard.redoLastSpell();
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.INVISIBLE, goblin.getVisibility());
 	}
+
+    @Test
+    public void test_UndoingWhenNotPossible() {
+        Wizard wizard = new Wizard();
+        Goblin goblin = new Goblin();
+
+        //initial state
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //undoing when no spell
+        wizard.undoLastSpell();
+
+        //who care = same as initial state
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //do one spell
+        wizard.castSpell(new ShrinkSpell(), goblin);
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //undoing back to initial state
+        wizard.undoLastSpell();
+
+        //who care = same as initial state
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //nothing else to undo but who cares - state still same
+        wizard.undoLastSpell();
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+    }
+
+    @Test
+    public void test_RedoingWhenNotPossible() {
+        Wizard wizard = new Wizard();
+        Goblin goblin = new Goblin();
+
+        //initial state
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //undoing when no spell
+        wizard.redoLastSpell();
+
+        //who care = same as initial state
+        assertEquals(Size.NORMAL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        wizard.castSpell(new ShrinkSpell(), goblin);
+
+        //who care = same as initial state
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+
+        //redoing same spell
+        wizard.redoLastSpell();
+
+        //nothing to do in this case - spell has already been done
+        assertEquals(Size.SMALL, goblin.getSize());
+        assertEquals(Visibility.VISIBLE, goblin.getVisibility());
+    }
 }
