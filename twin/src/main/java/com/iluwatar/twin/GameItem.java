23fe7d08299e69a099f7e44efff70e35b6016145
hoswey <hongshuwei@gmail.com>
2015-11-14 17:26:42 +0800

    implements Twin design pattern #63

---
+++ twin/src/main/java/com/iluwatar/twin/GameItem.java
@@ -0,0 +1,25 @@
+
+
+package com.iluwatar.twin;
+
+/**
+ * GameItem is a common class which provides some common methods for game object.
+ */
+public abstract class GameItem {
+
+  /**
+   * Template method, do some common logic before draw
+   * 
+   * @param other
+   * @return
+   */
+  public void draw() {
+    System.out.println("draw");
+    doDraw();
+  }
+
+  public abstract void doDraw();
+
+
+  public abstract void click();
+}
