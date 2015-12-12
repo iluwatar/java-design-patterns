3dc370e2d17677419b3c7f824210876ecb22fce4
Jeroen Meulemeester <jeroen.meulemeester@gmail.com>
2015-12-12 22:43:47 +0100

    Add proper tests for flux pattern

---
+++ flux/src/test/java/com/iluwatar/flux/store/MenuStoreTest.java
@@ -0,0 +1,47 @@
+package com.iluwatar.flux.store;
+
+import com.iluwatar.flux.action.Content;
+import com.iluwatar.flux.action.ContentAction;
+import com.iluwatar.flux.action.MenuAction;
+import com.iluwatar.flux.action.MenuItem;
+import com.iluwatar.flux.view.View;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+/**
+ * Date: 12/12/15 - 10:18 PM
+ *
+ * @author Jeroen Meulemeester
+ */
+public class MenuStoreTest {
+
+  @Test
+  public void testOnAction() throws Exception {
+    final MenuStore menuStore = new MenuStore();
+
+    final View view = mock(View.class);
+    menuStore.registerView(view);
+
+    verifyZeroInteractions(view);
+
+    // Menu should not react on content action ...
+    menuStore.onAction(new ContentAction(Content.COMPANY));
+    verifyZeroInteractions(view);
+
+    // ... but it should react on a menu action
+    menuStore.onAction(new MenuAction(MenuItem.PRODUCTS));
+    verify(view, times(1)).storeChanged(eq(menuStore));
+    verifyNoMoreInteractions(view);
+    assertEquals(MenuItem.PRODUCTS, menuStore.getSelected());
+
+  }
+
+}
