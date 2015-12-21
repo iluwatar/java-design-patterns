531158c83610d2ffb6fb4655189212a2122a7e95
Jeroen Meulemeester <jeroen.meulemeester@gmail.com>
2015-12-21 13:29:25 +0100

    Added tests for monostate pattern

--- monostate/src/test/java/com/iluwatar/monostate/AppTest.java
+++ monostate/src/test/java/com/iluwatar/monostate/AppTest.java
@@ -6,17 +6,6 @@ import org.junit.Test;
 public class AppTest {
 
   @Test
-  public void testSameStateAmonstAllInstances() {
-    LoadBalancer balancer = new LoadBalancer();
-    LoadBalancer balancer2 = new LoadBalancer();
-    balancer.addServer(new Server("localhost", 8085, 6));
-    // Both should have the same number of servers.
-    Assert.assertTrue(balancer.getNoOfServers() == balancer2.getNoOfServers());
-    // Both Should have the same LastServedId
-    Assert.assertTrue(balancer.getLastServedId() == balancer2.getLastServedId());
-  }
-
-  @Test
   public void testMain() {
     String[] args = {};
     App.main(args);
