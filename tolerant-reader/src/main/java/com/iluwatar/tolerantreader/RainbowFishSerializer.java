6e496e7c8682f15b0181c95d84b7054952ac4b2c
Kirill Vlasov <kirill.vlasov@devfactory.com>
2015-12-22 14:48:24 +0500

    Fixing squid:S1854 - Dead stores should be removed

--- tolerant-reader/src/main/java/com/iluwatar/tolerantreader/RainbowFishSerializer.java
+++ tolerant-reader/src/main/java/com/iluwatar/tolerantreader/RainbowFishSerializer.java
@@ -70,10 +70,9 @@ public class RainbowFishSerializer {
    * @throws ClassNotFoundException
    */
   public static RainbowFish readV1(String filename) throws IOException, ClassNotFoundException {
-    Map<String, String> map = null;
     FileInputStream fileIn = new FileInputStream(filename);
     ObjectInputStream objIn = new ObjectInputStream(fileIn);
-    map = (Map<String, String>) objIn.readObject();
+    Map<String, String> map = (Map<String, String>) objIn.readObject();
     objIn.close();
     fileIn.close();
     return new RainbowFish(map.get("name"), Integer.parseInt(map.get("age")), Integer.parseInt(map
