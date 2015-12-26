f0ff8ad4b321af3977f25bf1617764459ade5ea9
Joseph McCarthy <luckymikuhatsune@gmail.com>
2015-12-26 22:55:12 +0000

    Created a unit test for build, added junit to pom for delegation maven module. #324

--- delegation/src/test/java/com/iluwatar/delegation/simple/AppTest.java
+++ delegation/src/test/java/com/iluwatar/delegation/simple/AppTest.java
@@ -3,12 +3,14 @@ package com.iluwatar.delegation.simple;
 import com.iluwatar.delegation.simple.printers.CanonPrinter;
 import com.iluwatar.delegation.simple.printers.EpsonPrinter;
 import com.iluwatar.delegation.simple.printers.HPPrinter;
+import org.junit.Test;
 
 public class AppTest {
 
     public static final String MESSAGE_TO_PRINT = "hello world";
 
-    public static void main(String args[]) {
+    @Test
+    public static void main() {
         Printer hpPrinter = new HPPrinter();
         Printer canonPrinter = new CanonPrinter();
         Printer epsonPrinter = new EpsonPrinter();
