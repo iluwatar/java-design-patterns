64b89ecb595cb1a6d36aee8623fe0e7b800781eb
JuhoKang <rjduakcjs@gmail.com>
2016-01-23 10:06:57 +0900

    Create project for value-object pattern

---
+++ value-object/value-object/src/main/java/com/iluwatar/value/object/App.java
@@ -0,0 +1,13 @@
+package com.iluwatar.value.object;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
