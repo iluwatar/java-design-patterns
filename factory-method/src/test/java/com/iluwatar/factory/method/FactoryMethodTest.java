8b625a8d3c3b1062a85df2403a920ffc3e1e9f98
Crossy147 <kamilos147@gmail.com>
2016-02-15 20:54:55 +0100

    issue #333 minor revert

--- factory-method/src/test/java/com/iluwatar/factory/method/FactoryMethodTest.java
+++ factory-method/src/test/java/com/iluwatar/factory/method/FactoryMethodTest.java
@@ -1,9 +1,31 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.factory.method;
-
-import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * The Factory Method is a creational design pattern which uses factory methods to deal with the
@@ -11,8 +33,8 @@ import static org.junit.Assert.assertTrue;
  * This is done by creating objects via calling a factory method either specified in an interface
  * and implemented by child classes, or implemented in a base class and optionally overridden by
  * derived classes—rather than by calling a constructor.
- * <p>
- * <p>Factory produces the object of its liking.
+ * 
+ * <p>Factory produces the object of its liking. 
  * The weapon {@link Weapon} manufactured by the
  * blacksmith depends on the kind of factory implementation it is referring to.
  * </p>
@@ -20,7 +42,7 @@ import static org.junit.Assert.assertTrue;
 public class FactoryMethodTest {
 
   /**
-   * Testing {@link OrcBlacksmith} to produce a SPEAR asserting that the Weapon is an instance
+   * Testing {@link OrcBlacksmith} to produce a SPEAR asserting that the Weapon is an instance 
    * of {@link OrcWeapon}.
    */
   @Test
@@ -32,7 +54,7 @@ public class FactoryMethodTest {
 
   /**
    * Testing {@link OrcBlacksmith} to produce a AXE asserting that the Weapon is an instance
-   * of {@link OrcWeapon}.
+   *  of {@link OrcWeapon}.
    */
   @Test
   public void testOrcBlacksmithWithAxe() {
@@ -66,10 +88,10 @@ public class FactoryMethodTest {
   /**
    * This method asserts that the weapon object that is passed is an instance of the clazz and the
    * weapon is of type expectedWeaponType.
-   *
-   * @param weapon             weapon object which is to be verified
+   * 
+   * @param weapon weapon object which is to be verified
    * @param expectedWeaponType expected WeaponType of the weapon
-   * @param clazz              expected class of the weapon
+   * @param clazz expected class of the weapon
    */
   private void verifyWeapon(Weapon weapon, WeaponType expectedWeaponType, Class clazz) {
     assertTrue("Weapon must be an object of: " + clazz.getName(), clazz.isInstance(weapon));
