2fa705ab595ed2480be263ca4ada72a38864f8ae
Crossy147 <kamilos147@gmail.com>
2016-02-16 00:05:28 +0100

    issue #333 snapshot version fixed

--- factory-kit/src/main/java/com/iluwatar/factorykit/Builder.java
+++ factory-kit/src/main/java/com/iluwatar/factorykit/Builder.java
@@ -3,7 +3,7 @@ package com.iluwatar.factorykit;
 import java.util.function.Supplier;
 
 /**
- * Functional interface that allows adding builder with name to the factory
+ * Functional interface that allows adding builder with name to the factory.
  */
 public interface Builder {
   void add(WeaponType name, Supplier<Weapon> supplier);
