d2f620a5e62f187823629414286f141e45b91c98
Ilkka Seppälä <iluwatar@gmail.com>
2016-03-22 21:27:13 +0200

    Added tests for lottery ticket repository

---
+++ hexagonal/src/test/java/com/iluwatar/hexagonal/domain/LotteryTicketRepositoryTest.java
@@ -0,0 +1,61 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.iluwatar.hexagonal.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import com.iluwatar.hexagonal.database.LotteryTicketRepositoryMock;
+
+/**
+ * 
+ * Tests for {@link LotteryTicketRepository}
+ *
+ */
+public class LotteryTicketRepositoryTest {
+
+  @Test
+  public void testCrudOperations() {
+    LotteryTicketRepository repository = new LotteryTicketRepositoryMock();
+    assertEquals(repository.findAll().size(), 0);
+    LotteryTicket ticket = createLotteryTicket();
+    Optional<UUID> uuid = repository.save(ticket);
+    assertTrue(uuid.isPresent());
+    assertEquals(repository.findAll().size(), 1);
+    Optional<LotteryTicket> optionalTicket = repository.findByUuid(uuid.get());
+    assertTrue(optionalTicket.isPresent());
+  }
+  
+  private LotteryTicket createLotteryTicket() {
+    PlayerDetails details = PlayerDetails.create("foo@bar.com", "12231-213132", "+99324554");
+    LotteryNumbers numbers = LotteryNumbers.create(new HashSet<>(Arrays.asList(1, 2, 3, 4)));
+    return LotteryTicket.create(details, numbers);
+  }
+}
