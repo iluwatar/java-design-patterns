e549af25dd8b2e028fa62adfde00d22daff9fa82
Saiful Islam <mr.saiful.azad@gmail.com>
2016-03-25 22:50:17 +0600

    Severe is not appropriate.
    "Sever" is most Appropriate

--- strategy/src/main/java/com/iluwatar/strategy/MeleeStrategy.java
+++ strategy/src/main/java/com/iluwatar/strategy/MeleeStrategy.java
@@ -31,6 +31,6 @@ public class MeleeStrategy implements DragonSlayingStrategy {
 
   @Override
   public void execute() {
-    System.out.println("With your Excalibur you severe the dragon's head!");
+    System.out.println("With your Excalibur you sever the dragon's head!");
   }
 }
