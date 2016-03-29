00bde301d4715b08e2caffa51265020a10e24ff6
amit2103 <amit.pandey2103@gmail.com>
2016-03-29 20:40:39 +0530

    added formatting

--- leader-follower/src/main/java/com/iluwatar/leaderfollower/WorkStation.java
+++ leader-follower/src/main/java/com/iluwatar/leaderfollower/WorkStation.java
@@ -1,9 +1,29 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.leaderfollower;
 
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,30 +37,30 @@ public class WorkStation {
     this.executorService = executorService;
   }
 
+  /**
+   * Start the work. Add workers and then dispatch new work to be processed by the set of workers
+   */
   public void startWork() throws InterruptedException {
-
-    BlockingQueue<Work> queue = new ArrayBlockingQueue<>(100);
-    int i =1;
-    
-    while (i <= 4) {
-      Worker worker = new Worker(queue, workers, i, this);
+    int i = 1;
+    HandleSet handleSet = new HandleSet();
+    ConcreteEventHandler concreteEventHandler = new ConcreteEventHandler();
+    while (i <= 5) {
+      Worker worker = new Worker(handleSet, workers, i, this, concreteEventHandler);
       workers.add(worker);
+      i++;
     }
     this.leader = workers.get(0);
-    executorService.submit( workers.get(0));
-    executorService.submit( workers.get(1));
-    executorService.submit( workers.get(2));
-    executorService.submit( workers.get(3));
+    executorService.submit(workers.get(0));
+    executorService.submit(workers.get(1));
+    executorService.submit(workers.get(2));
+    executorService.submit(workers.get(3));
     Random rand = new Random(1000);
-    int j =0;
+    int j = 0;
     while (j < 4) {
-      queue.put(new Work(rand.nextInt()));
-      queue.put(new Work(rand.nextInt()));
-      queue.put(new Work(rand.nextInt()));
-      queue.put(new Work(rand.nextInt()));
-      queue.put(new Work(rand.nextInt()));
+      handleSet.fireEvent(new Work(Math.abs(rand.nextInt())));
+      j++;
     }
-   
+
     // queue.
     Thread.sleep(1000);
   }
@@ -53,9 +73,11 @@ public class WorkStation {
     this.leader = leader;
   }
 
+  /**
+   * Add a worker to the work station.
+   */
   public void addWorker(Worker worker) {
     if (this.workers.size() <= 0) {
-      this.leader = worker;
       workers.add(worker);
     }
   }
--- leader-follower/src/main/java/com/iluwatar/leaderfollower/Worker.java
+++ leader-follower/src/main/java/com/iluwatar/leaderfollower/Worker.java
@@ -1,16 +1,41 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.leaderfollower;
 
 import java.util.List;
 
 public class Worker implements Runnable {
 
-  private Object leader = new Object();
   private final HandleSet handleSet;
   private List<Worker> workers;
   private final long id;
   private final WorkStation workstation;
   private final ConcreteEventHandler concreteEventHandler;
 
+  /**
+   * 
+   * Constructor to create a worker which will take work from the work station.
+   */
   public Worker(HandleSet queue, List<Worker> workers, long id, WorkStation workstation,
       ConcreteEventHandler concreteEventHandler) {
     super();
@@ -21,9 +46,12 @@ public class Worker implements Runnable {
     this.concreteEventHandler = concreteEventHandler;
   }
 
+  /**
+   * Become the leader, and notify others
+   */
   public void becomeLeader() {
-    synchronized (leader) {
-      leader.notifyAll();
+    synchronized (workstation) {
+      workstation.notifyAll();
     }
   }
 
@@ -35,8 +63,8 @@ public class Worker implements Runnable {
       try {
         if (workstation.getLeader() != null && !workstation.getLeader().equals(this)) {
           // System.out.println("ID " +id + " is follower");
-          synchronized (leader) {
-            leader.wait();
+          synchronized (workstation) {
+            workstation.wait();
           }
 
         }
@@ -50,6 +78,11 @@ public class Worker implements Runnable {
         } else {
           workstation.setLeader(null);
         }
+        synchronized (workstation) {
+          workstation.notifyAll();
+        }
+
+
         concreteEventHandler.handleEvent(work);
         Thread.sleep(100);
         System.out.println("The Worker with the ID " + id + " completed the task");
@@ -69,17 +102,24 @@ public class Worker implements Runnable {
     return result;
   }
 
+  /**
+   * Overridden equala method
+   */
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
+    if (this == obj) {
       return true;
-    if (obj == null)
+    }
+    if (obj == null) {
       return false;
-    if (getClass() != obj.getClass())
+    }
+    if (getClass() != obj.getClass()) {
       return false;
+    }
     Worker other = (Worker) obj;
-    if (id != other.id)
+    if (id != other.id) {
       return false;
+    }
     return true;
   }
 
