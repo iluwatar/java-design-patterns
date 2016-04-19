28c271486274c0e9aca866a90c28f630524c5d48
Amit Dixit <amtdxt@yahoo.com>
2016-04-19 11:25:42 +0530

    review comments++
    review comments++

--- data-mapper/src/main/java/com/iluwatar/datamapper/DataMapperException.java
+++ data-mapper/src/main/java/com/iluwatar/datamapper/DataMapperException.java
@@ -30,15 +30,6 @@ public final class DataMapperException extends RuntimeException {
   private static final long serialVersionUID = 1L;
 
   /**
-   * Constructs a new runtime exception with {@code null} as its detail message. The cause is not
-   * initialized, and may subsequently be initialized by a call to {@link #initCause}.
-   */
-  public DataMapperException() {
-    super();
-  }
-
-
-  /**
    * Constructs a new runtime exception with the specified detail message. The cause is not
    * initialized, and may subsequently be initialized by a call to {@link #initCause}.
    *
@@ -47,35 +38,5 @@ public final class DataMapperException extends RuntimeException {
    */
   public DataMapperException(final String message) {
     super(message);
-  }
-
-  /**
-   * Constructs a new runtime exception with the specified detail message and cause.
-   * <p>
-   * Note that the detail message associated with {@code cause} is <i>not</i> automatically
-   * incorporated in this runtime exception's detail message.
-   *
-   * @param message the detail message (which is saved for later retrieval by the
-   *        {@link #getMessage()} method).
-   * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method).
-   *        (A <tt>null</tt> value is permitted, and indicates that the cause is nonexistent or
-   *        unknown.)
-   */
-  public DataMapperException(final String message, final Throwable cause) {
-    super(message, cause);
-  }
-
-  /**
-   * Constructs a new runtime exception with the specified cause and a detail message of
-   * <tt>(cause==null ? null : cause.toString())</tt> (which typically contains the class and detail
-   * message of <tt>cause</tt>). This constructor is useful for runtime exceptions that are little
-   * more than wrappers for other throwables.
-   *
-   * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method).
-   *        (A <tt>null</tt> value is permitted, and indicates that the cause is nonexistent or
-   *        unknown.)
-   */
-  public DataMapperException(final Throwable cause) {
-    super(cause);
   }
 }
