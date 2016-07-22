09ba5ca656dd17ac5c2d518452ad5f89df388e57
Narendra Pathai <narendra.pathai@gmail.com>
2016-07-22 18:34:05 +0530

    Work on #403, added class diagrams and javadocs

--- promise/src/main/java/com/iluwatar/promise/App.java
+++ promise/src/main/java/com/iluwatar/promise/App.java
@@ -1,5 +1,28 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.promise;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -7,14 +30,31 @@ import java.util.concurrent.Executors;
 
 /**
  * 
- * Application that uses promise pattern.
+ * <p>The Promise object is used for asynchronous computations. A Promise represents an operation that
+ * hasn't completed yet, but is expected in the future.
+ * 
+ * <p>A Promise represents a proxy for a value not necessarily known when the promise is created. It 
+ * allows you to associate dependent promises to an asynchronous action's eventual success value or 
+ * failure reason. This lets asynchronous methods return values like synchronous methods: instead of the final
+ * value, the asynchronous method returns a promise of having a value at some point in the future.
+ * 
+ * <p>Promises provide a few advantages over callback objects:
+ * <ul>
+ * <li> Functional composition and error handling
+ * <li> Prevents callback hell and provides callback aggregation
+ * </ul>
+ * 
+ * @see CompletableFuture
  */
 public class App {
 
+  private App() {
+  }
+  
   /**
    * Program entry point
    * @param args arguments
-   * @throws InterruptedException if main thread is interruped.
+   * @throws InterruptedException if main thread is interrupted.
    * @throws ExecutionException if an execution error occurs.
    */
   public static void main(String[] args) throws InterruptedException, ExecutionException {
