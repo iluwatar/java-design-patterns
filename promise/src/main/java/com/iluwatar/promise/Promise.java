ad11ea46b11e638ee564b86eaf2e5763a3f780f1
Narendra Pathai <narendra.pathai@gmail.com>
2016-08-29 11:55:30 +0530

    Work on #403, javadocs updated

--- promise/src/main/java/com/iluwatar/promise/Promise.java
+++ promise/src/main/java/com/iluwatar/promise/Promise.java
@@ -29,7 +29,11 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- * Implements the promise pattern.
+ * A Promise represents a proxy for a value not necessarily known when the promise is created. It
+ * allows you to associate dependent promises to an asynchronous action's eventual success value or
+ * failure reason. This lets asynchronous methods return values like synchronous methods: instead 
+ * of the final value, the asynchronous method returns a promise of having a value at some point 
+ * in the future.
  * 
  * @param <T> type of result.
  */
