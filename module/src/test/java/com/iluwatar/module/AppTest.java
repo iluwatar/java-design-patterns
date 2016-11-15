f67d2fd9a9146649ba7a33ec1fcc855a21f4ff40
Amit Dixit <amtdxt@yahoo.com>
2016-11-15 13:01:37 +0530

    Review comments++

--- module/src/test/java/com/iluwatar/module/AppTest.java
+++ module/src/test/java/com/iluwatar/module/AppTest.java
@@ -1,5 +1,5 @@
 /**
- * The MIT License Copyright (c) 2016 Amit Dixit
+ * The MIT License Copyright (c) 2014 Ilkka Seppälä
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
