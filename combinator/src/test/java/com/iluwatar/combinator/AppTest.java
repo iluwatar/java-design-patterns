029865ceb82594bf664ccc9d3251ed54fcabbc21
matthewwier <fjh5919@gmail.com>
2017-01-04 17:15:36 +0100

    Create AppTest.java #499

---
+++ combinator/src/test/java/com/iluwatar/combinator/AppTest.java
@@ -0,0 +1,17 @@
+package com.iluwatar.combinator;
+
+import org.junit.Test;
+
+/**
+ * 
+ * Application test
+ *
+ */
+public class AppTest {
+
+  @Test
+  public void test() {
+    String[] args = {};
+    App.main(args);
+  }
+}
