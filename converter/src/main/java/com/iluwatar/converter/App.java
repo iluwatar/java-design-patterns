77a534385cba9b357b4e73c57c42faebe74de0c3
Kamil Pietruszka <kamilos147@gmail.com>
2017-03-11 13:35:55 +0100

    fixed pmd violation

--- converter/src/main/java/com/iluwatar/converter/App.java
+++ converter/src/main/java/com/iluwatar/converter/App.java
@@ -48,7 +48,7 @@ public class App {
 
     UserDto dtoUser = new UserDto("John", "Doe", true, "whatever[at]wherever.com");
     User user = userConverter.convertFromDto(dtoUser);
-    UserDto dtoUserCopy = userConverter.convertFromEntity(user);
+    System.out.println("Entity converted from DTO:" + user);
 
     ArrayList<User> users = Lists.newArrayList(new User("Camile", "Tough", false, "124sad"),
         new User("Marti", "Luther", true, "42309fd"), new User("Kate", "Smith", true, "if0243"));
