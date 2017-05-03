1abd96a9c8f9a07bee0048b43d4b09f0391dc5f9
Aleksandar Dudukovic <lesansa00@gmail.com>
2017-05-03 22:09:47 +0200

    #567 checkstyle fix

--- marker/src/test/java/ThiefTest.java
+++ marker/src/test/java/ThiefTest.java
@@ -1,11 +1,10 @@
-/**
- * Created by Alexis on 02-May-17.
- */
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 
+/**
+ * Created by Alexis on 02-May-17.
+ */
 public class ThiefTest {
   @Test
   public void testGuard() {
