c746004f730c76ace78ff51a494d3e9c37b51a72
kapinuss <kapinuss@rambler.ru>
2017-05-18 19:13:45 +0300

    Update AdvancedSorceress.java

--- dependency-injection/src/main/java/com/iluwatar/dependency/injection/AdvancedSorceress.java
+++ dependency-injection/src/main/java/com/iluwatar/dependency/injection/AdvancedSorceress.java
@@ -3,17 +3,17 @@ package com.iluwatar.dependency.injection;
 /**
  * The MIT License
  * Copyright (c) 2014-2017 Ilkka Seppälä
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,14 +32,14 @@ package com.iluwatar.dependency.injection;
  */
 public class AdvancedSorceress implements Wizard {
 
-    private Tobacco tobacco;
+  private Tobacco tobacco;
 
-    public void setTobacco(Tobacco tobacco) {
-        this.tobacco = tobacco;
-    }
+  public void setTobacco(Tobacco tobacco) {
+    this.tobacco = tobacco;
+  }
 
-    @Override
-    public void smoke() {
-        tobacco.smoke(this);
-    }
+  @Override
+  public void smoke() {
+    tobacco.smoke(this);
+  }
 }
