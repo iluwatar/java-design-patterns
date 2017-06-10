139741df53f9bd206fefa082c633faf4f1c20ed3
Vladislav Simic <dovlasim@gmail.com>
2017-06-10 13:00:23 +0200

    Initial commit.

---
+++ sam/src/main/java/com/iluwatar/sam/App.java
@@ -0,0 +1,13 @@
+package com.iluwatar.sam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
