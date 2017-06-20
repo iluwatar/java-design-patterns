4e9988877ae73b20595da3fdbb7c0648f13ef746
Sabiq Ihab <sabiq.ihab@hotmail.fr>
2017-06-20 21:30:28 +0000

    Initial commit

---
+++ cqrs/src/main/java/com/iluwatar/cqrs/app/App.java
@@ -0,0 +1,10 @@
+package com.iluwatar.cqrs.app;
+
+public class App {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ cqrs/src/test/java/com/iluwatar/cqrs/AppTest.java
@@ -0,0 +1,14 @@
+package com.iluwatar.cqrs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
