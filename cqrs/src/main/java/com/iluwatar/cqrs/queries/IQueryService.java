8e25ec55bf85872414626c00d0597498a45aca42
Sabiq Ihab <sabiq.ihab@hotmail.fr>
2017-06-21 23:35:53 +0000

    change long with BigIntger and replace getBook()

--- cqrs/src/main/java/com/iluwatar/cqrs/queries/IQueryService.java
+++ cqrs/src/main/java/com/iluwatar/cqrs/queries/IQueryService.java
@@ -1,5 +1,6 @@
 package com.iluwatar.cqrs.queries;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import com.iluwatar.cqrs.dto.AuthorDTO;
@@ -9,12 +10,12 @@ public interface IQueryService {
 
   public abstract AuthorDTO getAuthorByUsername(String username);
 
-  public abstract Double getBookPrice(String title);
+  public abstract BookDTO getBook(String title);
 
   public abstract List<BookDTO> getAuthorBooks(String username);
 
-  public abstract long getAuthorBooksCount(String username);
+  public abstract BigInteger getAuthorBooksCount(String username);
 
-  public abstract long getAuthorsCount();
+  public abstract BigInteger getAuthorsCount();
 
 }
