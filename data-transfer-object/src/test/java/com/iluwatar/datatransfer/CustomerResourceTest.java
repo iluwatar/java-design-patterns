df9be7850490558e7e506961a24c4816765c37d4
Gopinath Langote <glangote@thoughtworks.com>
2017-08-11 16:17:51 +0530

    #348 - Data Tranfer Object : customer client request customer details to server at one shot.

--- data-transfer-object/src/test/java/com/iluwatar/datatransfer/CustomerResourceTest.java
+++ data-transfer-object/src/test/java/com/iluwatar/datatransfer/CustomerResourceTest.java
@@ -37,7 +37,7 @@ import static org.junit.Assert.assertEquals;
 public class CustomerResourceTest {
   @Test
   public void shouldGetAllCustomers() {
-    CustomerDto customer = new CustomerDto("1", "David", "Roy");
+    CustomerDto customer = new CustomerDto("1", "Melody", "Yates");
     List<CustomerDto> customers = new ArrayList<>();
     customers.add(customer);
 
@@ -47,26 +47,26 @@ public class CustomerResourceTest {
 
     assertEquals(allCustomers.size(), 1);
     assertEquals(allCustomers.get(0).getId(), "1");
-    assertEquals(allCustomers.get(0).getFirstName(), "David");
-    assertEquals(allCustomers.get(0).getLastName(), "Roy");
+    assertEquals(allCustomers.get(0).getFirstName(), "Melody");
+    assertEquals(allCustomers.get(0).getLastName(), "Yates");
   }
 
   @Test
   public void shouldSaveCustomer() {
-    CustomerDto customer = new CustomerDto("1", "David", "Roy");
+    CustomerDto customer = new CustomerDto("1", "Rita", "Reynolds");
     CustomerResource customerResource = new CustomerResource(new ArrayList<>());
 
     customerResource.save(customer);
 
     List<CustomerDto> allCustomers = customerResource.getAllCustomers();
     assertEquals(allCustomers.get(0).getId(), "1");
-    assertEquals(allCustomers.get(0).getFirstName(), "David");
-    assertEquals(allCustomers.get(0).getLastName(), "Roy");
+    assertEquals(allCustomers.get(0).getFirstName(), "Rita");
+    assertEquals(allCustomers.get(0).getLastName(), "Reynolds");
   }
 
   @Test
   public void shouldDeleteCustomer() {
-    CustomerDto customer = new CustomerDto("1", "David", "Roy");
+    CustomerDto customer = new CustomerDto("1", "Terry", "Nguyen");
     List<CustomerDto> customers = new ArrayList<>();
     customers.add(customer);
 
