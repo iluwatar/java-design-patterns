a59e0fa961ea3beeb201ed75208ee2ad96db41af
Gopinath Langote <gopinathlangote11@gmail.com>
2017-09-22 11:53:26 +0530

    #631 - Partial Response : Update copyright header.

--- partial-response/src/test/java/com/iluwatar/partialresponse/VideoResourceTest.java
+++ partial-response/src/test/java/com/iluwatar/partialresponse/VideoResourceTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 Gopinath Langote
+ * Copyright (c) 2014-2017 Gopinath Langote
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
