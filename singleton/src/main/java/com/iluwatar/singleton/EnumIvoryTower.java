dddfb52c70cf8cae27127e16c9aaa2bac9f9bc3a
Zafar Khaydarov <zafar@linux.com>
2018-01-23 00:07:11 -0500

    Update EnumIvoryTower.java

--- singleton/src/main/java/com/iluwatar/singleton/EnumIvoryTower.java
+++ singleton/src/main/java/com/iluwatar/singleton/EnumIvoryTower.java
@@ -24,6 +24,9 @@ package com.iluwatar.singleton;
 
 /**
  * Enum based singleton implementation. Effective Java 2nd Edition (Joshua Bloch) p. 18
+ *
+ * This implementation is thread safe, however adding any other method and its thread safety
+ * is developers responsibility.
  */
 public enum EnumIvoryTower {
 
