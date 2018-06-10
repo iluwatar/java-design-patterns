a250efa6012ebee72b41b410f460ce8e78e75420
nikhilbarar <nikhilbarar@gmail.com>
2018-06-11 01:56:32 +0530

    #509: Checkstyle Fixes

--- component-object/src/main/java/com/iluwatar/component/app/TodoApp.java
+++ component-object/src/main/java/com/iluwatar/component/app/TodoApp.java
@@ -1,12 +1,49 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.component.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Web development is shifting more and more towards reusable components. 
+ * Frameworks like React, Polymer, Angular, Ember and others provide various 
+ * component friendly abstractions to make front-end code-bases more maintainable. 
+ * So our web applications are now full of “widgets” that have same behavior. 
+ * We can use component various times on single web page or re-use it on various web pages.
+ * Component Object pattern provides an abstraction which covers functionality of 
+ * single component and reuse it across end-to-end tests. When we have various same 
+ * components on single web page, we are going to use various Component Objects of 
+ * same type per Page Object.
+ * 
+ */
 @SpringBootApplication
 public class TodoApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApp.class, args);
-	}
+  /**
+   * Program entry point
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(TodoApp.class, args);
+  }
 }
