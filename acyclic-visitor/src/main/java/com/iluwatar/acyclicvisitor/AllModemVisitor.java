6636fb151b2620102733c8689ec0f2916d83ad6c
Argyro-Sioziou <arguro_s@outlook.com>
2018-06-14 12:40:41 +0300

    Adding AllModemVisitor interface

--- acyclic-visitor/src/main/java/com/iluwatar/acyclicvisitor/AllModemVisitor.java
+++ acyclic-visitor/src/main/java/com/iluwatar/acyclicvisitor/AllModemVisitor.java
@@ -22,22 +22,6 @@
  */
 package com.iluwatar.acyclicvisitor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public interface AllModemVisitor extends ModemVisitor, ZoomVisitor, HayesVisitor{
 
-/**
- * ConfigureForDosVisitor class implements both zoom's and  hayes' visit method 
- * for Dos manufacturer
- */
-public class ConfigureForDosVisitor implements AllModemVisitor {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(ConfigureForDosVisitor.class);
-
-  public void visit(Hayes hayes) {
-    LOGGER.info(hayes + " used with Dos configurator.");
-  }
-
-  public void visit(Zoom zoom) {
-    LOGGER.info(zoom + " used with Dos configurator.");
-  }
 }
