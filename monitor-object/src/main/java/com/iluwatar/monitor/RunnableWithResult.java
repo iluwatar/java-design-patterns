5b9d12813a1d43aea634490acb6d054e4c08d759
nikhilbarar <nikhilbarar@gmail.com>
2018-08-02 09:31:23 +0530

    #466: Checkstyle Fixes

--- monitor-object/src/main/java/com/iluwatar/monitor/RunnableWithResult.java
+++ monitor-object/src/main/java/com/iluwatar/monitor/RunnableWithResult.java
@@ -2,6 +2,8 @@ package com.iluwatar.monitor;
 
 /**
  * The runnable interface to be executed by a thread which returns a result.
+ *  
+ * @param <T> Parameter to specify the return type
  */
 public interface RunnableWithResult<T> {
   public T run();
