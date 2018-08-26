9978741487bb09dfae9d68876b498a6467853ba1
nikhilbarar <nikhilbarar@gmail.com>
2018-08-26 23:18:56 +0530

    Checkstyle Fixes and MIT License Header

--- monitor-object/src/test/java/com/iluwatar/monitor/ThreadSignalTest.java
+++ monitor-object/src/test/java/com/iluwatar/monitor/ThreadSignalTest.java
@@ -1,9 +1,33 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.monitor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import com.iluwatar.monitor.examples.VoteInterface;
 import com.iluwatar.monitor.examples.VoteMonitor;
@@ -11,6 +35,7 @@ import com.iluwatar.monitor.examples.VoteMonitor;
 /**
  * Test Case for Thread Signaling.
  */
+@TestInstance(Lifecycle.PER_CLASS)
 public class ThreadSignalTest {
 
   private Voter v0;
