30fd089e8aa44727b06e12f536b25ed16bcc538b
AnaghaSasikumar <anagha.sasikumar@gmail.com>
2018-10-06 15:21:21 +0530

    adding retry with exponential backoff

--- retry/src/main/java/com/iluwatar/retry/App.java
+++ retry/src/main/java/com/iluwatar/retry/App.java
@@ -71,6 +71,7 @@ public final class App {
     noErrors();
     errorNoRetry();
     errorWithRetry();
+    errorWithRetryExponentialBackoff();
   }
 
   private static void noErrors() throws Exception {
@@ -102,4 +103,19 @@ public final class App {
         + "the result %s after a number of attempts %s", customerId, retry.attempts()
     ));
   }
+
+    private static void errorWithRetryExponentialBackoff() throws Exception {
+        final RetryWithExponentialBackoff<String> retry = new RetryWithExponentialBackoff<>(
+                new FindCustomer("123", new CustomerNotFoundException("not found")),
+                6,  //6 attempts
+                30000, //30 s max delay between attempts
+                e -> CustomerNotFoundException.class.isAssignableFrom(e.getClass())
+        );
+        op = retry;
+        final String customerId = op.perform();
+        LOG.info(String.format(
+                "However, retrying the operation while ignoring a recoverable error will eventually yield "
+                        + "the result %s after a number of attempts %s", customerId, retry.attempts()
+        ));
+    }
 }
