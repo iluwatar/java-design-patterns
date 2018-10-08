621260ab42dccabcb255c845eafb8ef0416ddcf5
AnaghaSasikumar <42939261+AnaghaSasikumar@users.noreply.github.com>
2018-10-08 19:12:07 +0530

    Update Point.java

--- spatial-partition/src/main/java/com/iluwatar/spatialpartition/Point.java
+++ spatial-partition/src/main/java/com/iluwatar/spatialpartition/Point.java
@@ -28,7 +28,8 @@ import java.util.Hashtable;
 
 /**
  * The abstract Point class which will be extended by any object in the field
- * whose location has to be kept track of.
+ * whose location has to be kept track of. Defined by x,y coordinates and an id
+ * for easy hashing into hashtable.
  * @param <T> T will be type subclass
  */
 
@@ -38,7 +39,22 @@ public abstract class Point<T> {
   public int y; 
   public int id;
 
+  /**
+   * defines how the object moves
+   */
   abstract void move();
+  
+  /**
+   * defines conditions for interacting with an object obj
+   * @param obj is another object on field which also extends Point
+   * @return whether the object can interact with the other or not
+   */
   abstract boolean touches(T obj);
+  
+  /**
+   * handling interactions/collisions with other objects
+   * @param pointsToCheck contains the objects which need to be checked
+   * @param allPoints contains hashtable of all points on field at this time
+   */
   abstract void handleCollision(ArrayList<Point> pointsToCheck, Hashtable<Integer, T> allPoints);
 }
