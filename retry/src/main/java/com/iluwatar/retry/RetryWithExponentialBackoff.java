bce48951173f25e2d7847895323e463dba7f1738
AnaghaSasikumar <anagha.sasikumar@gmail.com>
2018-10-10 23:19:44 +0530

    fixed error in retryexponentialbackoff.java

--- retry/src/main/java/com/iluwatar/retry/RetryWithExponentialBackoff.java
+++ retry/src/main/java/com/iluwatar/retry/RetryWithExponentialBackoff.java
@@ -41,7 +41,6 @@ import java.util.function.Predicate;
 public final class RetryWithExponentialBackoff<T> implements BusinessOperation<T> {
   private final BusinessOperation<T> op;
   private final int maxAttempts;
-  private final long delay;
   private final long maxDelay;
   private final AtomicInteger attempts;
   private final Predicate<Exception> test;
