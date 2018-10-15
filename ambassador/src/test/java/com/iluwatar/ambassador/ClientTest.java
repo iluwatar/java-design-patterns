339dacc01c4f4c63a43421c6814edf460f94cda1
Narendra Pathai <narendra.pathai@gmail.com>
2018-10-15 21:16:00 +0530

    Addressing review comments, Deleting unintentional file and used FAILURE constant in ClientTest as well

--- ambassador/src/test/java/com/iluwatar/ambassador/ClientTest.java
+++ ambassador/src/test/java/com/iluwatar/ambassador/ClientTest.java
@@ -37,6 +37,6 @@ public class ClientTest {
     Client client = new Client();
     long result = client.useService(10);
 
-    assertTrue(result == 100 || result == -1);
+    assertTrue(result == 100 || result == RemoteService.FAILURE);
   }
 }
