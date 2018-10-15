2b06158210e5914e84bece876ed0f042bdaf0182
Narendra Pathai <narendra.pathai@gmail.com>
2018-10-15 19:58:26 +0530

    1) Updated test cases to use Junit Assert method as compared to assert keyword 2) Proper testing of RemoteService using RandomProvider interface. Introduced RandomProvider interface so that randomness can be controlled from test cases. 3) For readability used constant for representing FAILURE

--- ambassador/src/main/java/com/iluwatar/ambassador/RemoteService.java
+++ ambassador/src/main/java/com/iluwatar/ambassador/RemoteService.java
@@ -22,6 +22,7 @@
  */
 package com.iluwatar.ambassador;
 
+import com.iluwatar.ambassador.util.RandomProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,9 +32,10 @@ import static java.lang.Thread.sleep;
  * A remote legacy application represented by a Singleton implementation.
  */
 public class RemoteService implements RemoteServiceInterface {
-
+  static final int THRESHOLD = 200;
   private static final Logger LOGGER = LoggerFactory.getLogger(RemoteService.class);
   private static RemoteService service = null;
+  private final RandomProvider randomProvider;
 
   static synchronized RemoteService getRemoteService() {
     if (service == null) {
@@ -42,24 +44,33 @@ public class RemoteService implements RemoteServiceInterface {
     return service;
   }
 
-  private RemoteService() {}
+  private RemoteService() {
+    this(Math::random);
+  }
 
+  /**
+   * This constuctor is used for testing purposes only.
+   */
+  RemoteService(RandomProvider randomProvider) {
+    this.randomProvider = randomProvider;
+  }
   /**
    * Remote function takes a value and multiplies it by 10 taking a random amount of time.
    * Will sometimes return -1. This imitates connectivity issues a client might have to account for.
    * @param value integer value to be multiplied.
-   * @return if waitTime is more than 200ms, it returns value * 10, otherwise -1.
+   * @return if waitTime is less than {@link RemoteService#THRESHOLD}, it returns value * 10,
+   *     otherwise {@link RemoteServiceInterface#FAILURE}.
    */
   @Override
   public long doRemoteFunction(int value) {
 
-    long waitTime = (long) Math.floor(Math.random() * 1000);
+    long waitTime = (long) Math.floor(randomProvider.random() * 1000);
 
     try {
       sleep(waitTime);
     } catch (InterruptedException e) {
       LOGGER.error("Thread sleep state interrupted", e);
     }
-    return waitTime >= 200 ? value * 10 : -1;
+    return waitTime <= THRESHOLD ? value * 10 : FAILURE;
   }
 }
--- ambassador/src/test/java/com/iluwatar/ambassador/ServiceAmbassadorTest.java
+++ ambassador/src/test/java/com/iluwatar/ambassador/ServiceAmbassadorTest.java
@@ -24,6 +24,8 @@ package com.iluwatar.ambassador;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Test for {@link ServiceAmbassador}
  */
@@ -32,6 +34,6 @@ public class ServiceAmbassadorTest {
   @Test
   public void test() {
     long result = new ServiceAmbassador().doRemoteFunction(10);
-    assert result == 100 || result == -1;
+    assertTrue(result == 100 || result == RemoteServiceInterface.FAILURE);
   }
 }
