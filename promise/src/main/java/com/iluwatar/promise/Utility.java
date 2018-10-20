70b4931ab88361f053131d29487ddcc7a99483ef
Narendra Pathai <narendra.pathai@gmail.com>
2018-10-20 18:01:41 +0530

    Removed unused import from Promise

--- promise/src/main/java/com/iluwatar/promise/Utility.java
+++ promise/src/main/java/com/iluwatar/promise/Utility.java
@@ -32,7 +32,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
