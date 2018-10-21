922fd62da6db31426bdd39e05f7b6cfefd432c3d
Narendra Pathai <narendra.pathai@gmail.com>
2018-10-21 23:44:07 +0530

    Resolves #643, test cases failed due to global state in CallsCount (#803)
    * Resolves #643, test cases failed due to presence of global state in CallsCount. Because AppTest was executed before B2BServiceTest, it scheduled 1 sec timer using ThrottleTimerImpl class. While resetting it used that global CallCount class reset() method, which reset all counters. So that causes thread safety issue because of unintended sharing of application state between test cases, which is not a good practice.
    
    * Updated class diagram png and added UCLS file

--- throttling/src/main/java/com/iluwatar/throttling/Tenant.java
+++ throttling/src/main/java/com/iluwatar/throttling/Tenant.java
@@ -38,13 +38,13 @@ public class Tenant {
    * @param allowedCallsPerSecond The number of calls allowed for a particular tenant.
    * @throws InvalidParameterException If number of calls is less than 0, throws exception.
    */
-  public Tenant(String name, int allowedCallsPerSecond) {
+  public Tenant(String name, int allowedCallsPerSecond, CallsCount callsCount) {
     if (allowedCallsPerSecond < 0) {
       throw new InvalidParameterException("Number of calls less than 0 not allowed");
     }
     this.name = name;
     this.allowedCallsPerSecond = allowedCallsPerSecond;
-    CallsCount.addTenant(name);
+    callsCount.addTenant(name);
   }
 
   public String getName() {
