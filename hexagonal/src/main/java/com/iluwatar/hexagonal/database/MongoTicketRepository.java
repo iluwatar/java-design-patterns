724bbb39e9842ec3b016e6973eb000a63cc3f869
Paweł Matejko <matejko.pawel@gmail.com>
2018-10-25 01:11:48 +0200

    Deleted unused imports

--- hexagonal/src/main/java/com/iluwatar/hexagonal/database/MongoTicketRepository.java
+++ hexagonal/src/main/java/com/iluwatar/hexagonal/database/MongoTicketRepository.java
@@ -34,7 +34,6 @@ import org.bson.Document;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
