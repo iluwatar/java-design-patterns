c65bd3bcb5f3f5cc01a678a5a808c898df42bbb9
AnaghaSasikumar <42939261+AnaghaSasikumar@users.noreply.github.com>
2018-10-28 16:55:59 +0530

    Update ArrayEquality.java

--- master-worker/src/test/java/com/iluwatar/masterworker/ArrayEquality.java
+++ master-worker/src/test/java/com/iluwatar/masterworker/ArrayEquality.java
@@ -1,49 +1,87 @@
-package com.iluwatar.masterworker;
-
 /**
- *Class ArrayEquality has method matricesSame to compare values of 2
- *matrices (int[][]).
+ * The MIT License
+ * Copyright (c) 2014-2016 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
-public class ArrayEquality {
- 
-  /**
-   * Method matricesSame compares 2 matrices @param m1 and @param m2
-   * and @return whether their values are equal (boolean).
-   */
-	
-  public static boolean matricesSame(int[][] m1, int[][] m2) {
-    if (m1.length != m2.length) {
-      return false;
-    } else {
-      boolean answer = false;
-      for (int i = 0; i < m1.length; i++) {
-        if (arraysSame(m1[i], m2[i])) {
-          answer = true;
+package com.iluwatar.masterworker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *Class ArrayInput extends abstract class Input and contains data
+ *of type int[][].
+ */
+
+public class ArrayInput extends Input<int[][]> {
+
+  public ArrayInput(int[][] data) {
+    super(data);
+  }
+  
+  static int[] makeDivisions(int[][] data, int num) {
+    int initialDivision = data.length / num; //equally dividing
+    int[] divisions = new int[num];
+    Arrays.fill(divisions, initialDivision);
+    if (initialDivision * num != data.length) {
+      int extra = data.length - initialDivision * num;
+      int l = 0;
+      //equally dividing extra among all parts
+      while (extra > 0) {
+        divisions[l] = divisions[l] + 1;
+        extra--;
+        if (l == num - 1) {
+          l = 0;
         } else {
-          answer = false;
-          break;
+          l++;
         }
       }
-      return answer;
     }
+    return divisions;
   }
 
-  static boolean arraysSame(int[] a1, int[] a2) {
-    //compares if 2 arrays have the same value
-    if (a1.length != a2.length) {
-      return false;
+  @Override
+  public ArrayList<Input> divideData(int num) {
+    if (this.data == null) {
+      return null;
     } else {
-      boolean answer = false;
-      for (int i = 0; i < a1.length; i++) {
-        if (a1[i] == a2[i]) {
-          answer = true;
+      int[] divisions = makeDivisions(this.data, num);
+      ArrayList<Input> result = new ArrayList<Input>(num);
+      int rowsDone = 0; //number of rows divided so far
+      for (int i = 0; i < num; i++) {
+        int rows = divisions[i];
+        if (rows != 0) {
+          int[][] divided = new int[rows][this.data[0].length];
+          for (int j = 0; j < rows; j++) {
+            divided[j] = this.data[rowsDone + j];
+          }
+          rowsDone += rows;
+          ArrayInput dividedInput = new ArrayInput(divided);
+          result.add(dividedInput);
         } else {
-          answer = false;
-          break;
+          break; //rest of divisions will also be 0
         }
       }
-      return answer;
-    }
+      return result;
+    }    
   }
+  
 }
