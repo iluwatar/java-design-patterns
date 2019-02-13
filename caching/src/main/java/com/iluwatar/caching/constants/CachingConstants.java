c74d1a50a3d0fed24608e51799c7a92f672b0d62
kanwar <kanwarpreet25@gmail.com>
2019-02-13 16:18:11 +0530

    508 : Sunar Qube Fixes Define a constant instead of duplicating this literal "user_accounts" 4 times.

---
+++ caching/src/main/java/com/iluwatar/caching/constants/CachingConstants.java
@@ -0,0 +1,10 @@
+package com.iluwatar.caching.constants;
+
+public class CachingConstants {
+
+	public static String USER_ACCOUNT="user_accounts";
+	public static String USER_ID="userID";
+	public static String USER_NAME="userName";
+	public static String ADD_INFO="additionalInfo";
+	
+}
