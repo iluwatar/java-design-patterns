f0dc1c7b58f7d5e80c03057b9e35e171b8fc095c
kanwar <kanwarpreet25@gmail.com>
2019-02-13 16:31:31 +0530

    508 : Sonar Qube FIxes Define a constant instead of duplicating this literal "username" 3 times.

---
+++ cqrs/src/main/java/com/iluwatar/cqrs/constants/AppConstants.java
@@ -0,0 +1,10 @@
+package com.iluwatar.cqrs.constants;
+
+public class AppConstants {
+
+	public static String E_Evans="eEvans";
+	public static String J_Bloch="jBloch";
+	public static String M_Fowler="mFowler";
+	public static String USER_NAME="username";
+	
+}
