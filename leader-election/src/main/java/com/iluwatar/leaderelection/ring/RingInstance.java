6b7dd6bf6cd4f44f036fa86be01c42a7549348fd
Azureyjt <azureyjt@gmail.com>
2019-09-12 10:39:21 +0800

    Add comments

--- leader-election/src/main/java/com/iluwatar/leaderelection/ring/RingInstance.java
+++ leader-election/src/main/java/com/iluwatar/leaderelection/ring/RingInstance.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014-2016 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.iluwatar.leaderelection.ring;
 
 import com.iluwatar.leaderelection.Instance;
@@ -10,6 +33,17 @@ import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
 
+/**
+ * Implementation with token ring alogirthm. The instances in the system are organized as a ring.
+ * Each instance should have a sequential id and the instance with smallest (or largest) id should
+ * be the initial leader. All the other instances send heartbeat message to leader periodically
+ * to check its health. If one certain instance finds the server done, it will send an election
+ * message to the next alive instance in the ring, which contains its own ID. Then the next instance
+ * add its ID into the message and pass it to the next. After all the alive instances' ID are add
+ * to the message, the message is send back to the first instance and it will choose the instance
+ * with smallest ID to be the new leader, and then send a leader message to other instances to
+ * inform the result.
+ */
 public class RingInstance implements Instance, Runnable {
 
     private MessageManager messageManager;
@@ -18,6 +52,9 @@ public class RingInstance implements Instance, Runnable {
     private int leaderID;
     private boolean alive;
 
+    /**
+     * Constructor of RingInstance.
+     */
     public RingInstance(MessageManager messageManager, int localID, int leaderID) {
         this.messageManager = messageManager;
         this.messageQueue = new ConcurrentLinkedQueue<>();
@@ -26,6 +63,9 @@ public class RingInstance implements Instance, Runnable {
         this.alive = true;
     }
 
+    /**
+     * The instance will execute the message in its message queue periodically once it is alive.
+     */
     @Override
     public void run() {
         while (true) {
@@ -36,6 +76,10 @@ public class RingInstance implements Instance, Runnable {
         }
     }
 
+    /**
+     * Message listening method of the instance.
+     * @param message
+     */
     @Override
     public void onMessage(Message message) {
         messageQueue.offer(message);
@@ -58,6 +102,11 @@ public class RingInstance implements Instance, Runnable {
         }
     }
 
+    /**
+     * Process the heartbeat invoke message. After receiving the message, the instance will send a heartbeat
+     * to leader to check its health. If alive, it will inform the next instance to do the heartbeat. If not,
+     * it will start the election process.
+     */
     private void handleHeartbeatMessage(Message message) {
         boolean isLeaderAlive = messageManager.sendHeartbeatMessage(this.leaderID);
         if (isLeaderAlive) {
@@ -69,6 +118,11 @@ public class RingInstance implements Instance, Runnable {
         }
     }
 
+    /**
+     * Process election message. If the local ID is contained in the ID list, the instance will select the
+     * alive instance with smallest ID to be the new leader, and send the leader inform message. If not,
+     * it will add its local ID to the list and send the message to the next instance in the ring.
+     */
     private void handleElectionMessage(Message message) {
         String content = message.getContent();
         System.out.println("Instance " + localID + " - Election Message: " + content);
@@ -86,6 +140,10 @@ public class RingInstance implements Instance, Runnable {
         }
     }
 
+    /**
+     * Process leader Message. The instance will set the leader ID to be the new one and send the message to
+     * the next instance until all the alive instance in the ring is informed.
+     */
     private void handleLeaderMessage(Message message) {
         int newLeaderID = Integer.valueOf(message.getContent());
         if (this.leaderID != newLeaderID) {
@@ -98,11 +156,18 @@ public class RingInstance implements Instance, Runnable {
         }
     }
 
+    /**
+     * Check whether the certain instnace is alive or not.
+     * @return {@code true} if the instance is alive
+     */
     @Override
     public boolean isAlive() {
         return alive;
     }
 
+    /**
+     * Set the status of instance.
+     */
     @Override
     public void setAlive(boolean alive) {
         this.alive = alive;
--- leader-election/src/main/java/com/iluwatar/leaderelection/ring/RingMessageManager.java
+++ leader-election/src/main/java/com/iluwatar/leaderelection/ring/RingMessageManager.java
@@ -1,11 +1,32 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014-2016 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.iluwatar.leaderelection.ring;
 
 import com.iluwatar.leaderelection.Instance;
 import com.iluwatar.leaderelection.Message;
 import com.iluwatar.leaderelection.MessageManager;
 import com.iluwatar.leaderelection.MessageType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -15,12 +36,20 @@ public class RingMessageManager implements MessageManager {
 
     private Map<Integer, Instance> instanceMap;
 
+    /**
+     * Constructor of RingMessageManager.
+     */
     public RingMessageManager() {}
 
     public RingMessageManager(Map<Integer, Instance> instanceMap) {
         this.instanceMap = instanceMap;
     }
 
+    /**
+     * Send heartbeat message to current leader instance to check the health.
+     * @param leaderID
+     * @return {@code true} if the leader is alive.
+     */
     @Override
     public boolean sendHeartbeatMessage(int leaderID) {
         Instance leaderInstance = instanceMap.get(leaderID);
@@ -28,6 +57,11 @@ public class RingMessageManager implements MessageManager {
         return alive;
     }
 
+    /**
+     * Send election message to the next instance.
+     * @param currentID
+     * @param content list contains all the IDs of instances which have received this election message.
+     */
     @Override
     public void sendElectionMessage(int currentID, String content) {
         Instance nextInstance = this.findNextInstance(currentID);
@@ -35,6 +69,11 @@ public class RingMessageManager implements MessageManager {
         nextInstance.onMessage(electionMessage);
     }
 
+    /**
+     * Send leader message to the next instance.
+     * @param currentID
+     * @param leaderID
+     */
     @Override
     public void sendLeaderMessage(int currentID, int leaderID) {
         Instance nextInstance = this.findNextInstance(currentID);
@@ -42,6 +81,10 @@ public class RingMessageManager implements MessageManager {
         nextInstance.onMessage(leaderMessage);
     }
 
+    /**
+     * Send heartbeat invoke message to the next instance.
+     * @param currentID
+     */
     @Override
     public void sendHeartbeatInvokeMessage(int currentID) {
         Instance nextInstance = this.findNextInstance(currentID);
@@ -49,6 +92,11 @@ public class RingMessageManager implements MessageManager {
         nextInstance.onMessage(heartbeatInvokeMessage);
     }
 
+    /**
+     * Find the next instance in the ring.
+     * @param currentID
+     * @return The next instance.
+     */
     private Instance findNextInstance(int currentID) {
         Instance result = null;
         List<Integer> candidateSet =
