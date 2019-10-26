f7a53f2d17f339ee8cefefc0140f67f768b84af0
GVSharma <gvsharma4@gmail.com>
2019-10-26 21:05:32 +0530

    service-layer local type inference changes (#1034)
    local variable type inference for service-layer design patterns

--- service-layer/src/main/java/com/iluwatar/servicelayer/spell/SpellDaoImpl.java
+++ service-layer/src/main/java/com/iluwatar/servicelayer/spell/SpellDaoImpl.java
@@ -41,9 +41,9 @@ public class SpellDaoImpl extends DaoBaseImpl<Spell> implements SpellDao {
   public Spell findByName(String name) {
     Transaction tx = null;
     Spell result = null;
-    try (Session session = getSessionFactory().openSession()) {
+    try (var session = getSessionFactory().openSession()) {
       tx = session.beginTransaction();
-      Criteria criteria = session.createCriteria(persistentClass);
+      var criteria = session.createCriteria(persistentClass);
       criteria.add(Restrictions.eq("name", name));
       result = (Spell) criteria.uniqueResult();
       tx.commit();
