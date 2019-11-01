1d4a7681e2d8699457c557fbab3d69d5f8aa105b
Arpit Jain <jarpit96@gmail.com>
2019-11-01 23:31:30 +0530

    Fix for Issue #549 : Add Fallbacks in Aggregator Service (#971)
    *  Fix for Issue##549
    
    Catch ClientProtocolException and Update Error Logs
    
    * Fix indentation, checkstyle errors
    
    * Fix for Issue #549
    Add fallbacks in Aggregator service when other microservices fail
    
    * Make ProductInventoryClientImpl return null instead of zero in case of failure

--- aggregator-microservices/aggregator-service/src/main/java/com/iluwatar/aggregator/microservices/ProductInventoryClientImpl.java
+++ aggregator-microservices/aggregator-service/src/main/java/com/iluwatar/aggregator/microservices/ProductInventoryClientImpl.java
@@ -42,8 +42,8 @@ public class ProductInventoryClientImpl implements ProductInventoryClient {
   private static final Logger LOGGER = LoggerFactory.getLogger(ProductInventoryClientImpl.class);
 
   @Override
-  public int getProductInventories() {
-    var response = "0";
+  public Integer getProductInventories() {
+    var response = "";
 
     var request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:51516/inventories")).build();
     var client = HttpClient.newHttpClient();
@@ -55,6 +55,10 @@ public class ProductInventoryClientImpl implements ProductInventoryClient {
     } catch (InterruptedException ie) {
       LOGGER.error("InterruptedException Occurred", ie);
     }
-    return Integer.parseInt(response);
+    if("".equalsIgnoreCase(response)) {
+        return null;
+    } else {
+        return Integer.parseInt(response);
+    }
   }
 }
