073a4ab101de54b404e03a1147fc377e8b04aedd
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-11-10 02:31:00 +0530

    Reduces checkstyle errors in reactor

--- reactor/src/main/java/com/iluwatar/reactor/framework/Dispatcher.java
+++ reactor/src/main/java/com/iluwatar/reactor/framework/Dispatcher.java
@@ -29,14 +29,12 @@ import java.nio.channels.SelectionKey;
  * Represents the event dispatching strategy. When {@link NioReactor} senses any event on the
  * registered {@link AbstractNioChannel}s then it de-multiplexes the event type, read or write or
  * connect, and then calls the {@link Dispatcher} to dispatch the read events. This decouples the
- * I/O processing from application specific processing. <br>
- * Dispatcher should call the {@link ChannelHandler} associated with the channel on which event
- * occurred.
- * 
- * <p>
- * The application can customize the way in which event is dispatched such as using the reactor
+ * I/O processing from application specific processing. <br> Dispatcher should call the {@link
+ * ChannelHandler} associated with the channel on which event occurred.
+ *
+ * <p>The application can customize the way in which event is dispatched such as using the reactor
  * thread to dispatch event to channels or use a worker pool to do the non I/O processing.
- * 
+ *
  * @see SameThreadDispatcher
  * @see ThreadPoolDispatcher
  */
@@ -45,19 +43,18 @@ public interface Dispatcher {
    * This hook method is called when read event occurs on particular channel. The data read is
    * provided in <code>readObject</code>. The implementation should dispatch this read event to the
    * associated {@link ChannelHandler} of <code>channel</code>.
-   * 
-   * <p>
-   * The type of <code>readObject</code> depends on the channel on which data was received.
-   * 
-   * @param channel on which read event occurred
+   *
+   * <p>The type of <code>readObject</code> depends on the channel on which data was received.
+   *
+   * @param channel    on which read event occurred
    * @param readObject object read by channel
-   * @param key on which event occurred
+   * @param key        on which event occurred
    */
   void onChannelReadEvent(AbstractNioChannel channel, Object readObject, SelectionKey key);
 
   /**
    * Stops dispatching events and cleans up any acquired resources such as threads.
-   * 
+   *
    * @throws InterruptedException if interrupted while stopping dispatcher.
    */
   void stop() throws InterruptedException;
--- reactor/src/main/java/com/iluwatar/reactor/framework/NioServerSocketChannel.java
+++ reactor/src/main/java/com/iluwatar/reactor/framework/NioServerSocketChannel.java
@@ -23,9 +23,6 @@
 
 package com.iluwatar.reactor.framework;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -33,10 +30,12 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * A wrapper over {@link NioServerSocketChannel} which can read and write data on a
- * {@link SocketChannel}.
+ * A wrapper over {@link NioServerSocketChannel} which can read and write data on a {@link
+ * SocketChannel}.
  */
 public class NioServerSocketChannel extends AbstractNioChannel {
 
@@ -47,11 +46,11 @@ public class NioServerSocketChannel extends AbstractNioChannel {
   /**
    * Creates a {@link ServerSocketChannel} which will bind at provided port and use
    * <code>handler</code> to handle incoming events on this channel.
-   * <p>
-   * Note the constructor does not bind the socket, {@link #bind()} method should be called for
+   *
+   * <p>Note the constructor does not bind the socket, {@link #bind()} method should be called for
    * binding the socket.
-   * 
-   * @param port the port on which channel will be bound to accept incoming connection requests.
+   *
+   * @param port    the port on which channel will be bound to accept incoming connection requests.
    * @param handler the handler that will handle incoming requests on this channel.
    * @throws IOException if any I/O error occurs.
    */
@@ -68,6 +67,8 @@ public class NioServerSocketChannel extends AbstractNioChannel {
   }
 
   /**
+   * Get server socket channel.
+   *
    * @return the underlying {@link ServerSocketChannel}.
    */
   @Override
@@ -94,7 +95,7 @@ public class NioServerSocketChannel extends AbstractNioChannel {
 
   /**
    * Binds TCP socket on the provided <code>port</code>.
-   * 
+   *
    * @throws IOException if any I/O error occurs.
    */
   @Override
