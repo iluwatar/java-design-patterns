7beb2dcf380ad27a6e1a21f60375e971a979e19e
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-11-10 02:53:16 +0530

    Reduces checkstyle errors in twin

--- twin/src/main/java/com/iluwatar/twin/App.java
+++ twin/src/main/java/com/iluwatar/twin/App.java
@@ -26,17 +26,17 @@ package com.iluwatar.twin;
 /**
  * Twin pattern is a design pattern which provides a standard solution to simulate multiple
  * inheritance in java.
- * <p>
- * In this example, the essence of the Twin pattern is the {@link BallItem} class and
- * {@link BallThread} class represent the twin objects to coordinate with each other(via the twin
+ *
+ * <p>In this example, the essence of the Twin pattern is the {@link BallItem} class and {@link
+ * BallThread} class represent the twin objects to coordinate with each other(via the twin
  * reference) like a single class inheriting from {@link GameItem} and {@link Thread}.
  */
 
 public class App {
 
   /**
-   * Program entry point
-   * 
+   * Program entry point.
+   *
    * @param args command line args
    */
   public static void main(String[] args) throws Exception {
