02b486a2a253ea28eb7d1ddaee7b1878d7f55154
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-11-09 22:16:54 +0530

    Reduces checkstyle errors in bridge

--- bridge/src/main/java/com/iluwatar/bridge/Enchantment.java
+++ bridge/src/main/java/com/iluwatar/bridge/Enchantment.java
@@ -24,9 +24,7 @@
 package com.iluwatar.bridge;
 
 /**
- * 
- * Enchantment
- * 
+ * Enchantment.
  */
 public interface Enchantment {
 
--- bridge/src/main/java/com/iluwatar/bridge/FlyingEnchantment.java
+++ bridge/src/main/java/com/iluwatar/bridge/FlyingEnchantment.java
@@ -27,9 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 
- * FlyingEnchantment
- *
+ * FlyingEnchantment.
  */
 public class FlyingEnchantment implements Enchantment {
 
