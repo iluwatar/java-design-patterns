0031a0bc7499bb2ed840cabeea4bd0f94d840e2e
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-11-10 01:52:40 +0530

    Reduces checkstyle errors in delegation

--- delegation/src/main/java/com/iluwatar/delegation/simple/printers/CanonPrinter.java
+++ delegation/src/main/java/com/iluwatar/delegation/simple/printers/CanonPrinter.java
@@ -28,8 +28,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Specialised Implementation of {@link Printer} for a Canon Printer, in
- * this case the message to be printed is appended to "Canon Printer : "
+ * Specialised Implementation of {@link Printer} for a Canon Printer, in this case the message to be
+ * printed is appended to "Canon Printer : ".
  *
  * @see Printer
  */
