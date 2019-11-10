c66fd0d1bb9a1bda90171ac59a00d4221e129f79
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-11-10 11:53:38 +0530

    Moves builder pattern  to java 11

--- builder/src/main/java/com/iluwatar/builder/App.java
+++ builder/src/main/java/com/iluwatar/builder/App.java
@@ -59,20 +59,22 @@ public class App {
    */
   public static void main(String[] args) {
 
-    Hero mage =
-        new Hero.Builder(Profession.MAGE, "Riobard").withHairColor(HairColor.BLACK)
-            .withWeapon(Weapon.DAGGER).build();
+    var mage = new Hero.Builder(Profession.MAGE, "Riobard")
+        .withHairColor(HairColor.BLACK)
+        .withWeapon(Weapon.DAGGER)
+        .build();
     LOGGER.info(mage.toString());
 
-    Hero warrior =
-        new Hero.Builder(Profession.WARRIOR, "Amberjill").withHairColor(HairColor.BLOND)
-            .withHairType(HairType.LONG_CURLY).withArmor(Armor.CHAIN_MAIL).withWeapon(Weapon.SWORD)
-            .build();
+    var warrior = new Hero.Builder(Profession.WARRIOR, "Amberjill")
+        .withHairColor(HairColor.BLOND)
+        .withHairType(HairType.LONG_CURLY).withArmor(Armor.CHAIN_MAIL).withWeapon(Weapon.SWORD)
+        .build();
     LOGGER.info(warrior.toString());
 
-    Hero thief =
-        new Hero.Builder(Profession.THIEF, "Desmond").withHairType(HairType.BALD)
-            .withWeapon(Weapon.BOW).build();
+    var thief = new Hero.Builder(Profession.THIEF, "Desmond")
+        .withHairType(HairType.BALD)
+        .withWeapon(Weapon.BOW)
+        .build();
     LOGGER.info(thief.toString());
 
   }
