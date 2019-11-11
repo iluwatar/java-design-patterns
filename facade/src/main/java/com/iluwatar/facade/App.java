f670c9b79e658f9959af8523789ae067a7e36362
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-11-11 08:38:40 +0530

    Reduces checkstyle errors in facade

--- facade/src/main/java/com/iluwatar/facade/App.java
+++ facade/src/main/java/com/iluwatar/facade/App.java
@@ -24,23 +24,21 @@
 package com.iluwatar.facade;
 
 /**
- * 
  * The Facade design pattern is often used when a system is very complex or difficult to understand
  * because the system has a large number of interdependent classes or its source code is
  * unavailable. This pattern hides the complexities of the larger system and provides a simpler
  * interface to the client. It typically involves a single wrapper class which contains a set of
  * members required by client. These members access the system on behalf of the facade client and
  * hide the implementation details.
- * <p>
- * In this example the Facade is ({@link DwarvenGoldmineFacade}) and it provides a simpler interface
- * to the goldmine subsystem.
- * 
+ *
+ * <p>In this example the Facade is ({@link DwarvenGoldmineFacade}) and it provides a simpler
+ * interface to the goldmine subsystem.
  */
 public class App {
 
   /**
-   * Program entry point
-   * 
+   * Program entry point.
+   *
    * @param args command line args
    */
   public static void main(String[] args) {
