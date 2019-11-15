1b5d3b2ffca158c981780a84806e5a97d475608d
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-11-15 12:12:11 +0530

    Reduces checkstyle errors in mute-idiom

--- mute-idiom/src/main/java/com/iluwatar/mute/Resource.java
+++ mute-idiom/src/main/java/com/iluwatar/mute/Resource.java
@@ -26,9 +26,8 @@ package com.iluwatar.mute;
 import java.io.Closeable;
 
 /**
- * Represents any resource that the application might acquire and that must be closed
- * after it is utilized. Example of such resources can be a database connection, open
- * files, sockets. 
+ * Represents any resource that the application might acquire and that must be closed after it is
+ * utilized. Example of such resources can be a database connection, open files, sockets.
  */
 public interface Resource extends Closeable {
 
