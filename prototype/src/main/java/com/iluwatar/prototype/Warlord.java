515b7e713497813b854932a900a42d5262f1e3a0
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2019-11-19 08:34:24 +0100

    Changes the implementation of the prototype pattern (#1103)
    * Changes the implementation of the prototype pattern
    
    * Fixes the checkstyle warnings
    
    * Fixes additional checkstyle warnings

--- prototype/src/main/java/com/iluwatar/prototype/Warlord.java
+++ prototype/src/main/java/com/iluwatar/prototype/Warlord.java
@@ -26,9 +26,24 @@ package com.iluwatar.prototype;
 /**
  * Warlord.
  */
-public abstract class Warlord extends Prototype {
+public abstract class Warlord implements Prototype {
+
+  public Warlord() { }
+
+  public Warlord(Warlord source) { }
 
   @Override
-  public abstract Warlord copy() throws CloneNotSupportedException;
+  public abstract Warlord copy();
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    return getClass() == obj.getClass();
+  }
 
 }
