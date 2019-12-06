959bab8b9cc4b7df8745f39fa1a95d3a44ddfa69
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-12-06 17:26:32 +0530

    Moves eip-message-channel to Java 11

--- eip-message-channel/src/test/java/com/iluwatar/eip/message/channel/AppTest.java
+++ eip-message-channel/src/test/java/com/iluwatar/eip/message/channel/AppTest.java
@@ -26,15 +26,12 @@ package com.iluwatar.eip.message.channel;
 import org.junit.jupiter.api.Test;
 
 /**
- * 
  * Application test
- *
  */
 public class AppTest {
 
   @Test
   public void test() throws Exception {
-    String[] args = {};
-    App.main(args);
+    App.main(new String[]{});
   }
 }
