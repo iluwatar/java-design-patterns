7a7282f7999990713f038f24d65a14cc3945c5d2
Zhang WH <npczwh@163.com>
2020-02-25 22:01:10 +0800

    Java 11 support: lazy-loading pattern

--- lazy-loading/src/main/java/com/iluwatar/lazy/loading/App.java
+++ lazy-loading/src/main/java/com/iluwatar/lazy/loading/App.java
@@ -46,18 +46,18 @@ public class App {
   public static void main(String[] args) {
 
     // Simple lazy loader - not thread safe
-    HolderNaive holderNaive = new HolderNaive();
-    Heavy heavy = holderNaive.getHeavy();
+    var holderNaive = new HolderNaive();
+    var heavy = holderNaive.getHeavy();
     LOGGER.info("heavy={}", heavy);
 
     // Thread safe lazy loader, but with heavy synchronization on each access
-    HolderThreadSafe holderThreadSafe = new HolderThreadSafe();
-    Heavy another = holderThreadSafe.getHeavy();
+    var holderThreadSafe = new HolderThreadSafe();
+    var another = holderThreadSafe.getHeavy();
     LOGGER.info("another={}", another);
 
     // The most efficient lazy loader utilizing Java 8 features
-    Java8Holder java8Holder = new Java8Holder();
-    Heavy next = java8Holder.getHeavy();
+    var java8Holder = new Java8Holder();
+    var next = java8Holder.getHeavy();
     LOGGER.info("next={}", next);
   }
 }
