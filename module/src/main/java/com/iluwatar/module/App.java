f1b27ef5c78eb379ca5f2ef539cca8c533ddc0a8
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2020-04-12 22:38:00 +0000

    Java 11 migraiton: module

--- module/src/main/java/com/iluwatar/module/App.java
+++ module/src/main/java/com/iluwatar/module/App.java
@@ -65,10 +65,8 @@ public class App {
 
   /**
    * Following method is main executor.
-   *
-   * @param args for providing default program arguments
    */
-  public static void execute(final String... args) {
+  public static void execute() {
 
     /* Send logs on file system */
     fileLoggerModule.printString("Message");
@@ -88,7 +86,7 @@ public class App {
    */
   public static void main(final String... args) throws FileNotFoundException {
     prepare();
-    execute(args);
+    execute();
     unprepare();
   }
 }
