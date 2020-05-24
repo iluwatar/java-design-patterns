35ad9737300fb38871a4ce1b44639f4e3df9a652
YanchaoMiao <11710204@mail.sustech.edu.cn>
2020-05-24 19:09:13 +0800

    Update App.java

--- component/src/main/java/com/iluwatar/component/App.java
+++ component/src/main/java/com/iluwatar/component/App.java
@@ -37,7 +37,7 @@ import java.util.ArrayList;
  * components, in another way, multiple components can construct a object.
  * Here is a demo using component pattern to solve a game-like problem.
  * A person named Bjorn who has three components: input;physics;graphics
- * These three components with a common game object can construct our protagonist:Bjorn, also can
+ * These three component with a common game object can construct our protagonist:Bjorn, also can
  * construct other objects like dog or cat if you want to write a real game.
  */
 
@@ -46,11 +46,11 @@ public class App {
    * Launcher for this demo design pattern
    */
   public static void main(String[] args) {
-    ArrayList<Component> arrayList = new ArrayList<>();
+    var arrayList = new ArrayList<Component>();
     arrayList.add(new BjornInputComponent());
     arrayList.add(new BjornPhysicsComponent());
     arrayList.add(new BjornGraphicsComponent());
-    GameObject gameObject = new GameObject(arrayList);
+    var gameObject = new GameObject(arrayList);
     gameObject.update();
   }
 }
