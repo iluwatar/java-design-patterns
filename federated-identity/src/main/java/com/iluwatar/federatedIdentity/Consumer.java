a98e6aadd10ab1439c47ae774097f15f3aad6a94
jasciiz <lh_560@163.com>
2020-05-31 12:57:46 +0800

    update

--- federated-identity/src/main/java/com/iluwatar/federatedIdentity/Consumer.java
+++ federated-identity/src/main/java/com/iluwatar/federatedIdentity/Consumer.java
@@ -1,176 +1,175 @@
 package com.iluwater.federatedIdentity;
-import org.apache.http.client.fluent.Request;
 
 import java.io.IOException;
-import java.net.ConnectException;
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import org.apache.http.client.fluent.Request;
 
 /**
  * act as consumer.
- * <p>
- * @author jasciiz
+ *
+ * <p>@author jasciiz
  */
 public class Consumer {
-    String name;
-    int age;
-    Date data;
-    String others;
-    static String IdP = "http://localhost:7001/IdP";
-    static String Server = "http://localhost:7003/Server";
-    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    boolean isRegister;
-    boolean isLand;
-    String claim;
+  String name;
+  int age;
+  Date data;
+  String others;
+  static String IdP = "http://localhost:7001/IdP";
+  static String Server = "http://localhost:7003/Server";
+  static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+  boolean isRegister;
+  boolean isLand;
+  String claim;
 
-    /**
-     * construct with parameters.
-     * <p>
-     * @param name the name of consumer
-     * @param age the age of consumer
-     * @param data the date of birth of consumer
-     * @param others the other information of consumer
-     * @throws IOException throw
-     *  when input name with "//".
-     */
-    public Consumer(String name, int age, String data, String others) throws IOException {
-        if(name.contains("//")){
-            throw new IOException("wrong name! Do not contain \"//\"!");
-        }
-        this.name = name;
-        try{
-            this.data = sdf.parse(data);
-        }catch (ParseException e){
-            e.printStackTrace();
-        }
-        this.others = others;
-        this.age = age;
-        this.isRegister = false;
-        this.claim = null;
-        this.isLand = false;
+  /**
+   * construct with parameters.
+   *
+   * <p>@param name the name of consumer
+   * @param age the age of consumer
+   * @param data the date of birth of consumer
+   * @param others the other information of consumer
+   * @throws IOException throw
+   *  when input name with "//".
+   */
+  public Consumer(String name, int age, String data, String others) throws IOException {
+    if(name.contains("//")){
+      throw new IOException("wrong name! Do not contain \"//\"!");
+    }
+    this.name = name;
+    try{
+      this.data = sdf.parse(data);
+    }catch (ParseException e){
+      e.printStackTrace();
+    }
+    this.others = others;
+    this.age = age;
+    this.isRegister = false;
+    this.claim = null;
+    this.isLand = false;
+  }
+
+  /**
+   * construct without parameter.
+   * <p>
+   */
+  public Consumer(){
+    this.name = "iluwater";
+    this.age = 2020-1990;
+    this.data = new Date(1990-1900, Calendar.DECEMBER,8);
+    this.others = "hello world";
+    this.isRegister = false;
+    this.claim = null;
+    this.isLand = false;
+  }
+
+  /**
+   * get IdP link.
+   * <p>
+   * @return
+   */
+  public String getIdP() {
+    return IdP;
+  }
+
+  /**
+   * set new IdP link.
+   * <p>
+   * @param IdP the new link
+   */
+  public void setIdP(String IdP) {
+    Consumer.IdP = IdP;
+  }
+
+  /**
+   * resister self in IdP.
+   * <P>
+   * @exception IOException throw
+   * when no get response from IdP.
+   */
+  public void register() {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append(this.name+"//");
+    stringBuilder.append(this.age+"//");
+    stringBuilder.append(sdf.format(this.data)+"//");
+    stringBuilder.append(this.others);
+    byte[] postBytes = stringBuilder.toString().getBytes(StandardCharsets.UTF_8);
+    try {
+      String responseString = Request.Post(IdP+"/register").bodyByteArray(postBytes).execute().returnContent().asString();
+      if(responseString.equals("success")){
+        isRegister = true;
+      }
+    }catch (IOException e){
+      e.printStackTrace();
     }
 
-    /**
-     * construct without parameter.
-     * <p>
-     */
-    public Consumer(){
-        this.name = "iluwater";
-        this.age = 2020-1990;
-        this.data = new Date(1990-1900, Calendar.DECEMBER,8);
-        this.others = "hello world";
-        this.isRegister = false;
-        this.claim = null;
-        this.isLand = false;
-    }
+  }
 
-    /**
-     * get IdP link.
-     * <p>
-     * @return
-     */
-    public String getIdP() {
-        return IdP;
-    }
-
-    /**
-     * set new IdP link.
-     * <p>
-     * @param IdP the new link
-     */
-    public void setIdP(String IdP) {
-        Consumer.IdP = IdP;
-    }
-
-    /**
-     * resister self in IdP.
-     * <P>
-     * @exception IOException throw
-     * when no get response from IdP.
-     */
-    public void register() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(this.name+"//");
-        stringBuilder.append(this.age+"//");
-        stringBuilder.append(sdf.format(this.data)+"//");
-        stringBuilder.append(this.others);
-        byte[] postBytes = stringBuilder.toString().getBytes(StandardCharsets.UTF_8);
-        try {
-            String responseString = Request.Post(IdP+"/register").bodyByteArray(postBytes).execute().returnContent().asString();
-            if(responseString.equals("success")){
-                isRegister = true;
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
-    }
-
-    /**
-     * get claim from IdP.
-     * <P>
-     * @return if get claim.
-     * @exception IOException throw
-     * when no get response from IdP.
-     */
-    public boolean getClaim(){
-        try {
-            byte[] postBytes = this.name.getBytes(StandardCharsets.UTF_8);
-            this.claim = Request.Post(IdP+"/getClaim").bodyByteArray(postBytes).execute().returnContent().asString();
-            if(this.claim.equals("fail")){
-                return false;
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    /**
-     * log in server.
-     * <p>
-     * @return if log in server
-     * @exception IOException throw
-     * when no get response from server.
-     */
-    public boolean logIn(){
-        if(!this.isRegister || this.claim == null) return false;
-        try {
-            byte[] postBytes =this.claim.getBytes(StandardCharsets.UTF_8);
-            String response = Request.Post(Server+"/logIn").bodyByteArray(postBytes).execute().returnContent().asString();
-            if(response.equals("success")){
-                this.isLand = true;
-                return true;
-            }else {
-                return false;
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+  /**
+   * get claim from IdP.
+   * <P>
+   * @return if get claim.
+   * @exception IOException throw
+   * when no get response from IdP.
+   */
+  public boolean getClaim(){
+    try {
+      byte[] postBytes = this.name.getBytes(StandardCharsets.UTF_8);
+      this.claim = Request.Post(IdP+"/getClaim").bodyByteArray(postBytes).execute().returnContent().asString();
+      if(this.claim.equals("fail")){
         return false;
+      }
+    }catch (IOException e){
+      e.printStackTrace();
     }
+    return true;
+  }
 
-    /**
-     * using server function.
-     * <p>
-     * @exception IOException throw
-     * when no get response from server.
-     */
-    public String run(){
-        if(!isLand){
-            return "no log in";
-        }
-        try {
-            String response = Request.Get(Server+"/function").execute().returnContent().asString();
-            System.out.println(response);
-            return response;
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        return "error";
+  /**
+   * log in server.
+   * <p>
+   * @return if log in server
+   * @exception IOException throw
+   * when no get response from server.
+   */
+  public boolean logIn(){
+    if(!this.isRegister || this.claim == null) return false;
+    try {
+      byte[] postBytes =this.claim.getBytes(StandardCharsets.UTF_8);
+      String response = Request.Post(Server+"/logIn").bodyByteArray(postBytes).execute().returnContent().asString();
+      if(response.equals("success")){
+        this.isLand = true;
+        return true;
+      }else {
+        return false;
+      }
+    }catch (IOException e){
+      e.printStackTrace();
     }
+    return false;
+  }
+
+  /**
+   * using server function.
+   * <p>
+   * @exception IOException throw
+   * when no get response from server.
+   */
+  public String run(){
+    if(!isLand){
+      return "no log in";
+    }
+    try {
+      String response = Request.Get(Server+"/function").execute().returnContent().asString();
+      System.out.println(response);
+      return response;
+    }catch (IOException e){
+      e.printStackTrace();
+    }
+    return "error";
+  }
 
 }
--- federated-identity/src/main/java/com/iluwatar/federatedIdentity/IdP.java
+++ federated-identity/src/main/java/com/iluwatar/federatedIdentity/IdP.java
@@ -5,99 +5,98 @@ import io.javalin.plugin.openapi.OpenApiOptions;
 import io.javalin.plugin.openapi.OpenApiPlugin;
 import io.javalin.plugin.openapi.ui.SwaggerOptions;
 import io.swagger.v3.oas.models.info.Info;
-
-
 import java.util.HashMap;
 
 /**
  * provide authentication.
- * <p>
- * @author jasciiz
+ *
+ *<p>@author jasciiz
  */
 public class IdP {
-    String name;
-    HashMap <String, String> hashMap;
-    HashMap <String ,Consumer> consumerList;
-    Javalin app;
-    int id;
+  String name;
+  HashMap<String, String> hashMap;
+  HashMap<String, Consumer> consumerList;
+  Javalin app;
+  int id;
 
-    /**
-     * construct with IdP name.
-     * <P>
-     * @param name the name of IdP.
-     */
-    public IdP(String name) {
-        this.name = name;
-        this.hashMap = new HashMap<String, String>();
-        this.app = Javalin.create(config -> {
-            config.registerPlugin(getConfiguredOpenApiPlugin());
-        }).start(7001);
-        this.id = 0;
-        this.hashMap = new HashMap<>();
-        this.consumerList = new HashMap<>();
-    }
+  /**
+   * construct with IdP name.
+   *
+   * <P>@param name the name of IdP.
+   */
+  public IdP(String name) {
+    this.name = name;
+    this.hashMap = new HashMap<String, String>();
+    this.app = Javalin.create(config -> {
+      config.registerPlugin(getConfiguredOpenApiPlugin());
+    }).start(7001);
+    this.id = 0;
+    this.hashMap = new HashMap<>();
+    this.consumerList = new HashMap<>();
+  }
 
-    /**
-     * construct without parameter.
-     * <p>
-     */
-    public IdP() {
-        this.name = "IdP1";
-        this.hashMap = new HashMap<String, String>();
-        this.app = Javalin.create(config -> {
-            config.registerPlugin(getConfiguredOpenApiPlugin());
-        }).start(7001);
-        this.id = 0;
-        this.hashMap = new HashMap<>();
-        this.consumerList = new HashMap<>();
-    }
+  /**
+   * construct without parameter.
+   *
+   */
+  public IdP() {
+    this.name = "IdP1";
+    this.hashMap = new HashMap<String, String>();
+    this.app = Javalin.create(config -> {
+      config.registerPlugin(getConfiguredOpenApiPlugin());
+    }).start(7001);
+    this.id = 0;
+    this.hashMap = new HashMap<>();
+    this.consumerList = new HashMap<>();
+  }
 
-    /**
-     * listen all http request.
-     * <p>
-     * accept register, getClaim, getInfo requests.
-     */
-    public void listener(){
-        this.app.post("/IdP/register", ctx -> {
-            ctx.res.setCharacterEncoding("UTF-8");
-            String user = ctx.body();
-            String[] information = user.split("//");
-            if(hashMap.containsKey(information[0])){
-                ctx.result("Please input another name.");
-                return;
-            }
-            Consumer consumer = new Consumer(information[0],Integer.parseInt(information[1]),information[2],information[3]);
-            consumerList.put(information[0], consumer);
-            hashMap.put(information[0],String.valueOf(this.id));
-            ctx.result("success");
-        });
+  /**
+   * listen all http request.
+   *
+   * <p>accept register, getClaim, getInfo requests.
+   */
+  public void listener() {
+    this.app.post("/IdP/register", ctx -> {
+      ctx.res.setCharacterEncoding("UTF-8");
+      String user = ctx.body();
+      String[] information = user.split("//");
+      if (hashMap.containsKey(information[0])) {
+        ctx.result("Please input another name.");
+        return;
+      }
+      Consumer consumer = new Consumer(information[0], Integer.parseInt(information[1]),
+          information[2], information[3]);
+      consumerList.put(information[0], consumer);
+      hashMap.put(information[0], String.valueOf(this.id));
+      ctx.result("success");
+    });
 
-        this.app.post("/IdP/getClaim", ctx -> {
-            ctx.res.setCharacterEncoding("UTF-8");
-            String name = ctx.body();
-            if(!hashMap.containsKey(name)){
-                ctx.result("fail");
-                return;
-            }
-            String ID = hashMap.get(name);
-            ctx.result(this.name+"//"+ID);
-        } );
+    this.app.post("/IdP/getClaim", ctx -> {
+      ctx.res.setCharacterEncoding("UTF-8");
+      String name = ctx.body();
+      if (!hashMap.containsKey(name)) {
+        ctx.result("fail");
+        return;
+      }
+      String id = hashMap.get(name);
+      ctx.result(this.name + "//" + id);
+    });
 
-        this.app.get("/IdP/getInfo", ctx -> ctx.result(this.name));
+    this.app.get("/IdP/getInfo", ctx -> ctx.result(this.name));
 
-    }
+  }
 
-    /**
-     * make configure of Javalin link.
-     * <p>
-     * @return OpenApiPlugin
-     */
-    private OpenApiPlugin getConfiguredOpenApiPlugin() {
-        Info info = new Info().version("1.0").description("RESTful Corpus Platform API");
-        OpenApiOptions options = new OpenApiOptions(info)
-                .activateAnnotationScanningFor("com.iluwater.dederatedIdentity.IdP")
-                .path("/swagger-docs")
-                .swagger(new SwaggerOptions("/swagger-ui"));
-        return new OpenApiPlugin(options);
-    }
+  /**
+   * make configure of Javalin link.
+   *
+   * <p>@return OpenApiPlugin
+   */
+  private OpenApiPlugin getConfiguredOpenApiPlugin() {
+    Info info = new Info().version("1.0").description("RESTful Corpus Platform API");
+    OpenApiOptions options = new OpenApiOptions(info)
+        .activateAnnotationScanningFor("com.iluwater.dederatedIdentity.IdP")
+        .path("/swagger-docs")
+        .swagger(new SwaggerOptions("/swagger-ui"));
+    return new OpenApiPlugin(options);
+  }
 }
