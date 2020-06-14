68e1ca83e84d63a30983506c99e6c91a79344383
Ilkka Seppälä <ilkka.seppala@gofore.com>
2020-06-14 22:21:45 +0300

    commit

--- ambassador/src/main/java/com/iluwatar/ambassador/App.java
+++ ambassador/src/main/java/com/iluwatar/ambassador/App.java
@@ -45,9 +45,11 @@ public class App {
    * Entry point.
    */
   public static void main(String[] args) {
+
     var host1 = new Client();
     var host2 = new Client();
     host1.useService(12);
     host2.useService(73);
+    
   }
 }
