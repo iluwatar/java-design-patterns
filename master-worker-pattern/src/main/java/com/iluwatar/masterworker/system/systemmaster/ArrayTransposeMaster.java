fdab5318f3b36cdc93a3496a943afd6c84f4362a
Ilkka Seppälä <ilkka.seppala@gofore.com>
2020-07-30 19:38:52 +0300

    Fix compile error

--- master-worker-pattern/src/main/java/com/iluwatar/masterworker/system/systemmaster/ArrayTransposeMaster.java
+++ master-worker-pattern/src/main/java/com/iluwatar/masterworker/system/systemmaster/ArrayTransposeMaster.java
@@ -52,19 +52,19 @@ public class ArrayTransposeMaster extends Master {
   @Override
   ArrayResult aggregateData() {
     // number of rows in final result is number of rows in any of obtained results from workers
-    int rows = this.getAllResultData()
-        .get(this.getAllResultData().keys().nextElement()).data.length;
+    int rows = ((ArrayResult) this.getAllResultData()
+        .get(this.getAllResultData().keys().nextElement())).data.length;
     int columns =
         0; //number of columns is sum of number of columns in all results obtained from workers
     for (Enumeration<Integer> e = this.getAllResultData().keys(); e.hasMoreElements(); ) {
-      columns += this.getAllResultData().get(e.nextElement()).data[0].length;
+      columns += ((ArrayResult) this.getAllResultData().get(e.nextElement())).data[0].length;
     }
     int[][] resultData = new int[rows][columns];
     int columnsDone = 0; //columns aggregated so far
     for (int i = 0; i < this.getExpectedNumResults(); i++) {
       //result obtained from ith worker
       int[][] work =
-          this.getAllResultData().get(this.getWorkers().get(i).getWorkerId()).data;
+          ((ArrayResult) this.getAllResultData().get(this.getWorkers().get(i).getWorkerId())).data;
       for (int m = 0; m < work.length; m++) {
         //m = row number, n = columns number
         for (int n = 0; n < work[0].length; n++) {
