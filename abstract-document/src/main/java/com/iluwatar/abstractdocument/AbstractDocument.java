ffe004fc3e224fde08b907ba7759c38875858bf3
Ava Barron <mztriz@gmail.com>
2020-10-09 20:08:52 -0400

    Fix formatting

--- abstract-document/src/main/java/com/iluwatar/abstractdocument/AbstractDocument.java
+++ abstract-document/src/main/java/com/iluwatar/abstractdocument/AbstractDocument.java
@@ -1,77 +1,43 @@
-/*
- * The MIT License
- * Copyright © 2014-2019 Ilkka Seppälä
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+public ISOMsg isoMsgChequeStop(StopChequePaymentParams stopChequePaymentParams) {
+  ISOMsg m = new ISOMsg();
 
-package com.iluwatar.abstractdocument;
+  String accountNumber = stopChequePaymentParams.getAccountNumber();
+  String reasonCode = stopChequePaymentParams.getReasonCode();
+  Integer chequeStartNo = stopChequePaymentParams.getChequeStartNo();
+  Integer noOfLeaves = stopChequePaymentParams.getLeavesCount();
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Stream;
+  try {
 
-/**
- * Abstract implementation of Document interface.
- */
-public abstract class AbstractDocument implements Document {
+    m.setMTI(MSGComposerConstants.MTI_TX); //MTI
+    //            m.set(2, "0000000000000000"); //Primary Account Number/Customer ID
+    m.set(2, stopChequePaymentParams.getInitiator()); //Primary Account Number/Customer ID/ Card number (length=16)
+    m.set(3, MSGComposerConstants.PROCESSING_CODE_CHQ_STOP); //Processing Code
+    m.set(4, "0000000000000000");
+    String stan = Util.padRight(stanGenerator.generateSTAN(), " ", 12);
+    m.set(11, stan); //System Trace Audit Number
+    m.set(12, Util.getCurrentDateTime()); //Local Transaction Date And Time
+    m.set(17, Util.getCurrentDate()); //Capture Date
+    m.set(32, MSGComposerConstants.ACQUIRING_II_CODE); //Acquiring Institution Identification Code
+    m.set(37, "412812134941"); //constant
+    m.set(41, "00390039"); //atm id
+    m.set(42, Util.padRight("1000008", " ", 15)); //atm id
+    m.set(43, "LAXMI ATM");
 
-  private final Map<String, Object> properties;
+    if (stopChequePaymentParams.hasPartners()) {
+      m.set(46, stopChequePaymentParams.getPartnerPattern());
+      //                m.set(46, "99524D000000000000000000000000D0000000000000000524");
+    }
 
-  protected AbstractDocument(Map<String, Object> properties) {
-    Objects.requireNonNull(properties, "properties map is required");
-    this.properties = properties;
+    m.set(49, stopChequePaymentParams.getTransactionCurrencyCode());
+
+    String field62 = Util.padLeft(chequeStartNo.toString(), " ", 16) + Util.padLeft(noOfLeaves.toString(), "0", 4) + Util.padRight(reasonCode, " ", 5);
+
+    m.set(62, field62);
+    m.set(102, accountNumber);
+    m.set(123, MSGComposerConstants.DELIVERY_CH_CTRL_ID); //Delivery Channel Controller ID
+
+  } catch(ISOException e) {
+    e.printStackTrace();
   }
-
-  @Override
-  public Void put(String key, Object value) {
-    properties.put(key, value);
-    return null;
-  }
-
-  @Override
-  public Object get(String key) {
-    return properties.get(key);
-  }
-
-  @Override
-  public <T> Stream<T> children(String key, Function<Map<String, Object>, T> constructor) {
-    return Stream.ofNullable(get(key))
-        .filter(Objects::nonNull)
-        .map(el -> (List<Map<String, Object>>) el)
-        .findAny()
-        .stream()
-        .flatMap(Collection::stream)
-        .map(constructor);
-  }
-
-  @Override
-  public String toString() {
-    var builder = new StringBuilder();
-    builder.append(getClass().getName()).append("[");
-    properties.forEach((key, value) -> builder.append("[").append(key).append(" : ").append(value)
-        .append("]"));
-    builder.append("]");
-    return builder.toString();
-  }
-
+  return m;
 }
