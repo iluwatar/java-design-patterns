4f09f2119ce19f3742e697c155ef9f099481a5bc
Shrirang <shrirang.pinjarkar@gmail.com>
2020-12-27 20:43:51 +0530

    Added Microservices and kafka

---
+++ claim-check-pattern/extraction-microservice/src/main/java/com/iluwatar/extraction/microservice/ExtractionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.iluwatar.extraction.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ claim-check-pattern/load-microservice/src/test/java/com/iluwatar/load/microservice/LoadMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iluwatar.load.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
