c85112e62cea90863d6b281bcce05cffc54fa8b6
Ali Ghasemi <computerfreak.2968@gmail.com>
2021-01-30 09:23:31 +0330

    add monitor design pattern .

---
+++ monitor/README.md
@@ -0,0 +1,32 @@
+---
+layout: pattern
+title: Monitor
+folder: monitor
+permalink: /patterns/monitor/
+categories: Concurrency
+tags:
+ - Thread-safe
+ - Mutual exclusion
+---
+
+## Intent
+Monitor pattern is used to create thread-safe object . In the concurrent application to prevent conflict between threads .
+## Explanation
+
+In plain words
+
+> Monitor pattern is used when we have critical section or race condition on shared resource in your application so we have to use mutual exclusion to solve this problem ! one of the ways to achieve mutual exclusion is Monitor pattern that invented by Tony Hoare.
+
+Wikipedia says
+
+> In concurrent programming (also known as parallel programming), a monitor is a synchronization construct that allows threads to have both mutual exclusion and the ability to wait (block) for a certain condition to become false. Monitors also have a mechanism for signaling other threads that their condition has been met. 
+
+## Class diagram
+![alt text](./etc/monitor.urm.png "Monitor class diagram")
+
+## Applicability
+Use the Monitor pattern when
+
+* we have a shared resource and there is critical section .
+* you want to create thread-safe objects .
+* you want to achieve mutual exclusion in high level programming language .
---
+++ monitor/src/main/java/com/iluwatar/monitor/Main.java
@@ -0,0 +1,57 @@
+package com.iluwatar.monitor;
+
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AtomicReference<Bank> bank = new AtomicReference<>();
+        bank.set(new Bank(4, 1000));
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep((long) (Math.random() * 1000));
+                Random random = new Random();
+                for (int i = 0; i < 1000000; i++)
+                    bank.get().transfer(random.nextInt(4), random.nextInt(4), (int) (Math.random() * 1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        executorService.execute(runnable);
+        executorService.execute(runnable);
+        executorService.execute(runnable);
+        executorService.execute(runnable);
+        executorService.execute(runnable);
+    }
+}
+
+//We call this class as monitor !
+class Bank {
+
+    private int[] accounts;
+
+    public Bank(int accountNum, int baseAmount) {
+        accounts = new int[accountNum];
+        Arrays.fill(accounts, baseAmount);
+    }
+
+    public synchronized void transfer(int accountA, int accountB, int amount) {
+        if (accounts[accountA] >= amount) {
+            accounts[accountB] += amount;
+            accounts[accountA] -= amount;
+            System.out.println("Transferred from account :" + accountA + " to account :" + accountB + " , amount :" + amount + " . balance :" + getBalance());
+        }
+    }
+
+    public synchronized int getBalance() {
+        int balance = 0;
+        for (int account : accounts) {
+            balance += account;
+        }
+        return balance;
+    }
+}
