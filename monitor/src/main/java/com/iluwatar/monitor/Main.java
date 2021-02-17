e9ce3a9d6ec4cc2e5de19a3e26a543021a396f25
Ali Ghasemi <60359433+Dev-AliGhasemi@users.noreply.github.com>
2021-02-17 22:44:44 +0330

    explanations added !

--- monitor/src/main/java/com/iluwatar/monitor/Main.java
+++ monitor/src/main/java/com/iluwatar/monitor/Main.java
@@ -1,9 +1,44 @@
+/*
+ * The MIT License
+ * Copyright © 2014-2021 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.iluwatar.monitor;
 
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
+
+/**
+ *
+ * <p>The Monitor pattern is use in concurrent algorithm and a good way to achieve mutual exclusive
+ * and a good solution to prevent critical section </p>
+ *
+ * <p>Bank class is a simple class that transfer amount from an account to another account using
+ * {@link Bank#transfer} and get balance return total amount that store in bank (default amount specify in constructor)</p>
+ *
+ * <p>In Main class uses ThreadPool to run some threads for do some transactions</p>
+ *
+ */
 
 public class Main {
 
