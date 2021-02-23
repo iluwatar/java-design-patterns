988e31947aec7ebab1df081acd84b3afbe0d64d8
Noam <noamgrinch@gmail.com>
2021-02-23 14:37:22 +0200

    Fixed more whitespaces problems.

--- active-object/src/main/java/com/iluwatar/activeobject/ActiveCounter.java
+++ active-object/src/main/java/com/iluwatar/activeobject/ActiveCounter.java
@@ -10,69 +10,69 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class ActiveCounter {
-    
-    private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
+  
+  private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
 
-    private Integer val;
+  private Integer val;
 
-    private BlockingQueue<Runnable> requests;
+  private BlockingQueue<Runnable> requests;
 
-    public ActiveCounter() {
-        this.requests = new LinkedBlockingQueue<Runnable>();
-        this.val = 0;
-        new Thread (new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        try {
-                            requests.take().run();
-                        } catch (InterruptedException e) {   
-                            
-                        }
-                    }
-                }
+  public ActiveCounter() {
+    this.requests = new LinkedBlockingQueue<Runnable>();
+    this.val = 0;
+    new Thread (new Runnable() {
+        @Override
+        public void run() {
+          while (true) {
+            try {
+              requests.take().run();
+            } catch (InterruptedException e) {   
+              
             }
-        ).start();
-    }
-    /**
-     * Zerorizes val property.
-     * @throws InterruptedException
-     */
-    public void zerorize() throws InterruptedException {
-        requests.put(new Runnable() {
-                @Override
-                public void run() { 
-                    val = 0; 
-                    LOGGER.info("val has been set to 0.");
-                }
-            }
-        );
-    }
-    /**
-     * Incremented val property by one.
-     * @throws InterruptedException
-     */
-    public void incremenet() throws InterruptedException {
-        requests.put(new Runnable() {
-                @Override
-                public void run() { 
-                    val++; 
-                    LOGGER.info("val has been incremented.");
-                }
-            }
-        );
-    }
-    /**
-     * Logging the current value of val property.
-     * @throws InterruptedException
-     */
-    public void printVal() throws InterruptedException {
-        requests.put(new Runnable() {
-                @Override
-                public void run() { 
-                    LOGGER.info(val.toString());
-                }
-            }
-        );
-    }   
+          }
+        }
+      }
+    ).start();
+  }
+  /**
+   * Zerorizes val property.
+   * @throws InterruptedException
+   */
+  public void zerorize() throws InterruptedException {
+    requests.put(new Runnable() {
+        @Override
+        public void run() { 
+          val = 0; 
+          LOGGER.info("val has been set to 0.");
+        }
+      }
+    );
+  }
+  /**
+   * Incremented val property by one.
+   * @throws InterruptedException
+   */
+  public void incremenet() throws InterruptedException {
+    requests.put(new Runnable() {
+        @Override
+        public void run() { 
+          val++; 
+          LOGGER.info("val has been incremented.");
+        }
+      }
+    );
+  }
+  /**
+   * Logging the current value of val property.
+   * @throws InterruptedException
+   */
+  public void printVal() throws InterruptedException {
+    requests.put(new Runnable() {
+        @Override
+        public void run() { 
+          LOGGER.info(val.toString());
+        }
+      }
+    );
+  }   
 }
--- active-object/src/main/java/com/iluwatar/activeobject/App.java
+++ active-object/src/main/java/com/iluwatar/activeobject/App.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ * Copyright © 2014-2021 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.iluwatar.activeobject;
 
 import java.util.concurrent.ExecutorService;
@@ -5,7 +28,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 /**
  * 
@@ -20,38 +42,38 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class App {
-    
-    public final static int WORKERS = 20;
-    
-    private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
+  
+  public final static int WORKERS = 20;
+  
+  private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
 
-    /**
-     * Program entry point.
-     *
-     * @param args command line args
-     */
-    public static void main(String[] args) {    
-        ActiveCounter counter = new ActiveCounter();
-        ExecutorService e = Executors.newCachedThreadPool();
-        for(int i=0;i<WORKERS;i++) {
-            e.execute(new Runnable() {            
-                @Override
-                public void run() {
-                    try {
-                        counter.incremenet();
-                        counter.printVal();
-                    } catch (InterruptedException e) {
-                        LOGGER.error(e.getMessage());
-                    }
-                }
-                
-            });
+  /**
+   * Program entry point.
+   *
+   * @param args command line args
+   */
+  public static void main(String[] args) {  
+    ActiveCounter counter = new ActiveCounter();
+    ExecutorService e = Executors.newCachedThreadPool();
+    for(int i=0;i<WORKERS;i++) {
+      e.execute(new Runnable() {      
+        @Override
+        public void run() {
+          try {
+            counter.incremenet();
+            counter.printVal();
+          } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage());
+          }
         }
-        try {
-            e.awaitTermination(1, TimeUnit.SECONDS);
-        } catch (InterruptedException e1) {
-            LOGGER.error(e1.getMessage());
-        }
-        System.exit(1);
+        
+      });
     }
+    try {
+      e.awaitTermination(1, TimeUnit.SECONDS);
+    } catch (InterruptedException e1) {
+      LOGGER.error(e1.getMessage());
+    }
+    System.exit(1);
+  }
 }
