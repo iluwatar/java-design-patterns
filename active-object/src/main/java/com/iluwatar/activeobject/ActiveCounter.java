53736398e909c45c68096d8cd27ff4a322e6992c
Noam <noamgrinch@gmail.com>
2021-02-23 14:18:16 +0200

    Replaced tabs with whitespaces, added Javadocs.

--- active-object/src/main/java/com/iluwatar/activeobject/ActiveCounter.java
+++ active-object/src/main/java/com/iluwatar/activeobject/ActiveCounter.java
@@ -2,25 +2,30 @@ package com.iluwatar.activeobject;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+/**
+ * ActiveCounter class is the active object example.
+ * @author Noam Greenshtain
+ *
+ */
 public class ActiveCounter {
-	
-	private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
+    
+    private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
+
     private Integer val;
+
     private BlockingQueue<Runnable> requests;
 
     public ActiveCounter() {
-    	this.requests = new LinkedBlockingQueue<Runnable>();
-    	this.val = 0;
+        this.requests = new LinkedBlockingQueue<Runnable>();
+        this.val = 0;
         new Thread (new Runnable() {
                 @Override
                 public void run() {
                     while (true) {
                         try {
-                        	requests.take().run();
+                            requests.take().run();
                         } catch (InterruptedException e) {   
                             
                         }
@@ -29,10 +34,12 @@ public class ActiveCounter {
             }
         ).start();
     }
-
-    
+    /**
+     * Zerorizes val property.
+     * @throws InterruptedException
+     */
     public void zerorize() throws InterruptedException {
-    	requests.put(new Runnable() {
+        requests.put(new Runnable() {
                 @Override
                 public void run() { 
                     val = 0; 
@@ -41,9 +48,12 @@ public class ActiveCounter {
             }
         );
     }
-
+    /**
+     * Incremented val property by one.
+     * @throws InterruptedException
+     */
     public void incremenet() throws InterruptedException {
-    	requests.put(new Runnable() {
+        requests.put(new Runnable() {
                 @Override
                 public void run() { 
                     val++; 
@@ -52,16 +62,17 @@ public class ActiveCounter {
             }
         );
     }
-    
+    /**
+     * Logging the current value of val property.
+     * @throws InterruptedException
+     */
     public void printVal() throws InterruptedException {
-    	requests.put(new Runnable() {
+        requests.put(new Runnable() {
                 @Override
                 public void run() { 
-                	LOGGER.info(val.toString());
+                    LOGGER.info(val.toString());
                 }
             }
         );
-    }
-   
-    
+    }   
 }
--- active-object/src/main/java/com/iluwatar/activeobject/App.java
+++ active-object/src/main/java/com/iluwatar/activeobject/App.java
@@ -3,7 +3,6 @@ package com.iluwatar.activeobject;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,42 +11,47 @@ import org.slf4j.LoggerFactory;
  * 
  * @author Noam Greenshtain
  * 
- * The Active Object pattern helps to solve synchronization difficulties without using 'synchronized' methods.
- * The active object will contain a thread-safe data structure(such as BlockingQueue) and use to synchronize
- * method calls by moving the logic of the method into an invocator(usually a Runnable) and store it in the DSA.
- *
+ * The Active Object pattern helps to solve synchronization difficulties without using 
+ * 'synchronized' methods.The active object will contain a thread-safe data structure 
+ * (such as BlockingQueue) and use to synchronize method calls by moving the logic of the method
+ * into an invocator(usually a Runnable) and store it in the DSA.
+ * 
  * In this example, we fire 20 threads to modify a value in the target class.
  *
  */
 public class App {
-	
-	public final static int WORKERS = 20;
-	
-	private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
+    
+    public final static int WORKERS = 20;
+    
+    private final static Logger LOGGER = LoggerFactory.getLogger(ActiveCounter.class.getName());
 
-	public static void main(String[] args) {	
-		ActiveCounter counter = new ActiveCounter();
-		ExecutorService e = Executors.newCachedThreadPool();
-		for(int i=0;i<WORKERS;i++) {
-			e.execute(new Runnable() {			
-				@Override
-				public void run() {
-					try {
-						counter.incremenet();
-						counter.printVal();
-					} catch (InterruptedException e) {
-						LOGGER.error(e.getMessage());
-					}
-				}
-				
-			});
-		}
-		try {
-			e.awaitTermination(1, TimeUnit.SECONDS);
-		} catch (InterruptedException e1) {
-			LOGGER.error(e1.getMessage());
-		}
-		System.exit(1);
-	}
-
+    /**
+     * Program entry point.
+     *
+     * @param args command line args
+     */
+    public static void main(String[] args) {    
+        ActiveCounter counter = new ActiveCounter();
+        ExecutorService e = Executors.newCachedThreadPool();
+        for(int i=0;i<WORKERS;i++) {
+            e.execute(new Runnable() {            
+                @Override
+                public void run() {
+                    try {
+                        counter.incremenet();
+                        counter.printVal();
+                    } catch (InterruptedException e) {
+                        LOGGER.error(e.getMessage());
+                    }
+                }
+                
+            });
+        }
+        try {
+            e.awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e1) {
+            LOGGER.error(e1.getMessage());
+        }
+        System.exit(1);
+    }
 }
