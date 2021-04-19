8561c45e7dae3d5517a63dd6de3089e30d1714f0
zwebrain <11811721@mail.sustech.edu.cn>
2021-04-19 21:46:01 +0800

    removed the author name and adjusted the spacing

--- composite-entity/src/test/java/com/iluwatar/compositeentity/PersistenceTest.java
+++ composite-entity/src/test/java/com/iluwatar/compositeentity/PersistenceTest.java
@@ -4,33 +4,29 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Date: 4/17/21 - 11:45 AM
- *
- * @author zwebrain
- */
 class PersistenceTest {
-    final static ConsoleCoarseGrainedObject console = new ConsoleCoarseGrainedObject();
 
-    @Test
-    void dependentObjectChangedForPersistenceTest() {
-        MessageDependentObject dependentObject = new MessageDependentObject();
-        console.init();
-        console.dependentObjects[0] = dependentObject;
-        String message = "Danger";
-        assertNull(console.dependentObjects[0].getData());
-        dependentObject.setData(message);
-        assertEquals(message, console.dependentObjects[0].getData());
-    }
+  final static ConsoleCoarseGrainedObject console = new ConsoleCoarseGrainedObject();
 
-    @Test
-    void coarseGrainedObjectChangedForPersistenceTest() {
-        MessageDependentObject dependentObject = new MessageDependentObject();
-        console.init();
-        console.dependentObjects[0] = dependentObject;
-        String message = "Danger";
-        assertNull(console.dependentObjects[0].getData());
-        console.setData(message);
-        assertEquals(message, dependentObject.getData());
-    }
+  @Test
+  void dependentObjectChangedForPersistenceTest() {
+    MessageDependentObject dependentObject = new MessageDependentObject();
+    console.init();
+    console.dependentObjects[0] = dependentObject;
+    String message = "Danger";
+    assertNull(console.dependentObjects[0].getData());
+    dependentObject.setData(message);
+    assertEquals(message, console.dependentObjects[0].getData());
+  }
+
+  @Test
+  void coarseGrainedObjectChangedForPersistenceTest() {
+    MessageDependentObject dependentObject = new MessageDependentObject();
+    console.init();
+    console.dependentObjects[0] = dependentObject;
+    String message = "Danger";
+    assertNull(console.dependentObjects[0].getData());
+    console.setData(message);
+    assertEquals(message, dependentObject.getData());
+  }
 }
