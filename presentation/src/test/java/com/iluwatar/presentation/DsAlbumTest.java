43aa0235d12ef1f9c2f14eed4c276079fec37323
edison <735318617@qq.com>
2021-04-24 11:44:44 +0800

    #415 modify test

--- presentation/src/test/java/com/iluwatar/presentation/DsAlbumTest.java
+++ presentation/src/test/java/com/iluwatar/presentation/DsAlbumTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DsAlbumTest {
+class DsAlbumTest {
   @Test
   void testAdd_true(){
     DsAlbum dsAlbum = new DsAlbum();
