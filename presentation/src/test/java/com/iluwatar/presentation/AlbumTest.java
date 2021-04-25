90a89398028172d03ae87a0f53a8adbb98ef266a
edison <735318617@qq.com>
2021-04-25 20:21:00 +0800

    #415 fix code smell.

--- presentation/src/test/java/com/iluwatar/presentation/AlbumTest.java
+++ presentation/src/test/java/com/iluwatar/presentation/AlbumTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AlbumTest {
+class AlbumTest {
   @Test
   void testSetTitle(){
     Album album = new Album("a", "b", false, "");
