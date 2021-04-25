450f7dc570153916918d9d3425fb23af3dd5fc97
edison <735318617@qq.com>
2021-04-25 19:19:08 +0800

    #415 add package-info.java.

---
+++ presentation/src/main/java/com/iluwatar/presentation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This is a demo for presentation model pattern.
+ * Presentation Model pulls the state and behavior
+ * of the view out into a model class that is part
+ * of the presentation.
+ */
+package com.iluwatar.presentation;
