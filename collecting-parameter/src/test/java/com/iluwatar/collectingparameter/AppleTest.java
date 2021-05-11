d1819cd7b725fd4b892e925525546f448f961e75
santyelegy <11811719@mail.sustech.edu.cn>
2021-05-11 16:56:13 +0800

    remove code smell

--- collecting-parameter/src/test/java/com/iluwatar/collectingparameter/AppleTest.java
+++ collecting-parameter/src/test/java/com/iluwatar/collectingparameter/AppleTest.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class AppleTest {
+class AppleTest {
   @Test
   void writeWeightToTest() {
     List<String> info = new ArrayList<>();
