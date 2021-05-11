5eb19f54758bf89ce7217da1a6d1b7c4f9943381
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2021-05-11 18:37:48 +0530

    Delete package-info.java

--- presentation/src/main/java/com/iluwatar/presentation/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This is a demo for presentation model pattern.
- * Presentation Model pulls the state and behavior
- * of the view out into a model class that is part
- * of the presentation.
- */
-package com.iluwatar.presentation;
