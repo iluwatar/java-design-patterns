4137b5b4d055076f74c76b249beaed6beaec03eb
wangihzyue <58692091+wangihzyue@users.noreply.github.com>
2021-05-14 18:08:52 +0800

    Update facet/src/main/java/com/iluwatar/facet/Sentry.java
    Co-authored-by: Subhrodip Mohanta <subhrodipmohanta@gmail.com>

--- facet/src/main/java/com/iluwatar/facet/Sentry.java
+++ facet/src/main/java/com/iluwatar/facet/Sentry.java
@@ -2,7 +2,7 @@ package com.iluwatar.facet;
 
 /**
  * The sentry interface to consider the interface and context being
- * used to make the request and select one of it comprising objects to handle it.
+ * used to make the request and select one of its comprising objects to handle it.
  */
 public interface Sentry {
   public abstract boolean execute(User user, Class<? extends SecurityMethods> interfaceClass);
