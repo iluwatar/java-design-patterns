800e99cebc87b9591e8a90241f5e3909bd53de94
zy <1164138952@qq.com>
2021-05-18 14:19:25 +0800

    add table data gateway (issue #1318)

--- table-data-gateway/src/main/java/com/iluwatar/tabledatagateway/PersonInterface.java
+++ table-data-gateway/src/main/java/com/iluwatar/tabledatagateway/PersonInterface.java
@@ -2,9 +2,12 @@ package com.iluwatar.tabledatagateway;
 
 public interface PersonInterface {
   int getId();
+
   int getAge();
 
   String getFirstName();
+
   String getLastName();
+
   String getGender();
 }
