99925ee9b8cb8679c8b377833accb21f0eddb599
AliGhasemi <computerfreak.2968@gmail.com>
2021-10-14 02:31:02 -0700

    AddUnitTest

--- monitor/src/main/java/com/iluwatar/monitor/Main.java
+++ monitor/src/main/java/com/iluwatar/monitor/Main.java
@@ -58,32 +58,3 @@ public class Main {
         }
     }
 }
-
-// Bank class implements the Monitor pattern
-class Bank {
-
-    private int[] accounts;
-    Logger logger;
-
-    public Bank(int accountNum, int baseAmount, Logger logger) {
-        this.logger = logger;
-        accounts = new int[accountNum];
-        Arrays.fill(accounts, baseAmount);
-    }
-
-    public synchronized void transfer(int accountA, int accountB, int amount) {
-        if (accounts[accountA] >= amount) {
-            accounts[accountB] += amount;
-            accounts[accountA] -= amount;
-            logger.info("Transferred from account :" + accountA + " to account :" + accountB + " , amount :" + amount + " . balance :" + getBalance());
-        }
-    }
-
-    private synchronized int getBalance() {
-        int balance = 0;
-        for (int account : accounts) {
-            balance += account;
-        }
-        return balance;
-    }
-}
