221e0316d34baf6b243c3e7ca1a9b7949862f5cf
Janus <januslinhc@hotmail.com>
2021-10-19 01:20:27 +0800

    fix style

--- currying/src/main/java/com/iluwatar/currying/Staff.java
+++ currying/src/main/java/com/iluwatar/currying/Staff.java
@@ -4,68 +4,103 @@ import java.time.LocalDate;
 import java.util.Objects;
 import java.util.function.Function;
 
+/**
+ * Staff Object for demonstrating how to use currying pattern.
+ */
 public class Staff {
-    private String firstName;
-    private String lastName;
-    private Gender gender;
-    private String email;
-    private LocalDate dateOfBirth;
+  private String firstName;
+  private String lastName;
+  private Gender gender;
+  private String email;
+  private LocalDate dateOfBirth;
 
-    public Staff(String firstName, String lastName, Gender gender, String email, LocalDate dateOfBirth) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.email = email;
-        this.dateOfBirth = dateOfBirth;
+  /**
+   * Contractor.
+
+   * @param firstName first name.
+   * @param lastName last name.
+   * @param gender gender.
+   * @param email email.
+   * @param dateOfBirth date of birth.
+   */
+  public Staff(String firstName,
+               String lastName,
+               Gender gender,
+               String email,
+               LocalDate dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.email = email;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Staff staff = (Staff) o;
-        return firstName.equals(staff.firstName) && lastName.equals(staff.lastName) && gender == staff.gender && email.equals(staff.email) && dateOfBirth.equals(staff.dateOfBirth);
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
+    Staff staff = (Staff) o;
+    return firstName.equals(staff.firstName)
+            && lastName.equals(staff.lastName)
+            && gender == staff.gender
+            && email.equals(staff.email)
+            && dateOfBirth.equals(staff.dateOfBirth);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, gender, email, dateOfBirth);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName, gender, email, dateOfBirth);
+  }
 
-    /**
-     * Use {@link Function} for currying
-     */
-    static Function<String, Function<String, Function<Gender, Function<String, Function<LocalDate, Staff>>>>> CREATOR =
-            firstName -> lastName -> gender -> email -> dateOfBirth -> new Staff(firstName, lastName, gender, email, dateOfBirth);
+  /**
+   * Use {@link Function} for currying.
+   */
+  static Function<String,
+            Function<String,
+                    Function<Gender,
+                            Function<String,
+                                    Function<LocalDate, Staff>>>>> CREATOR =
+                                        firstName -> lastName
+                                            -> gender -> email
+                                                -> dateOfBirth
+                                                    -> new Staff(firstName, lastName, gender,
+                                                         email, dateOfBirth);
 
-    /**
-     * Use functional interfaces for currying
-     */
-    static AddFirstName builder() {
-        return firstName -> lastName -> gender -> email -> dateOfBirth -> new Staff(firstName, lastName, gender, email, dateOfBirth);
-    }
+  /**
+   * Use functional interfaces for currying.
+   */
+  static AddFirstName builder() {
+    return firstName -> lastName
+        -> gender -> email
+            -> dateOfBirth
+                -> new Staff(firstName, lastName, gender, email, dateOfBirth);
+  }
 
-    interface AddFirstName {
-        AddLastName withReturnFirstName(String firstName);
-    }
+  interface AddFirstName {
+    AddLastName withReturnFirstName(String firstName);
+  }
 
-    interface AddLastName {
-        AddGender withReturnLastName(String lastName);
-    }
+  interface AddLastName {
+    AddGender withReturnLastName(String lastName);
+  }
 
-    interface AddGender {
-        AddEmail withReturnGender(Gender gender);
-    }
+  interface AddGender {
+    AddEmail withReturnGender(Gender gender);
+  }
 
-    interface AddEmail {
-        AddDateOfBirth withReturnEmail(String email);
-    }
+  interface AddEmail {
+    AddDateOfBirth withReturnEmail(String email);
+  }
 
-    interface AddDateOfBirth {
-        Staff withReturnDateOfBirth(LocalDate dateOfBirth);
-    }
+  interface AddDateOfBirth {
+    Staff withReturnDateOfBirth(LocalDate dateOfBirth);
+  }
 
-    enum Gender {
-        Male, Female
-    }
+  enum Gender {
+    Male, Female
+  }
 }
