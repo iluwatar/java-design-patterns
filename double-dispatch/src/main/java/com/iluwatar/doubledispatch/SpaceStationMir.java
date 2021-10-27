c8a9fb6cbdf88d1dcb09d07e970d8379bc4a9caf
Harshal <37841724+harshalkh@users.noreply.github.com>
2021-10-27 22:21:15 +0530

    Update SpaceStationMir.java

--- double-dispatch/src/main/java/com/iluwatar/doubledispatch/SpaceStationMir.java
+++ double-dispatch/src/main/java/com/iluwatar/doubledispatch/SpaceStationMir.java
@@ -33,6 +33,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpaceStationMir extends GameObject {
 
   private static final String IS_DAMAGED = " {} is damaged!";
+
   public SpaceStationMir(int left, int top, int right, int bottom) {
     super(left, top, right, bottom);
   }
