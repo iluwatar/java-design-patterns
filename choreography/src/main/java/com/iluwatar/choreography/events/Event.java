8df3e6fd53730b2c7b614e7eeede68b0303c6391
patbeagan1 <pbeagan1@gmail.com>
2021-11-02 23:11:49 -0500

    Updating the output to say saga instead of thread

--- choreography/src/main/java/com/iluwatar/choreography/events/Event.java
+++ choreography/src/main/java/com/iluwatar/choreography/events/Event.java
@@ -16,11 +16,11 @@ public class Event {
     public String prettyPrintSagaId() {
         switch (getSagaId()) {
             case 0:
-                return (ANSI_BG_RED + "Thread 1 " + ANSI_RESET + "| ");
+                return (ANSI_BG_RED + "Saga 1 " + ANSI_RESET + "| ");
             case 1:
-                return (ANSI_BG_GREEN + "Thread 2 " + ANSI_RESET + "| ");
+                return (ANSI_BG_GREEN + "Saga 2 " + ANSI_RESET + "| ");
             case 2:
-                return (ANSI_BG_BLUE + "Thread 3 " + ANSI_RESET + "| ");
+                return (ANSI_BG_BLUE + "Saga 3 " + ANSI_RESET + "| ");
         }
         return "Unknown| ";
     }
