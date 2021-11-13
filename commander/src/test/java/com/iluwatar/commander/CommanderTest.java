9896856a27cc6fd74b41dccb2095ae57dd11c463
atayal <Ankush_Tayal@intuit.com>
2021-11-13 14:01:38 +0530

    sonar bug fix : test commit

--- commander/src/test/java/com/iluwatar/commander/CommanderTest.java
+++ commander/src/test/java/com/iluwatar/commander/CommanderTest.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CommanderTest {
 
