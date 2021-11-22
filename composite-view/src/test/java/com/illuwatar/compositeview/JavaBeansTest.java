8f9ad823fb80c6495d3c25fbb2f9ea376d44e0ea
Kevin <kevinyl3@illinois.edu>
2021-11-22 17:33:25 -0600

    set up junit for tests folder.

--- composite-view/src/test/java/com/illuwatar/compositeview/JavaBeansTest.java
+++ composite-view/src/test/java/com/illuwatar/compositeview/JavaBeansTest.java
@@ -1,6 +1,13 @@
 package com.illuwatar.compositeview;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
 
 public class JavaBeansTest {
+    @Test
+    public void testJavaBeanConstructor() {
+
+    }
 
 }
