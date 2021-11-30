452aa123b0e95e88d0c81c0b4a2f6df82fbc3540
Natalie So <natalieso@Natalies-MacBook-Pro.local>
2021-11-30 14:10:04 -0800

    change according to checkstyle

--- notification/src/main/java/com/iluwatar/notification/RegisterCourseDto.java
+++ notification/src/main/java/com/iluwatar/notification/RegisterCourseDto.java
@@ -10,9 +10,12 @@ public class RegisterCourseDto extends DataTransferObject {
   private String department;
   private String semester;
 
-  public static final NotificationError MISSING_COURSE_ID = new NotificationError("Course ID is missing");
-  public static final NotificationError MISSING_DEPARTMENT = new NotificationError("Department is missing");
-  public static final NotificationError MISSING_SEMESTER = new NotificationError("Semester is missing");
+  public static final NotificationError MISSING_COURSE_ID =
+      new NotificationError("Course ID is missing");
+  public static final NotificationError MISSING_DEPARTMENT =
+      new NotificationError("Department is missing");
+  public static final NotificationError MISSING_SEMESTER =
+      new NotificationError("Semester is missing");
 
   protected RegisterCourseDto() {}
 
