779e007dbf221e38b586178ceb096ed76c7bd309
Arnab Sen <arnabsen1729@gmail.com>
2022-01-30 01:40:17 +0530

    fix: codesmells and bug

--- monitor/src/test/java/com/iluwatar/monitor/BankTest.java
+++ monitor/src/test/java/com/iluwatar/monitor/BankTest.java
@@ -1,3 +1,26 @@
+/*
+ *The MIT License
+ *Copyright © 2014-2021 Ilkka Seppälä
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in
+ *all copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *THE SOFTWARE.
+ */
+
 package com.iluwatar.monitor;
 
 import org.junit.jupiter.api.AfterAll;
@@ -23,26 +46,26 @@ public class BankTest {
   }
 
   @Test
-  public void GetAccountHaveNotBeNull() {
+  void GetAccountHaveNotBeNull() {
     assertNotNull(bank.getAccounts());
   }
 
   @Test
-  public void LengthOfAccountsHaveToEqualsToAccountNumConstant() {
+  void LengthOfAccountsHaveToEqualsToAccountNumConstant() {
     assumeTrue(bank.getAccounts() != null);
     assertEquals(ACCOUNT_NUM, bank.getAccounts().length);
   }
 
   @Test
-  public void TransferMethodHaveToTransferAmountFromAnAccountToOtherAccount() {
+  void TransferMethodHaveToTransferAmountFromAnAccountToOtherAccount() {
     bank.transfer(0, 1, 1000);
     int[] accounts = bank.getAccounts();
     assertEquals(0, accounts[0]);
-    assertEquals(2000, 2000);
+    assertEquals(2000, accounts[1]);
   }
 
   @Test
-  public void BalanceHaveToBeOK() {
+  void BalanceHaveToBeOK() {
     assertEquals(4000, bank.getBalance());
   }
 }
