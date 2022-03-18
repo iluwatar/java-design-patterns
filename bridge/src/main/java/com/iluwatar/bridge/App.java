1e6e1ef26469f4a83a4c3905e115b175dae9ebbb
JyotipriyoDutta <89502700+JyotipriyoDutta@users.noreply.github.com>
2022-03-19 01:38:43 +0530

    Update App.java

--- bridge/src/main/java/com/iluwatar/bridge/App.java
+++ bridge/src/main/java/com/iluwatar/bridge/App.java
@@ -2,7 +2,7 @@
  * The MIT License
  * Copyright © 2014-2021 Ilkka Seppälä
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of software
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
