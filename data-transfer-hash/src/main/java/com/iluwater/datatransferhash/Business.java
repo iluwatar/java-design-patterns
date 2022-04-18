747f695fe19668b5825dd0ed147729198c0fa79d
SpaceIshtar <740898026@qq.com>
2022-04-18 21:24:15 +0800

    Add some JavaDocs

--- data-transfer-hash/src/main/java/com/iluwater/datatransferhash/Business.java
+++ data-transfer-hash/src/main/java/com/iluwater/datatransferhash/Business.java
@@ -6,10 +6,24 @@ package com.iluwater.datatransferhash;
  */
 public class Business {
 
+  /**
+   * Create hashes for sending data.
+   *
+   * @param k Hash key of data
+   * @param v Data Value
+   * @param hash The data transfer hash object, which stores data for transport between layers
+   */
   public void createHash(final String k, final Object v, final DataTransferHashObject hash) {
     hash.put(k, v);
   }
 
+  /**
+   * Receive data according to hash key.
+   *
+   * @param k Hash key of data
+   * @param hash The data transfer hash object, which stores data for transport between layers
+   * @return Received Data from another layer
+   */
   public Object getData(final String k, final DataTransferHashObject hash) {
     return hash.getValue(k);
   }
--- data-transfer-hash/src/main/java/com/iluwater/datatransferhash/DataTransferHashObject.java
+++ data-transfer-hash/src/main/java/com/iluwater/datatransferhash/DataTransferHashObject.java
@@ -9,14 +9,30 @@ import java.util.HashMap;
 public class DataTransferHashObject {
   private final HashMap<String, Object> hash;
 
+  /**
+   * Constructor function for data transfer hash object.
+   * It initiates a HashMap for storing (key,value) pair.
+   */
   DataTransferHashObject() {
     hash = new HashMap<>();
   }
 
+  /**
+   * Put (key,value) pair into Hashmap storage structure.
+   *
+   * @param key Hash key of data value.
+   * @param value The data value that needs to be stored.
+   */
   public void put(final String key, final Object value) {
     hash.put(key, value);
   }
 
+  /**
+   * Get value from Hashmap storage structure according to given hash key.
+   *
+   * @param key Hash key of data value.
+   * @return Data value.
+   */
   public Object getValue(final String key) {
     return this.hash.get(key);
   }
