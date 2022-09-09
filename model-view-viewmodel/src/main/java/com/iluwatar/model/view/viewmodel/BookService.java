d57709bc90fafe291d11a3b61896a1a507bd107f
Ilkka Seppälä <ilkka.seppala@gofore.com>
2022-09-09 21:52:37 +0300

    fix checkstyle error

--- model-view-viewmodel/src/main/java/com/iluwatar/model/view/viewmodel/BookService.java
+++ model-view-viewmodel/src/main/java/com/iluwatar/model/view/viewmodel/BookService.java
@@ -22,21 +22,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * Module Model-view-viewmodel is using ZK framework
- * ZK framework is licensed under LGPL and the license can be found at lgpl-3.0.txt
- */
 
 package com.iluwatar.model.view.viewmodel;
 
