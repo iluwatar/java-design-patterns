932836f68bf827b0194a58e4668a6e4e8fe39467
Ilkka Seppälä <iluwatar@users.noreply.github.com>
2022-09-14 20:52:24 +0300

    Update license plugin (#2020)
    * update license plugin
    
    * suppress linelength and emptylineseparator rules
    
    * fix checkstyle error
    
    Co-authored-by: Subhrodip Mohanta <hello@subho.xyz>

--- model-view-viewmodel/src/main/java/com/iluwatar/model/view/viewmodel/Book.java
+++ model-view-viewmodel/src/main/java/com/iluwatar/model/view/viewmodel/Book.java
@@ -1,7 +1,8 @@
 /*
- * The MIT License (MIT)
+ * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
  *
- * Copyright © 2014-2021 Ilkka Seppälä
+ * The MIT License
+ * Copyright © 2014-2022 Ilkka Seppälä
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,21 +11,17 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * Module Model-view-viewmodel is using ZK framework
- * ZK framework is licensed under LGPL and the license can be found at lgpl-3.0.txt
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
-
 package com.iluwatar.model.view.viewmodel;
 
 import lombok.AllArgsConstructor;
