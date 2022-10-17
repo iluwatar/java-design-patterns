a87a42a153c8eaaf51f2df8561d66597c3fdcf01
Hugo Kat <u7286091@anu.edu.au>
2022-10-17 22:10:53 +1100

    #1842 Setting up project and creating example classes. Issues running site and deploy

---
+++ currying/src/main/java/com/iluwatar/currying/Book.java
@@ -0,0 +1,133 @@
+/*
+ * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
+ *
+ * The MIT License
+ * Copyright © 2014-2022 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.iluwatar.currying;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * This is a Javadoc comment to pass the style check.
+ */
+public class Book {
+  private Genre genre;
+  private String author;
+  private String title;
+  private LocalDate publicationDate;
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  Book(Genre genre, String author, String title, LocalDate publicationDate) {
+    this.genre = genre;
+    this.author = author;
+    this.title = title;
+    this.publicationDate = publicationDate;
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Book book = (Book) o;
+    return Objects.equals(author, book.author)
+            && Objects.equals(genre, book.genre)
+            && Objects.equals(title, book.title)
+            && Objects.equals(publicationDate, book.publicationDate);
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(author, genre, title, publicationDate);
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  @Override
+  public String toString() {
+    return "Book{" + "genre=" + genre + ", author='" + author + '\''
+            + ", title='" + title + '\'' + ", publicationDate=" + publicationDate + '}';
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  static Function<Genre, Function<String, Function<String, Function<LocalDate, Book>>>> BOOK_CREATOR = //
+      genre
+          -> author
+              -> title
+                  -> publicationDate
+                      -> new Book(genre, author, title, publicationDate);
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  public static AddGenre builder() {
+    return genre
+        -> author
+            -> title
+                -> publicationDate
+                    -> new Book(genre, author, title, publicationDate);
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  public interface AddGenre {
+    Book.AddAuthor withGenre(Genre genre);
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  public interface AddAuthor {
+    Book.AddTitle withAuthor(String author);
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  public interface AddTitle {
+    Book.AddPublicationDate withTitle(String title);
+  }
+
+  /**
+   * This is a Javadoc comment to pass the style check.
+   */
+  public interface AddPublicationDate {
+    Book withPublicationDate(LocalDate publicationDate);
+  }
+}
