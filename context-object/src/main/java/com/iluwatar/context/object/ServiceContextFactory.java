c1084eddff69823f7486d15cb0df07874252c014
Alvis Chan <u7287079@edu.anu.au>
2022-10-25 09:27:53 +1100

    Update context pattern

--- context-object/src/main/java/com/iluwatar/context/object/ServiceContextFactory.java
+++ context-object/src/main/java/com/iluwatar/context/object/ServiceContextFactory.java
@@ -8,22 +8,4 @@ public class ServiceContextFactory {
     public static ServiceContext createContext() {
         return new ServiceContext();
     }
-
-    public static ServiceContext createAccountContext(String accountService) {
-        ServiceContext accountContext = new ServiceContext();
-        accountContext.setACCOUNT_SERVICE(accountService);
-        return accountContext;
-    }
-
-    public static ServiceContext createSessionContext(String sessionService) {
-        ServiceContext sessionContext = new ServiceContext();
-        sessionContext.setACCOUNT_SERVICE(sessionService);
-        return sessionContext;
-    }
-
-    public static ServiceContext createSearchContext(String searchService) {
-        ServiceContext searchContext = new ServiceContext();
-        searchContext.setACCOUNT_SERVICE(searchService);
-        return searchContext;
-    }
 }
