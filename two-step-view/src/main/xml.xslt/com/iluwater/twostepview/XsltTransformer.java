eb86df38fb60d2ebb4fb5f9be51cb8fa3fddcdcf
Stephanie Sun <u7277130@anu.edu.au>
2022-10-25 23:05:18 +1100

    Added the logical representation of two-step-view pattern

--- two-step-view/src/main/xml.xslt/com/iluwater/twostepview/XsltTransformer.java
+++ two-step-view/src/main/xml.xslt/com/iluwater/twostepview/XsltTransformer.java
@@ -1,18 +1,52 @@
+/*
+ * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
+ *
+ * The MIT License
+ * Copyright © 2014-2022 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwater.twostepview;
 
 import javax.xml.transform.*;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.stream.*;
 import java.io.*;
 
 
-
-
+/**
+ * In this class,it is the first stage of the two-step view pattern which produced page in logical representation.
+ * It transforms the input XML document (couse.xml) with XSLT stylesheet (coursetostudent.xsl) to describe
+ * how the course data is transformed into another XML document (students.xml) that uses the formatting of
+ * remove the course title and map each student's personal details (userID, firstName, lastName and DOB).
+ */
 public class XsltTransformer {
+
+    /**
+     * @author Stephanie Sun
+     * @param args transform XML to another XML with XSLT
+     */
     public static void main(String[] args) {
-        String xslFile ="coursetostudent.xsl";
-        String inputFile = "course.xml";
-        String outputFile = "students.xml";
+
+        // Files that requires for the transformation process
+        String xslFile ="two-step-view/src/main/xml.xslt/coursetostudent.xsl";
+        String inputFile = "two-step-view/src/main/xml.xslt/course.xml";
+        String outputFile = "two-step-view/src/main/xml.xslt/students.xml";
 
         StreamSource xsl = new StreamSource(new File(xslFile));
         StreamSource input = new StreamSource(new File(inputFile));
--- two-step-view/src/main/xml.xslt/coursetostudent.xsl
+++ two-step-view/src/main/xml.xslt/coursetostudent.xsl
@@ -1,1 +1,28 @@
 <?xml version="1.0" encoding="UTF-8" ?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output method="xml" indent="yes"/>
+    <xsl:template match="/">
+        <students>
+            <xsl:apply-templates/>
+        </students>
+    </xsl:template>
+    <xsl:template match="courses/course/students/student">
+        <student>
+            <xsl:attribute name="id">
+                <xsl:value-of select="@id"/>
+            </xsl:attribute>
+            <firstName>
+                <xsl:value-of select="firstName"/>
+            </firstName>
+            <lastName>
+                <xsl:value-of select="lastName"/>
+            </lastName>
+            <userName>
+                <xsl:value-of select="userName"/>
+            </userName>
+            <DOB>
+                <xsl:value-of select="DOB"/>
+            </DOB>
+        </student>
+    </xsl:template>
+</xsl:stylesheet>
