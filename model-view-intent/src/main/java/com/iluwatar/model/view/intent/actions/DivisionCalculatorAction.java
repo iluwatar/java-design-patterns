dccee214a6934ab8e529de8e1f07738b60ea90c5
JanFidor <jan.p.fidor@gmail.com>
2022-10-26 17:36:16 +0200

    #2151 add Calculator Actions

---
+++ model-view-intent/src/main/java/com/iluwatar/model/view/intent/actions/DivisionCalculatorAction.java
@@ -0,0 +1,13 @@
+package com.iluwatar.model.view.intent.actions;
+
+/**
+ *  Division {@link CalculatorAction}.
+ * */
+public class DivisionCalculatorAction implements CalculatorAction {
+  public static final String TAG = "DIVISION";
+
+  @Override
+  public String tag() {
+    return TAG;
+  }
+}
---
+++ model-view-intent/src/main/java/com/iluwatar/model/view/intent/actions/SubtractionCalculatorAction.java
@@ -0,0 +1,13 @@
+package com.iluwatar.model.view.intent.actions;
+
+/**
+ *  Subtraction {@link CalculatorAction}.
+ * */
+public class SubtractionCalculatorAction implements CalculatorAction {
+  public static final String TAG = "SUBTRACTION";
+
+  @Override
+  public String tag() {
+    return TAG;
+  }
+}
