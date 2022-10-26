63b200a8005cba27b87897f31233079325539b99
JanFidor <jan.p.fidor@gmail.com>
2022-10-26 17:36:16 +0200

    #2151 add Calculator Actions

---
+++ model-view-intent/src/main/java/com/iluwatar/model/view/intent/actions/MultiplicationCalculatorAction.java
@@ -0,0 +1,13 @@
+package com.iluwatar.model.view.intent.actions;
+
+/**
+ *  Multiplication {@link CalculatorAction}.
+ * */
+public class MultiplicationCalculatorAction implements CalculatorAction {
+  public static final String TAG = "MULTIPLICATION";
+
+  @Override
+  public String tag() {
+    return TAG;
+  }
+}
