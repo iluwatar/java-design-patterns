639c5d74b7b08fb3650e65b7a50c7edf1fb29238
Joshua Singla <u7309180@anu.edu.au>
2022-10-26 22:30:58 +1100

    #1261 Removed getPrinterQueue test

--- collecting-parameter/src/test/java/com/iluwatar/collectingparameter/PrinterQueueTest.java
+++ collecting-parameter/src/test/java/com/iluwatar/collectingparameter/PrinterQueueTest.java
@@ -19,23 +19,6 @@ public class PrinterQueueTest {
     assertSame(printerQueue1, printerQueue2);
   }
 
-  @Test
-  @Timeout(1000)
-  public void getPrinterQueueTest() {
-    PrinterQueue printerQueue = PrinterQueue.getInstance();
-    PrinterItem item1 = new PrinterItem(PaperSizes.A4, 5, false, false);
-    PrinterItem item2 = new PrinterItem(PaperSizes.A4, 10, true, false);
-    PrinterItem item3 = new PrinterItem(PaperSizes.A3, 2, false, false);
-    printerQueue.addPrinterItem(item1);
-    printerQueue.addPrinterItem(item2);
-    printerQueue.addPrinterItem(item3);
-    Queue<PrinterItem> testQueue = new LinkedList<>();
-    testQueue.add(item1);
-    testQueue.add(item2);
-    testQueue.add(item3);
-    Assertions.assertArrayEquals(testQueue.toArray(), printerQueue.getPrinterQueue().toArray());
-  }
-
   @Test()
   @Timeout(1000)
   public void negativePageCount() throws IllegalArgumentException {
