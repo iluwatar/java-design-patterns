45023e7d89bfcbe2962bc2ba64c40fd36fd251c2
Alaina <u7299023@anu.edu.au>
2022-10-27 15:36:07 +1100

    update helper

---
+++ template-view-pattern/src/Helper.java
@@ -0,0 +1,10 @@
+private Artist artist;
+public ArtistHelper(Artist artist) {
+        this.artist = artist;
+        }
+
+class ArtistHelper...
+
+public String getName() {
+        return artist.getName();
+        }
