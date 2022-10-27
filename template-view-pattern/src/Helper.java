520338f3b912d8fe3d142f1b5172a9fcebef7a4b
Alaina <u7299023@anu.edu.au>
2022-10-27 15:41:02 +1100

    update controller & helper

--- template-view-pattern/src/Helper.java
+++ template-view-pattern/src/Helper.java
@@ -1,9 +1,10 @@
+class Helper...
+
 private Artist artist;
 public ArtistHelper(Artist artist) {
         this.artist = artist;
         }
 
-class ArtistHelper...
 
 public String getName() {
         return artist.getName();
