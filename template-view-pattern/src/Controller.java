3fbea4777df03c4d6cd9597b05b00bd834965a11
Alaina <u7299023@anu.edu.au>
2022-10-27 15:34:35 +1100

    update controller.java

---
+++ template-view-pattern/src/Controller.java
@@ -0,0 +1,11 @@
+class ArtistController...
+
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Artist artist = Artist.findNamed(request.getParameter("name"));
+        if (artist == null)
+        forward("/MissingArtistError.jsp", request, response);
+        else {
+        request.setAttribute("helper", new ArtistHelper(artist));
+       forward("/artist.jsp", request, response);
+        }
+     }
