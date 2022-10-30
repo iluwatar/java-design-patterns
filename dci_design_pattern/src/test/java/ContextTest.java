96aa8fb91fbc42b0e1ca01fd19a4912ee32781f5
u7239623 <u7239623@anu.edu.au>
2022-10-30 22:21:35 +1100

    edit context class

--- dci_design_pattern/src/test/java/ContextTest.java
+++ dci_design_pattern/src/test/java/ContextTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ContextTest {
-    private final Bank bank = new Bank();
-    private final Context context = new Context();
+    private Bank bank = new Bank();
+    private Context context = new Context();
 
     @Test
     public void testTransfer() {
@@ -20,7 +20,6 @@ public class ContextTest {
         context.transfer(100,123456,789011,bank);
         int balance = bank.getAccount(123456).balance;
         assertEquals(900,balance);
-
     }
 
     //if transfer is successful, then b account will increase to 1100;
@@ -31,9 +30,24 @@ public class ContextTest {
         Account b = new Account(1000);
         bank.accountTable.put(123456,a);
         bank.accountTable.put(789011, b);
-        context.transfer(200,123456,789011,bank);
+        context.transfer(300,123456,789011,bank);
+
         int balance = bank.getAccount(123456).balance;
-        assertEquals(800,balance);
+        //int balance2 = bank.getAccount(789011).balance;
+        //System.out.println(balance2);
+        assertEquals(700,balance);
+    }
+
+    @Test
+    public void testTransfer3(){
+        Account a = new Account(1000);
+        Account b = new Account(1000);
+        bank.accountTable.put(123456,a);
+        bank.accountTable.put(789011, b);
+        context.receive(100,123456,789011,bank);
+        int balance = bank.getAccount(789011).balance;
+        assertEquals(1100,balance);
+
     }
 
 
