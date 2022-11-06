e09451f9721673932e4d93d4f948722e358ff709
Robert Volkmann <20912167+robertvolkmann@users.noreply.github.com>
2022-11-06 19:59:54 +0100

    dep: upgrade com.h2database.h2 1.4.190 to 2.1.214

--- transaction-script/src/main/java/com/iluwatar/transactionscript/RoomSchemaSql.java
+++ transaction-script/src/main/java/com/iluwatar/transactionscript/RoomSchemaSql.java
@@ -30,7 +30,7 @@ package com.iluwatar.transactionscript;
 public final class RoomSchemaSql {
 
   public static final String CREATE_SCHEMA_SQL =
-      "CREATE TABLE ROOMS (ID NUMBER, ROOM_TYPE VARCHAR(100), PRICE INT(100), BOOKED VARCHAR(100))";
+      "CREATE TABLE ROOMS (ID NUMBER, ROOM_TYPE VARCHAR(100), PRICE INT, BOOKED VARCHAR(100))";
   public static final String DELETE_SCHEMA_SQL = "DROP TABLE ROOMS IF EXISTS";
 
   private RoomSchemaSql() {
