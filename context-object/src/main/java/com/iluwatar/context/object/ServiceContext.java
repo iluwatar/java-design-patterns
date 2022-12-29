e223459db81c4c03e741450b65171647d2584798
Chak-C <114961815+Chak-C@users.noreply.github.com>
2022-12-29 20:17:28 +1100

    feature: Add context object pattern (#2304)
    * Add context object pattern and corresponding tests
    
    * Update context pattern
    
    * Add README, class diagram, puml file.
    Add toString method in ServiceContext.java.
    Add tests for coverage.
    
    * Improvements:
    Remove plugin in pom file
    Add comments in app.java
    Change local variable keyword to var in app.java
    Use lombok for getters, setters and tostring
    Change method signature in context object
    
    * Refreshing environment-1
    
    * Reconfigure pom file.
    
    Co-authored-by: Alvis Chan <u7287079@edu.anu.au>
    Co-authored-by: u7287079 <u7287079@anu.edu.au>

---
+++ context-object/src/main/java/com/iluwatar/context/object/ServiceContext.java
@@ -0,0 +1,16 @@
+package com.iluwatar.context.object;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Where context objects are defined.
+ */
+@ToString
+@Getter
+@Setter
+public class ServiceContext {
+
+   String AccountService, SessionService, SearchService;
+}
