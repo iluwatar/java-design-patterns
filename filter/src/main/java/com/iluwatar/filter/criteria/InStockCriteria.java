9dc69230c82223d805c5731816eaf8f7900a2532
Louise Tidestav <58272806+lollanlollan@users.noreply.github.com>
2023-02-27 10:26:21 +0100

    Feat: add class for in-stock criteria. Resolves #7.

---
+++ filter/src/main/java/com/iluwatar/filter/criteria/InStockCriteria.java
@@ -0,0 +1,28 @@
+package com.iluwatar.filter.criteria;
+
+import com.iluwatar.filter.product.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class for InStockCriteria.
+ */
+public class InStockCriteria implements Criteria<Product> {
+
+  /**
+   * Overrides method meetCriteria from interface Criteria.
+   * Filters out items that are in stock.
+   * @param items the list of items to filter
+   * @return the list of items in stock
+   */
+  @Override
+  public List<Product> meetCriteria(List<Product> items) {
+    List<Product> inStockProducts = new ArrayList<>();
+    for (Product product : items) {
+      if (product.getNrInStock() > 0) {
+        inStockProducts.add(product);
+      }
+    }
+    return inStockProducts;
+  }
+}
