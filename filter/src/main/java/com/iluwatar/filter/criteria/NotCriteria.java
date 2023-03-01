15f4e5d67aa14d72568e6923aac7bd9193832c80
carterwsmith <csmithonthetrack@gmail.com>
2023-03-01 15:41:53 +0100

    fix: NotCriteria checkstyle

--- filter/src/main/java/com/iluwatar/filter/criteria/NotCriteria.java
+++ filter/src/main/java/com/iluwatar/filter/criteria/NotCriteria.java
@@ -5,38 +5,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class definition for NotCriteria
+ * Class definition for NotCriteria.
  * @param exclusions the list of criteria that must NOT be met
  */
 public class NotCriteria implements Criteria<Product> {
-    List<Criteria> exclusions;
-  
-    /**
-     * Constructor for NotCriteria
-     */
-    public NotCriteria(List<Criteria> exclusions) {
-      this.exclusions = exclusions;
-    }
-  
-    /**
-     * Overrides Criteria interface method meetCriteria.
-     * @param items the list of items to filter
-     * @return a list of items that must NOT meet ANY of the criteria
-     */
-    @Override
-    public List<Product> meetCriteria(List<Product> items) {
-   
-      List<Product> meetNone = items;
+  List<Criteria> exclusions;
 
-      for (Criteria criterion : exclusions) {
+  /**
+   * Constructor for NotCriteria.
+   */
+  public NotCriteria(List<Criteria> exclusions) {
+    this.exclusions = exclusions;
+  }
 
-        List<Product> met = criterion.meetCriteria(items);
-        for (Product p : met) {
-          meetNone.remove(p);
-        }
-        
+  /**
+   * Overrides Criteria interface method meetCriteria.
+   * @param items the list of items to filter
+   * @return a list of items that must NOT meet ANY of the criteria
+   */
+  @Override
+  public List<Product> meetCriteria(List<Product> items) {
+
+    List<Product> meetNone = items;
+
+    for (Criteria criterion : exclusions) {
+      List<Product> met = criterion.meetCriteria(items);
+      for (Product p : met) {
+        meetNone.remove(p);
       }
-      
-      return meetNone;
     }
+    
+    return meetNone;
+  }
 }
