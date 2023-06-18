86fa5299f70f7bb267d01765c5eaed42e0d0ae7d
Owen Leung <owen.leung2@gmail.com>
2023-06-18 17:07:58 +0800

    Fix checkstyle error

--- singleton/src/main/java/com/iluwatar/singleton/package-info.java
+++ singleton/src/main/java/com/iluwatar/singleton/package-info.java
@@ -23,38 +23,3 @@
  * THE SOFTWARE.
  */
 package com.iluwatar.singleton;
-
-/**
- * <p>Thread-safe Singleton class. The instance is lazily initialized and thus needs synchronization
- * mechanism.</p>
- *
- */
-public final class ThreadSafeLazyLoadedIvoryTower {
-
-  /**
-   * Singleton instance of the class, declared as volatile to ensure atomic access by multiple threads.
-   */
-  private static volatile ThreadSafeLazyLoadedIvoryTower instance;
-
-  /**
-   * Private constructor to prevent instantiation from outside the class.
-   */
-  private ThreadSafeLazyLoadedIvoryTower() {
-    // Protect against instantiation via reflection
-    if (instance != null) {
-      throw new IllegalStateException("Already initialized.");
-    }
-  }
-
-  /**
-   * The instance doesn't get created until the method is called for the first time.
-   *
-   * @return an instance of the class.
-   */
-  public static synchronized ThreadSafeLazyLoadedIvoryTower getInstance() {
-    if (instance == null) {
-      instance = new ThreadSafeLazyLoadedIvoryTower();
-    }
-    return instance;
-  }
-}
