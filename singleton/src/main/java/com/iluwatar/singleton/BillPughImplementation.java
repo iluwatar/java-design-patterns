900900dc69dea0c59b9f7e6ab89a48a294b08a52
Owen Leung <owen.leung2@gmail.com>
2023-07-11 21:25:47 +0800

    Reformat file

--- singleton/src/main/java/com/iluwatar/singleton/BillPughImplementation.java
+++ singleton/src/main/java/com/iluwatar/singleton/BillPughImplementation.java
@@ -37,35 +37,35 @@ package com.iluwatar.singleton;
  */
 public final class BillPughImplementation {
 
-    /**
-     * Private constructor to prevent instantiation from outside the class.
-     */
-    private BillPughImplementation() {
-        // private constructor
-    }
+  /**
+   * Private constructor to prevent instantiation from outside the class.
+   */
+  private BillPughImplementation() {
+    // private constructor
+  }
 
-    /**
-     * The InstanceHolder is a static inner class and it holds the Singleton instance.
-     * It is not loaded into memory until the getInstance() method is called.
-     */
-    private static class InstanceHolder {
-        /**
-         * Singleton instance of the class.
-         */
-        private static BillPughImplementation instance = new BillPughImplementation();
-    }
+  /**
+   * The InstanceHolder is a static inner class and it holds the Singleton instance.
+   * It is not loaded into memory until the getInstance() method is called.
+   */
+  private static class InstanceHolder {
+  /**
+   * Singleton instance of the class.
+   */
+    private static BillPughImplementation instance = new BillPughImplementation();
+  }
 
-    /**
-     * Public accessor for the singleton instance.
-     *
-     * When this method is called, the InstanceHolder is loaded into memory
-     * and creates the Singleton instance. This method provides a global access point
-     * for the singleton instance.
-     *
-     * @return an instance of the class.
-     */
+  /**
+   * Public accessor for the singleton instance.
+   *
+   * When this method is called, the InstanceHolder is loaded into memory
+   * and creates the Singleton instance. This method provides a global access point
+   * for the singleton instance.
+   *
+   * @return an instance of the class.
+   */
     // global access point
-    public static BillPughImplementation getInstance() {
+  public static BillPughImplementation getInstance() {
         return InstanceHolder.instance;
     }
 }
