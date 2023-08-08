6d20a2fd731d2342bbb5da642e621a05531e25d4
TianLeZhou <tianlezhou.zhou@thoughtworks.com>
2023-08-08 14:12:33 +0800

    feat:adjust the interval of amount because it is not reasonable before and add condition when transfer TianLeZhou 9 minutes ago

--- monitor/src/main/java/com/iluwatar/monitor/Bank.java
+++ monitor/src/main/java/com/iluwatar/monitor/Bank.java
@@ -47,10 +47,13 @@
 
 package com.iluwatar.monitor;
 
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 
-/** Bank Definition. */
+import java.util.Arrays;
+
+/**
+ * Bank Definition.
+ */
 @Slf4j
 public class Bank {
 
@@ -72,19 +75,21 @@ public class Bank {
    *
    * @param accountA - source account
    * @param accountB - destination account
-   * @param amount - amount to be transferred
+   * @param amount   - amount to be transferred
    */
   public synchronized void transfer(int accountA, int accountB, int amount) {
-    if (accounts[accountA] >= amount) {
+    if (accounts[accountA] >= amount && accountA != accountB) {
       accounts[accountB] += amount;
       accounts[accountA] -= amount;
       if (LOGGER.isDebugEnabled()) {
         LOGGER.debug(
-            "Transferred from account: {} to account: {} , amount: {} , balance: {}",
+            "Transferred from account: {} to account: {} , amount: {} , bank balance at: {}, source account balance: {}, destination account balance: {}",
             accountA,
             accountB,
             amount,
-            getBalance());
+            getBalance(),
+            getBalance(accountA),
+            getBalance(accountB));
       }
     }
   }
@@ -100,6 +105,16 @@ public class Bank {
       balance += account;
     }
     return balance;
+  }
+
+  /**
+   * Get the accountNumber balance.
+   *
+   * @param accountNumber - accountNumber number
+   * @return accounts[accountNumber]
+   */
+  public synchronized int getBalance(int accountNumber) {
+    return accounts[accountNumber];
   }
 
   /**
--- monitor/src/main/java/com/iluwatar/monitor/Main.java
+++ monitor/src/main/java/com/iluwatar/monitor/Main.java
@@ -24,10 +24,11 @@
  */
 package com.iluwatar.monitor;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.security.SecureRandom;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * The Monitor pattern is used in concurrent algorithms to achieve mutual exclusion.
@@ -41,6 +42,8 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
   private static final int NUMBER_OF_THREADS = 5;
+  private static final int BASE_AMOUNT = 1000;
+  private static final int ACCOUNT_NUM = 4;
 
   /**
    * Runner to perform a bunch of transfers and handle exception.
@@ -54,7 +57,7 @@ public class Main {
       Thread.sleep(random.nextInt(1000));
       LOGGER.info("Start transferring...");
       for (int i = 0; i < 1000000; i++) {
-        bank.transfer(random.nextInt(4), random.nextInt(4), random.nextInt());
+        bank.transfer(random.nextInt(4), random.nextInt(4), random.nextInt(0, BASE_AMOUNT));
       }
       LOGGER.info("Finished transferring.");
       latch.countDown();
@@ -70,7 +73,7 @@ public class Main {
    * @param args command line args
    */
   public static void main(String[] args) throws InterruptedException {
-    var bank = new Bank(4, 1000);
+    var bank = new Bank(ACCOUNT_NUM, BASE_AMOUNT);
     var latch = new CountDownLatch(NUMBER_OF_THREADS);
     var executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
