744a5a7c9cc27c333c04adbf6c0a38db771b27d3
hsuanchushih <u7443726@anu.edu.au>
2023-10-18 10:22:37 +1100

    Issue: https://github.com/iluwatar/java-design-patterns/issues/1843

--- loan/src/test/java/com/iluwatar/loan/FileResourceLenderTest.java
+++ loan/src/test/java/com/iluwatar/loan/FileResourceLenderTest.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
