9d14afb724a06e210eb64d8f6bea319048a70a28
bu_ausb <2036870220@qq.com>
2023-10-28 21:43:32 +0800

    i don't known if it is right

---
+++ client-side/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
