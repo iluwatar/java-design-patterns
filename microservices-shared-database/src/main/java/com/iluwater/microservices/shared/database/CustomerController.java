44e598c500f53d5c4e4dfd553579c778c8a14297
Evan Cheung <u7516507@anu.edu.au>
2023-10-29 23:46:44 +1100

    Fixes : Wrong import lexicographical order fix up

--- microservices-shared-database/src/main/java/com/iluwater/microservices/shared/database/CustomerController.java
+++ microservices-shared-database/src/main/java/com/iluwater/microservices/shared/database/CustomerController.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -65,8 +65,8 @@ public class CustomerController {
   @PostMapping("/create/{creditLimit}")
   public ResponseEntity<String> createCustomer(@PathVariable double creditLimit) {
     try {
-      String customerID = customerService.newCustomer(creditLimit);
-      return ResponseEntity.ok("Customer created successfully with ID: " + customerID);
+      String customerId = customerService.newCustomer(creditLimit);
+      return ResponseEntity.ok("Customer created successfully with ID: " + customerId);
     } catch (Exception e) {
       return ResponseEntity.badRequest().body("Error creating customer: " + e.getMessage());
     }
--- microservices-shared-database/src/main/java/com/iluwater/microservices/shared/database/OrderController.java
+++ microservices-shared-database/src/main/java/com/iluwater/microservices/shared/database/OrderController.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
