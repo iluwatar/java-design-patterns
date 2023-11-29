4c5eacf01b03994f62d9750d5c79cd76b4ceac62
besok <zhguchev@hotmail.com>
2023-11-29 06:25:08 +0100

    add check style corrections

--- anti-corruption-layer/src/main/java/com/iluwatar/corruption/system/AntiCorruptionLayer.java
+++ anti-corruption-layer/src/main/java/com/iluwatar/corruption/system/AntiCorruptionLayer.java
@@ -1,54 +1,64 @@
 package com.iluwatar.corruption.system;
 
+import com.iluwatar.corruption.system.legacy.LegacyOrder;
+import com.iluwatar.corruption.system.legacy.LegacyShop;
 import com.iluwatar.corruption.system.modern.Customer;
 import com.iluwatar.corruption.system.modern.ModernOrder;
 import com.iluwatar.corruption.system.modern.ModernShop;
 import com.iluwatar.corruption.system.modern.Shipment;
-import com.iluwatar.corruption.system.legacy.LegacyShop;
-import com.iluwatar.corruption.system.legacy.LegacyOrder;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 /**
  * The class represents an anti-corruption layer.
  * The main purpose of the class is to provide a layer between the modern and legacy systems.
  * The class is responsible for converting the data from one system to another
  * decoupling the systems to each other
- * <p>
- * It allows using one system a domain model of the other system without changing the domain model of the system.
+ *
+ * <p>It allows using one system a domain model of the other system
+ * without changing the domain model of the system.
  */
 @Service
 public class AntiCorruptionLayer {
 
-    @Autowired
-    private ModernShop modernShop;
+  @Autowired
+  private ModernShop modernShop;
 
-    @Autowired
-    private LegacyShop legacyShop;
+  @Autowired
+  private LegacyShop legacyShop;
 
-    public Optional<LegacyOrder> findOrderInModernSystem(String id) {
-        return modernShop.findOrder(id).map(o ->
-                new LegacyOrder(
-                        o.getId(),
-                        o.getCustomer().getAddress(),
-                        o.getShipment().getItem(),
-                        o.getShipment().getQty(),
-                        o.getShipment().getPrice()
-                ));
-    }
+  /**
+   * The method converts the order from the modern system to the legacy system.
+   * @param id the id of the order
+   * @return the order in the legacy system
+   */
+  public Optional<LegacyOrder> findOrderInModernSystem(String id) {
+    return modernShop.findOrder(id).map(o ->
+        new LegacyOrder(
+            o.getId(),
+            o.getCustomer().getAddress(),
+            o.getShipment().getItem(),
+            o.getShipment().getQty(),
+            o.getShipment().getPrice()
+        ));
+  }
 
-    public Optional<ModernOrder> findOrderInLegacySystem(String id) {
+  /**
+   * The method converts the order from the legacy system to the modern system.
+   * @param id the id of the order
+   * @return the order in the modern system
+   */
+  public Optional<ModernOrder> findOrderInLegacySystem(String id) {
 
-        return legacyShop.findOrder(id).map(o ->
-                new ModernOrder(
-                        o.getId(),
-                        new Customer(o.getCustomer()),
-                        new Shipment(o.getItem(), o.getQty(), o.getPrice()),
-                        ""
-                )
-        );
-    }
+    return legacyShop.findOrder(id).map(o ->
+        new ModernOrder(
+            o.getId(),
+            new Customer(o.getCustomer()),
+            new Shipment(o.getItem(), o.getQty(), o.getPrice()),
+            ""
+        )
+    );
+  }
 
 }
--- anti-corruption-layer/src/main/java/com/iluwatar/corruption/system/modern/ModernOrder.java
+++ anti-corruption-layer/src/main/java/com/iluwatar/corruption/system/modern/ModernOrder.java
@@ -2,81 +2,99 @@ package com.iluwatar.corruption.system.modern;
 
 import java.util.Objects;
 
+/**
+ * The class represents an order in the modern system.
+ */
 public class ModernOrder {
-    private String id;
-    private Customer customer;
+  private String id;
+  private Customer customer;
 
-    private Shipment shipment;
+  private Shipment shipment;
 
-    private String extra;
+  private String extra;
 
+  /**
+   * Constructor.
+   * @param id the id of the order
+   * @param customer the customer of the order
+   * @param shipment the shipment of the order
+   * @param extra  the extra data of the order
+   */
+  public ModernOrder(String id, Customer customer, Shipment shipment, String extra) {
+    this.id = id;
+    this.customer = customer;
+    this.shipment = shipment;
+    this.extra = extra;
+  }
 
-    public ModernOrder(String id, Customer customer, Shipment shipment, String extra) {
-        this.id = id;
-        this.customer = customer;
-        this.shipment = shipment;
-        this.extra = extra;
+  @Override
+  public String toString() {
+    return "ModernOrder{"
+        + "id='"
+        + id + '\''
+        + ", customer=" + customer
+        + ", shipment=" + shipment
+        + ", extra='" + extra + '\''
+        + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
 
-    @Override
-    public String toString() {
-        return "ModernOrder{" +
-                "id='" + id + '\'' +
-                ", customer=" + customer +
-                ", shipment=" + shipment +
-                ", extra='" + extra + '\'' +
-                '}';
+    ModernOrder that = (ModernOrder) o;
+
+    if (!Objects.equals(id, that.id)) {
+      return false;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ModernOrder that = (ModernOrder) o;
-
-        if (!Objects.equals(id, that.id)) return false;
-        if (!Objects.equals(customer, that.customer)) return false;
-        return Objects.equals(shipment, that.shipment);
+    if (!Objects.equals(customer, that.customer)) {
+      return false;
     }
+    return Objects.equals(shipment, that.shipment);
+  }
 
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (customer != null ? customer.hashCode() : 0);
-        result = 31 * result + (shipment != null ? shipment.hashCode() : 0);
-        return result;
-    }
+  @Override
+  public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (customer != null ? customer.hashCode() : 0);
+    result = 31 * result + (shipment != null ? shipment.hashCode() : 0);
+    return result;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public Customer getCustomer() {
-        return customer;
-    }
+  public Customer getCustomer() {
+    return customer;
+  }
 
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
 
-    public Shipment getShipment() {
-        return shipment;
-    }
+  public Shipment getShipment() {
+    return shipment;
+  }
 
-    public void setShipment(Shipment shipment) {
-        this.shipment = shipment;
-    }
+  public void setShipment(Shipment shipment) {
+    this.shipment = shipment;
+  }
 
-    public String getExtra() {
-        return extra;
-    }
+  public String getExtra() {
+    return extra;
+  }
 
-    public void setExtra(String extra) {
-        this.extra = extra;
-    }
+  public void setExtra(String extra) {
+    this.extra = extra;
+  }
 }
