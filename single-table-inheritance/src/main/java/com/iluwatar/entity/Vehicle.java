2eb5c1658d0ee6784d89ec0de5065c907bcba07e
junhkang <junhkang91@gmail.com>
2024-03-26 09:33:43 +0900

    Updated the imports in code of the single table inheritance pattern for Spring Boot 3.x
    #2825
    Change javax library to jakarta

--- single-table-inheritance/src/main/java/com/iluwatar/entity/Vehicle.java
+++ single-table-inheritance/src/main/java/com/iluwatar/entity/Vehicle.java
@@ -1,12 +1,12 @@
 package com.iluwatar.entity;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
