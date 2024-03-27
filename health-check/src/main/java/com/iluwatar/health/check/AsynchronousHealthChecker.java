ab31c4499221ce9c907bc532203f54e43e61a697
junhkang <junhkang91@gmail.com>
2024-03-27 10:28:14 +0900

    change import order to pass lexicographical order test

--- health-check/src/main/java/com/iluwatar/health/check/AsynchronousHealthChecker.java
+++ health-check/src/main/java/com/iluwatar/health/check/AsynchronousHealthChecker.java
@@ -1,12 +1,12 @@
 package com.iluwatar.health.check;
 
+import jakarta.annotation.PreDestroy;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import jakarta.annotation.PreDestroy;
 import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
