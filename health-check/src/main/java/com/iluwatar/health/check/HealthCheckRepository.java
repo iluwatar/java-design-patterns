be914e0bc3265027678f711c9edd59c6ae325202
Jun Kang <junhkang91@gmail.com>
2024-03-27 22:38:32 +0900

    deps: Updated the imports in code of the healthcheck pattern for SpringBoot 3.x (#2851)
    * Updated the imports in code of the single table inheritance pattern for Spring Boot 3.x
    
    #2825
    Change javax library to jakarta
    
    * add pom.xml
    
    * Updated the imports in code of the healthcheck pattern for SpringBoot 3.x
    
    Change javax library to jakarta and update maven dependency versions
    
    * change order of imports to pass Checkstyle violations
    
    * change import order to pass lexicographical order test
    
    * change import order to pass CustomImportOrder warning

--- health-check/src/main/java/com/iluwatar/health/check/HealthCheckRepository.java
+++ health-check/src/main/java/com/iluwatar/health/check/HealthCheckRepository.java
@@ -1,8 +1,8 @@
 package com.iluwatar.health.check;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
