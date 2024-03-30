7c1889b8e5800b60013a96d567b64df51dd62ae1
Ilkka Seppälä <ilkka.seppala@gofore.com>
2024-03-30 08:34:54 +0200

    refactor: update layers parent, reformat code, update license headers

--- layers/src/main/java/com/iluwatar/layers/app/LayersApp.java
+++ layers/src/main/java/com/iluwatar/layers/app/LayersApp.java
@@ -1,3 +1,27 @@
+/*
+ * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
+ *
+ * The MIT License
+ * Copyright © 2014-2022 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.layers.app;
 
 import org.springframework.boot.SpringApplication;
@@ -6,15 +30,23 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * The Layers pattern is a structural design pattern that organizes system architecture into
+ * distinct layers, each with a specific responsibility and abstraction level. This separation
+ * allows for increased modularity, facilitating independent development, maintenance, and reuse
+ * of each layer. Commonly, layers interact with each other through well-defined interfaces, with
+ * higher layers (more abstract) depending on lower layers (more concrete), but not vice versa,
+ * promoting a clear hierarchy and separation of concerns.
+ */
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "dao")
 @EntityScan(basePackages = "entity")
-@ComponentScan(basePackages = {"com.iluwatar.layers", "service", "dto", "exception", "view" ,"dao"})
+@ComponentScan(basePackages = {"com.iluwatar.layers", "service", "dto", "exception", "view", "dao"})
 public class LayersApp {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LayersApp.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(LayersApp.class, args);
 
-    }
+  }
 
 }
