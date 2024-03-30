daacaf1433563a02bbfed6b0f06312d17c8be321
drishtii7 <drishtipatel4543@gmail.com>
2024-03-30 20:52:12 -0300

    Changes updated

--- abstract-document/src/main/java/com/iluwatar/abstractdocument/AbstractDocument.java
+++ abstract-document/src/main/java/com/iluwatar/abstractdocument/AbstractDocument.java
@@ -1,3 +1,27 @@
+/*
+ * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
+ *
+ * The MIT License
+ * Copyright © 2014-2022 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.iluwatar.abstractdocument;
 
 import java.util.Collection;
@@ -12,22 +36,22 @@ import java.util.stream.Stream;
  */
 public abstract class AbstractDocument implements Document {
 
-  private final Map<String, Object> documentProperties; // Renamed variable
+  private final Map<String, Object> documentProperties;
 
   protected AbstractDocument(Map<String, Object> properties) {
     Objects.requireNonNull(properties, "properties map is required");
-    this.documentProperties = properties; // Updated variable assignment
+    this.documentProperties = properties;
   }
 
   @Override
   public Void put(String key, Object value) {
-    documentProperties.put(key, value); // Updated variable usage
+    documentProperties.put(key, value);
     return null;
   }
 
   @Override
   public Object get(String key) {
-    return documentProperties.get(key); // Updated variable usage
+    return documentProperties.get(key);
   }
 
   @Override
@@ -76,5 +100,5 @@ public abstract class AbstractDocument implements Document {
     builder.append("]");
     return builder.toString();
   }
-  
+
 }
