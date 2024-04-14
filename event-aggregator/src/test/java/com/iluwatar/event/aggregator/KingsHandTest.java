8a3bed0e950cbc5d9030aa51caf070ecd70cba94
Ilkka Seppälä <ilkka.seppala@gofore.com>
2024-04-14 11:52:52 +0300

    docs: update event aggregator docs

--- event-aggregator/src/test/java/com/iluwatar/event/aggregator/KingsHandTest.java
+++ event-aggregator/src/test/java/com/iluwatar/event/aggregator/KingsHandTest.java
@@ -49,11 +49,11 @@ class KingsHandTest extends EventEmitterTest<KingsHand> {
 
   /**
    * The {@link KingsHand} is both an {@link EventEmitter} as an {@link EventObserver} so verify if
-   * every event received is passed up to it's superior, in most cases {@link KingJoffrey} but now
+   * every event received is passed up to its superior, in most cases {@link KingJoffrey} but now
    * just a mocked observer.
    */
   @Test
-  void testPassThrough() throws Exception {
+  void testPassThrough() {
     final var observer = mock(EventObserver.class);
     final var kingsHand = new KingsHand();
     kingsHand.registerObserver(observer, Event.STARK_SIGHTED);
