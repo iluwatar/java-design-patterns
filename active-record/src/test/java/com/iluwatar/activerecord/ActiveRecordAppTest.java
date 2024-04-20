385cd4bf3b985ea1a6819d6c4d3543a0335db5c3
Sergejs Visockis <sergei.visotsky@gmail.com>
2024-04-20 14:08:57 +0200

    add copyright

--- active-record/src/test/java/com/iluwatar/activerecord/ActiveRecordAppTest.java
+++ active-record/src/test/java/com/iluwatar/activerecord/ActiveRecordAppTest.java
@@ -22,7 +22,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package com.iluwatar.activerecord;
 
 import static com.iluwatar.activerecord.SchemaConstants.DB_URL;
