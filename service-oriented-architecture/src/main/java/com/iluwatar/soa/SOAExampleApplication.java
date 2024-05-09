b35a33fb79403aa4542f964ac2a28323b3946d5c
Roman Nimets <roman.nimets97@gmail.com>
2024-05-09 11:28:21 +0200

    Starting registry implementation

--- service-oriented-architecture/src/main/java/com/iluwatar/soa/SOAExampleApplication.java
+++ service-oriented-architecture/src/main/java/com/iluwatar/soa/SOAExampleApplication.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SOAExampleApplication {
 
   private static final Logger log = LoggerFactory.getLogger(SOAExampleApplication.class);
