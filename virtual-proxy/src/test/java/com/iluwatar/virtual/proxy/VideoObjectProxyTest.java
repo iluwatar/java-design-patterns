3cadb6bbf48bfb16c2c3516f53cd4c5e5046c092
adelasaripova <adelya19.2001@mail.ru>
2024-05-14 19:29:59 +0300

    feature: Implement Virtual Proxy pattern iluwatar#2940

--- virtual-proxy/src/test/java/com/iluwatar/virtual/proxy/VideoObjectProxyTest.java
+++ virtual-proxy/src/test/java/com/iluwatar/virtual/proxy/VideoObjectProxyTest.java
@@ -22,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package com.iluwatar.virtual.proxy;
 
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
