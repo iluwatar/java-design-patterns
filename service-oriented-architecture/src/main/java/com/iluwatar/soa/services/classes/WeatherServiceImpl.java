3ac36043bed9f0aa0fd58ace197087fbf83cc034
Roman Nimets <roman.nimets97@gmail.com>
2024-06-22 17:42:17 +0200

    rename randomConditionIndex variable, use of ConcurrentHashMap in ServiceRegistry

--- service-oriented-architecture/src/main/java/com/iluwatar/soa/services/classes/WeatherServiceImpl.java
+++ service-oriented-architecture/src/main/java/com/iluwatar/soa/services/classes/WeatherServiceImpl.java
@@ -1,6 +1,5 @@
 package com.iluwatar.soa.services.classes;
 
-
 import com.iluwatar.soa.model.WeatherCondition;
 import java.util.Random;
 import com.iluwatar.soa.services.interfaces.WeatherService;
@@ -8,13 +7,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherServiceImpl implements WeatherService {
- final Random random = new Random();
+  private final Random random = new Random();
 
   public WeatherCondition getCurrentWeather() {
-    // Simulazione di una scelta casuale delle condizioni meteo
     WeatherCondition[] conditions = WeatherCondition.values();
-    int index = random.nextInt(conditions.length);
-    return conditions[index];
+    int randomConditionIndex = random.nextInt(conditions.length);
+    return conditions[randomConditionIndex];
   }
-
 }
--- service-oriented-architecture/src/main/java/com/iluwatar/soa/services/registry/ServiceRegistry.java
+++ service-oriented-architecture/src/main/java/com/iluwatar/soa/services/registry/ServiceRegistry.java
@@ -1,10 +1,10 @@
 package com.iluwatar.soa.services.registry;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ServiceRegistry {
-  public static final Map<String, Object> registry = new HashMap<>();
+  public static final Map<String, Object> registry = new ConcurrentHashMap<>();
 
   public static <T> void registerService(String serviceName, T serviceInstance) {
     registry.put(serviceName, serviceInstance);
