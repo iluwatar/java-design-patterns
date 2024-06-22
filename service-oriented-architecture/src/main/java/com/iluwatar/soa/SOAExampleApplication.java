aeb3bcd88627294fd0d37312f4779478a2d36f42
Roman Nimets <roman.nimets97@gmail.com>
2024-06-22 16:46:04 +0200

    added lombok

--- service-oriented-architecture/src/main/java/com/iluwatar/soa/SOAExampleApplication.java
+++ service-oriented-architecture/src/main/java/com/iluwatar/soa/SOAExampleApplication.java
@@ -7,43 +7,45 @@ import com.iluwatar.soa.services.interfaces.GreetingService;
 import com.iluwatar.soa.services.interfaces.PersonalizedGreetingService;
 import com.iluwatar.soa.services.interfaces.WeatherService;
 import com.iluwatar.soa.services.registry.ServiceRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SOAExampleApplication {
-
-  private static final Logger log = LoggerFactory.getLogger(SOAExampleApplication.class);
 
   public static void main(String[] args) {
     configureAndRegisterServices();
     SpringApplication.run(SOAExampleApplication.class, args);
-    log.info("SOA Example Application started successfully!");
-    log.info("You can access the application via: http://localhost:8080/login");
-    log.info("This endpoint will return a personalized greeting based on the weather conditions.");
-    log.info("You can explore other endpoints as well for different functionalities.");
+    LOGGER.info("SOA Example Application started successfully!");
+    LOGGER.info("You can access the application via: http://localhost:8080/login");
+    LOGGER.info(
+        "This endpoint will return a personalized greeting based on the weather conditions.");
+    LOGGER.info("You can explore other endpoints as well for different functionalities.");
   }
 
   private static void configureAndRegisterServices() {
-    log.info("Configuring and registering services...");
+    LOGGER.info("Configuring and registering services...");
 
 
     GreetingService greetingService = new GreetingServiceImpl();
     WeatherService weatherService = new WeatherServiceImpl();
-    PersonalizedGreetingService personalizedGreetingService = new PersonalizedGreetingServiceImpl(greetingService, weatherService);
+    PersonalizedGreetingService personalizedGreetingService =
+        new PersonalizedGreetingServiceImpl(greetingService, weatherService);
 
 
     ServiceRegistry.registerService("greetingService", greetingService);
     ServiceRegistry.registerService("weatherService", weatherService);
     ServiceRegistry.registerService("personalizedGreetingService", personalizedGreetingService);
 
-    log.info("Services configured and registered successfully.");
+    LOGGER.info("Services configured and registered successfully.");
 
 
-    log.info("Current service registry:");
-    ServiceRegistry.registry.forEach((serviceName, serviceInstance) -> log.info("- {} : {}", serviceName, serviceInstance.getClass().getName()));
+    LOGGER.info("Current service registry:");
+    ServiceRegistry.registry.forEach(
+        (serviceName, serviceInstance) -> LOGGER.info("- {} : {}", serviceName,
+            serviceInstance.getClass().getName()));
   }
 }
 
--- service-oriented-architecture/src/main/java/com/iluwatar/soa/services/classes/PersonalizedGreetingServiceImpl.java
+++ service-oriented-architecture/src/main/java/com/iluwatar/soa/services/classes/PersonalizedGreetingServiceImpl.java
@@ -4,21 +4,19 @@ import com.iluwatar.soa.model.WeatherCondition;
 import com.iluwatar.soa.services.interfaces.GreetingService;
 import com.iluwatar.soa.services.interfaces.PersonalizedGreetingService;
 import com.iluwatar.soa.services.interfaces.WeatherService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class PersonalizedGreetingServiceImpl implements PersonalizedGreetingService {
 
   private final GreetingService greetingService;
   private final WeatherService weatherService;
 
-  @Autowired
-  public PersonalizedGreetingServiceImpl(GreetingService greetingService, WeatherService weatherService) {
-    this.greetingService = greetingService;
-    this.weatherService = weatherService;
-  }
-
+ @Override
   public String generateGreeting() {
     String weatherGreeting = getWeatherGreeting();
     return weatherGreeting + "! " + greetingService.getGenericGreeting();
@@ -26,19 +24,15 @@ public class PersonalizedGreetingServiceImpl implements PersonalizedGreetingServ
 
   private String getWeatherGreeting() {
     WeatherCondition currentWeather = weatherService.getCurrentWeather();
-    switch (currentWeather) {
-      case SUNNY:
-        return "What a good sunny day!";
-      case RAINY:
-        return "What a rainy day!";
-      case CLOUDY:
-        return "What a cloudy day!";
-      case FOGGY:
-        return "What a foggy day!";
-      default:
-        // error case
-        return "unexpected weather condition";
-    }
+    return switch (currentWeather) {
+      case SUNNY -> "What a good sunny day!";
+      case RAINY -> "What a rainy day!";
+      case CLOUDY -> "What a cloudy day!";
+      case FOGGY -> "What a foggy day!";
+      default -> {
+        LOGGER.error("Unexpected weather condition: {}", currentWeather);
+        yield "unexpected weather condition";
+      }
+    };
   }
 }
-
