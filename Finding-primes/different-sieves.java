cd7382376cf66ceb1d6acbeee87b4cce1fbe2ddf
Darshit <166272518+Shad0w0192@users.noreply.github.com>
2024-10-24 19:23:22 +0530

    different methods to find primes
    its a famous method just thought it should exist in this repo :P

---
+++ Finding-primes/different-sieves.java
@@ -0,0 +1,20 @@
+class SieveOfEratosthenes {
+    void sieveOfEratosthenes(int n)
+    {
+        boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true) {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (prime[i] == true)
+                System.out.print(i + " ");
+        }
+    }
+}
