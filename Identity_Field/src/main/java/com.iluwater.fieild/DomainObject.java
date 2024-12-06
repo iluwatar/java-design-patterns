f6dcf905c64a3087a4d27b80b76d8d8b34c27b15
rehabmahmoud <rehabmahmoud521@gmail.com>
2024-12-06 21:40:21 +0200

    Identity Field Pattern

--- Identity_Field/src/main/java/com.iluwater.fieild/DomainObject.java
+++ Identity_Field/src/main/java/com.iluwater.fieild/DomainObject.java
@@ -1,22 +1,17 @@
 package com.iluwater.fieild;
 
-
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class DomainObject {
+  @lombok.Setter
+  @lombok.Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // automatically generate unique values for primary key columns
     //strategy = GenerationType.IDENTITY generate the primary key value by the database itself using the auto-increment column option
-    private Long id;
+    private Integer id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
