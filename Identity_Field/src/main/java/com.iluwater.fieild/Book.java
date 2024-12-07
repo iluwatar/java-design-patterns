98237cd018a0c96e69122d4d2b098dcf7af07750
rehabmahmoud <rehabmahmoud521@gmail.com>
2024-12-07 13:33:39 +0200

    Identity Field Pattern

--- Identity_Field/src/main/java/com.iluwater.fieild/Book.java
+++ Identity_Field/src/main/java/com.iluwater.fieild/Book.java
@@ -1,14 +1,38 @@
-/*
- * The Book class represents a book entity in the application.
- *
- * <p>This class extends DomainObject and inherits its unique identifier.
- * It includes additional fields and methods specific to a book's attributes,
- * such as title and author.</p>
- */
+ /*
+  * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
+  *
+  * The MIT License
+  * Copyright © 2014-2022 Ilkka Seppälä
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  * THE SOFTWARE.
+  */
+
 package com.iluwater.fieild;
 import lombok.Getter;
 import lombok.Setter;
-
+ /*
+  * The Book class represents a book entity in the application.
+  *
+  * <p>This class extends DomainObject and inherits its unique identifier.
+  * It includes additional fields and methods specific to a book's attributes,
+  * such as title and author.</p>
+  */
 @Setter
 @Getter
 public class Book extends DomainObject {
--- Identity_Field/src/main/java/com.iluwater.fieild/DomainObject.java
+++ Identity_Field/src/main/java/com.iluwater.fieild/DomainObject.java
@@ -1,11 +1,28 @@
+ /*
+  * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
+  *
+  * The MIT License
+  * Copyright © 2014-2022 Ilkka Seppälä
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  * THE SOFTWARE.
+  */
 
-/*
- * The DomainObject class provides a base for domain entities
- * that require unique identification within the application.
- *
- * <p>All child classes inherit the unique identifier field
- * and associated functionality.</p>
- */
 package com.iluwater.fieild;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +30,13 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+ /*
+  * The DomainObject class provides a base for domain entities
+  * that require unique identification within the application.
+  *
+  * <p>All child classes inherit the unique identifier field
+  * and associated functionality.</p>
+  */
 @Getter
 @Setter
 @MappedSuperclass
