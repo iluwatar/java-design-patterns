26cb1b2a258e169d4a1ae03b7d728e3e32d46e37
Soham Kamble <121136639+skamble2@users.noreply.github.com>
2025-06-01 12:42:17 -0500

    fixed shouldResetCounterAfterWindow() test #2973

--- rate-limiting-pattern/src/test/java/com/iluwatar/rate/limiting/pattern/FixedWindowRateLimiterTest.java
+++ rate-limiting-pattern/src/test/java/com/iluwatar/rate/limiting/pattern/FixedWindowRateLimiterTest.java
@@ -13,11 +13,18 @@ class FixedWindowRateLimiterTest extends RateLimiterTest {
 
   @Test
   void shouldResetCounterAfterWindow() throws Exception {
-    FixedWindowRateLimiter limiter = new FixedWindowRateLimiter(1, 1);
+    FixedWindowRateLimiter limiter = new FixedWindowRateLimiter(1, 1); // 1 request per 1 second window
+
+    // First request should pass
     limiter.check("test", "op");
+
+    // Second request in same window should be throttled
     assertThrows(RateLimitException.class, () -> limiter.check("test", "op"));
 
-    TimeUnit.SECONDS.sleep(1);
+    // Wait a bit more than 1 second to ensure window resets
+    TimeUnit.MILLISECONDS.sleep(1100);
+
+    // After window reset, this should pass again
     limiter.check("test", "op");
   }
 
