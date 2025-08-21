c20000c77d52a99d9983dbfab79c8f83c0c7c471
Tushar Kumar Roy <tusharkumarroy.dev@gmail.com>
2025-08-21 06:22:26 +0600

    Modified add method in feature/calculator differently to create a conflict

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public int add(int a, int b) { return a + b; }
+    public int add(int a, int b) { return a + b + 20; }   // different modification in feature/calculator to create conflict
     public int subtract(int a, int b) { return a - b; }
     public int multiply(int a, int b) { return a * b; }
     public int divide(int a, int b) {
