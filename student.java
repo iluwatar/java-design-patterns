a901403d9c335492f49c099bd0aeed5748b967b8
Arka Nath <natharka55@gmail.com>
2025-08-21 18:41:03 +0600

    student.java added

---
+++ student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {}
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+}
