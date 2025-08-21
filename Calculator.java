dcda634fe0f493c566f41526f5cc6dc7f507881e
Tushar Kumar Roy <tusharkumarroy.dev@gmail.com>
2025-08-21 06:20:06 +0600

    Modified add method in master to create a  conflict

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int add(int a, int b) { return a + b + 10; }  // modifying master to create merge conflict
+    public int subtract(int a, int b) { return a - b; }
+    public int multiply(int a, int b) { return a * b; }
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b;
+    }
+}
