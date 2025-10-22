9f414305e3d118f5325fb08e1ba1afe74594d894
crashmovies <swarnakarharshendu420@gmail.com>
2025-10-23 00:10:37 +0530

    reverted back to original model name

--- thread-specific-storage/src/test/java/com/iluwatar/threadspecificstorage/AppTest.java
+++ thread-specific-storage/src/test/java/com/iluwatar/threadspecificstorage/AppTest.java
@@ -4,7 +4,7 @@
  * The MIT License
  * Copyright © 2014-2022 Ilkka Seppälä
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy  
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -27,13 +27,14 @@ package com.iluwatar.threadspecificstorage;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class AppTest {
 
   @Test
   void appStartsWithoutException() {
-    assertDoesNotThrow(() -> App.main(new String[] {})) ;
+    assertDoesNotThrow(() -> App.main(new String[] {}));
   }
 
   @Test
