4a621f6920f471fbd5de9bba7a6ff1916bf387d6
Mulla Arsiya Tasleem <2400090250@kluniversity.in>
2025-10-23 13:16:39 +0530

    Create wordPattern.java

---
+++ wordPattern.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (!charToWord.containsKey(c)) {
+                charToWord.put(c, word);
+            }
+
+            if (!wordToChar.containsKey(word)) {
+                wordToChar.put(word, c);
+            }
+
+            if (!charToWord.get(c).equals(word) || !wordToChar.get(word).equals(c)) {
+                return false;
+            }
+        }
+
+        return true;        
+    }
+}
