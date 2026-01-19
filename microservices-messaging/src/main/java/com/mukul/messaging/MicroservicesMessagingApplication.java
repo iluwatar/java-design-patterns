6b1913ef8d2c6641f0139ec5eba0f49e3bdbac4d
Mukul Howale <howalemukul@gmail.com>
2026-01-19 22:31:11 +0530

    Initialize microservices-messaging Spring Boot project
    Add initial project structure for microservices-messaging using Spring Boot. Includes Maven configuration with dependencies for Kafka, Lombok, and testing, as well as main application, test class, and application properties.

---
+++ microservices-messaging/src/main/java/com/mukul/messaging/MicroservicesMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.mukul.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMessagingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroservicesMessagingApplication.class, args);
+  }
+
+}
