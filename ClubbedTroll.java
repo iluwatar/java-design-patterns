42468552281aa35683bc4ac20373c551053cf4e3
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-04-16 23:38:26 -0400

    Remove Lombok @RequiredArgsConstructor and manually implement constructor for Troll initialization

---
+++ ClubbedTroll.java
@@ -0,0 +1,11 @@
+public class ClubbedTroll {
+    private final String name;
+    private final int level;
+
+    public ClubbedTroll(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    // Other methods...
+}
