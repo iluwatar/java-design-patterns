1c415047f4068f73170aa7ca4e40a5f6a65ba889
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-27 13:30:31 +0530

    test: add ImmutableObjectTest for coverage

---
+++ immutable/src/test/java/com/iluwatar/immutable/ImmutableObjectTest.java
@@ -0,0 +1,35 @@
+package com.iluwatar.immutable;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImmutableObjectTest {
+
+    @Test
+    void testObjectCreation() {
+        ImmutableObject obj = 
+            new ImmutableObject("Prashant", 22, "p@gmail.com");
+        
+        assertEquals("Prashant", obj.getName());
+        assertEquals(22, obj.getAge());
+        assertEquals("p@gmail.com", obj.getEmail());
+    }
+
+    @Test
+    void testToString() {
+        ImmutableObject obj = 
+            new ImmutableObject("Prashant", 22, "p@gmail.com");
+        
+        assertTrue(obj.toString().contains("Prashant"));
+    }
+
+    @Test
+    void testImmutability() {
+        ImmutableObject obj1 = 
+            new ImmutableObject("A", 1, "a@gmail.com");
+        ImmutableObject obj2 = 
+            new ImmutableObject("A", 1, "a@gmail.com");
+        
+        assertNotSame(obj1, obj2);
+    }
+}
